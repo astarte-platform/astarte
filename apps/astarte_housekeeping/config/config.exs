6aeca9a0a66dee7c25550efd5c6599171cab422b
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-14 18:36:43 +0200

    config.exs: add configuration for amqp

--- apps/astarte_housekeeping/config/config.exs
+++ apps/astarte_housekeeping/config/config.exs
@@ -2,6 +2,24 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :housekeeping_engine, :amqp,
+  username: "guest",
+  password: "guest",
+  hostname: "localhost",
+  virtual_host: "/",
+  port: 5672,
+  rpc_queue: "housekeeping_rpc"
+
+# If SSL is needed add
+# ssl_options: [cacertfile: '/path/to/testca/cacert.pem',
+#               certfile: '/path/to/client/cert.pem',
+#               keyfile: '/path/to/client/key.pem',
+#               # only necessary with intermediate CAs
+#               # depth: 2,
+#               verify: :verify_peer,
+#               fail_if_no_peer_cert: true]
+
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
