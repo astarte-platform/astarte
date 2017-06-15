80a66cbe1bf66cc382c7401bb6d59bf606993721
Dario Freddi <dario.freddi@ispirata.com>
2017-06-15 16:15:59 +0200

    Enable tests and coverage lookup through excoveralls

--- apps/astarte_housekeeping/mix.exs
+++ apps/astarte_housekeeping/mix.exs
@@ -11,6 +11,8 @@ defmodule HousekeepingEngine.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
      deps: deps()]
   end
 
@@ -26,7 +28,9 @@ defmodule HousekeepingEngine.Mixfile do
       {:cqex, "~> 0.2.0"},
       {:cqerl, github: "matehat/cqerl"},
       {:re2, git: "https://github.com/tuncer/re2.git", tag: "v1.7.2", override: true},
-      {:distillery, "~> 1.4"}
+      {:distillery, "~> 1.4"},
+
+      {:excoveralls, "~> 0.6", only: :test}
     ]
   end
 end
