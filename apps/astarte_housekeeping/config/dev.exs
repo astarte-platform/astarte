f5a516e393385c3c8d83e023298b511c174cb762
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-16 10:52:38 +0200

    Split amqp configuration to {dev,prod,test}.exs
    Currently it is the same config but now it can be adapted for the
    different environments

--- apps/astarte_housekeeping/config/dev.exs
+++ apps/astarte_housekeeping/config/dev.exs
@@ -2,3 +2,20 @@ use Mix.Config
 
 config :cqerl,
   cassandra_nodes: [{System.get_env("CASSANDRA_DB_HOST"), System.get_env("CASSANDRA_DB_PORT")}]
+
+config :housekeeping_engine, :amqp,
+  username: "guest",
+  password: "guest",
+  hostname: "localhost",
+  virtual_host: "/",
+  port: 5672,
+  rpc_queue: "housekeeping_rpc"
+
+# If SSL is needed add
+# ssl_options: [cacertfile: '/path/to/testca/cacert.pem',
+#               certfile: '/path/to/client/cert.pem',
+#               keyfile: '/path/to/client/key.pem',
+#               # only necessary with intermediate CAs
+#               # depth: 2,
+#               verify: :verify_peer,
+#               fail_if_no_peer_cert: true]
--- apps/astarte_housekeeping/config/prod.exs
+++ apps/astarte_housekeeping/config/prod.exs
@@ -2,3 +2,11 @@ use Mix.Config
 
 config :cqerl,
   cassandra_nodes: [{System.get_env("CASSANDRA_DB_HOST"), System.get_env("CASSANDRA_DB_PORT")}]
+
+config :housekeeping_engine, :amqp,
+  username: "guest",
+  password: "guest",
+  hostname: "localhost",
+  virtual_host: "/",
+  port: 5672,
+  rpc_queue: "housekeeping_rpc"
