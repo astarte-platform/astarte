2fba1747505c9f173b51f90f323ed12b4d3aaee0
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-20 17:54:43 +0200

    housekeeping_engine_test: remove CallType from tests

--- apps/astarte_housekeeping/test/housekeeping_engine_test.exs
+++ apps/astarte_housekeeping/test/housekeeping_engine_test.exs
@@ -5,30 +5,24 @@ defmodule HousekeepingEngineTest do
   test "Protobuf RPC round trip" do
     create_realm_call = Housekeeping.RPC.CreateRealm.new(realm: "testRealm")
     reply_header = Housekeeping.RPC.ReplyHeader.new(call_id: 42, reply_exchange: "testExchange")
-    type = :CREATE_REALM
     version = 42
     rpc = Housekeeping.RPC.Call.new(call: {:create_realm, create_realm_call},
                                     reply_header: reply_header,
-                                    type: type,
                                     version: version)
     encoded = Housekeeping.RPC.Call.encode(rpc)
     decoded = Housekeeping.RPC.Call.decode(encoded)
     %Housekeeping.RPC.Call{call: {:create_realm, matched_call},
                            reply_header: matched_reply_header,
-                           type: matched_type,
                            version: matched_version} = decoded
     assert(matched_call == create_realm_call)
     assert(matched_reply_header == reply_header)
-    assert(matched_type == type)
     assert(matched_version == version)
   end
 
   test "that we know when to reply" do
     create_realm_call = Housekeeping.RPC.CreateRealm.new(realm: "testRealm")
-    type = :CREATE_REALM
     version = 42
     rpc = Housekeeping.RPC.Call.new(call: {:create_realm, create_realm_call},
-                                    type: type,
                                     version: version)
 
     willReply = case rpc do
