0b7d7bf3578020a139eded6f723725384f4c2286
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-21 15:44:28 +0200

    housekeeping_engine: use GenServer
    The state is the connection to the DB

--- apps/astarte_housekeeping/lib/housekeeping_engine.ex
+++ apps/astarte_housekeeping/lib/housekeeping_engine.ex
@@ -1,2 +1,11 @@
 defmodule Housekeeping.Engine do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: :housekeeping_engine)
+  end
+
+  def init(_opts) do
+    CQEx.Client.new()
+  end
 end
