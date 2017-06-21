8ad8342d5b6095a164d4c052b6a8a3c28cc90192
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-21 15:54:57 +0200

    housekeeping_engine: add process_rpc call

--- apps/astarte_housekeeping/lib/housekeeping_engine.ex
+++ apps/astarte_housekeeping/lib/housekeeping_engine.ex
@@ -8,4 +8,18 @@ defmodule Housekeeping.Engine do
   def init(_opts) do
     CQEx.Client.new()
   end
+
+  def process_rpc(message) do
+    GenServer.call(:housekeeping_engine, {:process_rpc, message})
+  end
+
+  def handle_call({:process_rpc, message}, _from, client) do
+    %Housekeeping.RPC.Call{call: call_tuple} = Housekeeping.RPC.Call.decode(message)
+    reply =
+      case call_tuple do
+        {:create_realm, %Housekeeping.RPC.CreateRealm{realm: realm}} ->
+          Housekeeping.Queries.create_realm(client, realm)
+      end
+    {:reply, reply, client}
+  end
 end
