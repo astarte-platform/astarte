aae472187515932bfe7506b8ea6601a61b78ecc8
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-21 15:23:15 +0200

    housekeeping_engine: remove placeholder code

--- apps/astarte_housekeeping/lib/housekeeping_engine.ex
+++ apps/astarte_housekeeping/lib/housekeeping_engine.ex
@@ -1,18 +1,2 @@
 defmodule HousekeepingEngine do
-  @moduledoc """
-  Documentation for HousekeepingEngine.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HousekeepingEngine.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
