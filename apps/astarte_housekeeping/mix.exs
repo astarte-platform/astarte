8362b8911620d8e3067dfb35e0a08bef4a31a4d6
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-28 16:35:28 +0200

    Add astarte_rpc dependency

--- apps/astarte_housekeeping/mix.exs
+++ apps/astarte_housekeeping/mix.exs
@@ -27,6 +27,7 @@ defmodule HousekeepingEngine.Mixfile do
   defp deps do
     [
       {:astarte_core, git: "https://git.ispirata.com/Astarte-NG/astarte_core"},
+      {:astarte_rpc, git: "https://git.ispirata.com/Astarte-NG/astarte_rpc"},
       {:cqex, "~> 0.2.0"},
       {:cqerl, github: "matehat/cqerl"},
       {:re2, git: "https://github.com/tuncer/re2.git", tag: "v1.7.2", override: true},
