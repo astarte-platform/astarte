8aa263a1ab4e48e555bb10a48618e8c1334e9d38
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-28 18:15:45 +0200

    mix.exs: add test configuration

--- apps/astarte_realm_management/mix.exs
+++ apps/astarte_realm_management/mix.exs
@@ -7,6 +7,8 @@ defmodule RealmManagement.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
      deps: deps()]
   end
 
