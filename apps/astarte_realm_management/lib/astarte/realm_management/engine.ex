4abe295fc515d96719aa516d57a45e3c45cc6daf
Davide Bettio <davide.bettio@ispirata.com>
2017-06-29 17:27:02 +0200

    engine: implement install_interface API call
    Implement install_interface RPC call and remove GenServer, we don't need
    it anymore because we don't need to keep any kind of state among
    different calls.

--- apps/astarte_realm_management/lib/astarte/realm_management/engine.ex
+++ apps/astarte_realm_management/lib/astarte/realm_management/engine.ex
@@ -1,36 +1,28 @@
 defmodule Astarte.RealmManagement.Engine do
-  use GenServer
   require Logger
-
-  @timeout 10000
-
-  def start_link do
-    GenServer.start_link(__MODULE__, [], name: :astarte_realm_management_engine)
-  end
-
-  def init(_opts) do
-    CQEx.Client.new()
-  end
+  alias CQEx.Client, as: DatabaseClient
 
   def process_rpc(message) do
-    GenServer.call(:astarte_realm_management_engine, {:process_rpc, message}, @timeout)
+    case Astarte.RPC.Protocol.RealmManagement.Call.decode(message) do
+      %Astarte.RPC.Protocol.RealmManagement.Call{call: call_tuple} when call_tuple != nil ->
+        case call_tuple do
+          {:install_interface, %Astarte.RPC.Protocol.RealmManagement.InstallInterface{realm_name: realm_name, interface_json: interface_json}} ->
+          install_interface(realm_name, interface_json)
+        _ ->
+          Logger.warn "Received unexpected call: " <> inspect call_tuple
+          {:error, :unexpected_call}
+        end
+      _ ->
+        Logger.warn "Received unexpected message: " <> inspect message
+        {:error, :unexpected_messsage}
+    end
   end
 
-  def handle_call({:process_rpc, message}, _from, client) do
-    reply =
-      case Astarte.RPC.Protocol.RealmManagement.Call.decode(message) do
-        %Astarte.RPC.Protocol.RealmManagement.Call{call: call_tuple} when call_tuple != nil ->
-          case call_tuple do
-            {:install_interface, %Astarte.RPC.Protocol.RealmManagement.InstallInterface{interface_json: interface_json}} ->
-              Astarte.RealmManagement.Queries.install_new_interface(client, interface_json)
-          _ ->
-            Logger.warn "Received unexpected call: " <> inspect call_tuple
-            {:error, :unexpected_call}
-          end
-        _ ->
-          Logger.warn "Received unexpected message: " <> inspect message
-          {:error, :unexpected_messsage}
-      end
-    {:reply, reply, client}
+  def install_interface(realm_name, interface_json) do
+    interface_document = Astarte.Core.InterfaceDocument.from_json(interface_json)
+
+    DatabaseClient.new!(List.first(Application.get_env(:cqerl, :cassandra_nodes)), [keyspace: realm_name])
+    |> Astarte.RealmManagement.Queries.install_new_interface(interface_document)
   end
+
 end
