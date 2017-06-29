69efecf34c9a200f7ff2fb065b59f991d619df5c
Davide Bettio <davide.bettio@ispirata.com>
2017-06-29 11:55:33 +0200

    Add config files
    Add config/{dev,prod,test}.exs.

--- apps/astarte_realm_management/mix.exs
+++ apps/astarte_realm_management/mix.exs
@@ -2,7 +2,7 @@ defmodule RealmManagement.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :realm_management,
+    [app: :astarte_realm_management,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
