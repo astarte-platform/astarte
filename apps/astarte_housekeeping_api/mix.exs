f194ce4f03516a96e1765350eab95625aa345924
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-07-06 15:06:43 +0200

    Add distillery dependency

--- apps/astarte_housekeeping_api/mix.exs
+++ apps/astarte_housekeeping_api/mix.exs
@@ -27,9 +27,13 @@ defmodule Astarte.Housekeeping.API.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+
+      {:distillery, "~> 1.4", runtime: false},
+    ]
   end
 end
