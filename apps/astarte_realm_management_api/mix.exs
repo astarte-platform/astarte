93fbb8d9b491efac6710ee9eba8785db909f8849
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-07-06 11:23:39 +0200

    Add distillery dependency

--- apps/astarte_realm_management_api/mix.exs
+++ apps/astarte_realm_management_api/mix.exs
@@ -31,9 +31,13 @@ defmodule Astarte.RealmManagement.API.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
+    [
+     {:phoenix, "~> 1.3.0-rc"},
      {:phoenix_pubsub, "~> 1.0"},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+
+     {:distillery, "~> 1.4", runtime: false}
+    ]
   end
 end
