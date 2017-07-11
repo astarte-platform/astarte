1f6e8cf09ffcccf049891dfefe293ec26430f7bc
Davide Bettio <davide.bettio@ispirata.com>
2017-07-11 17:14:48 +0200

    web/controllers/interface_controller: Add missing Astarte. prefix
    RelmManagement -> Astarte.RealmManagement

--- apps/astarte_realm_management_api/lib/astarte_realm_management_api/web/controllers/interface_controller.ex
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api/web/controllers/interface_controller.ex
@@ -7,12 +7,12 @@ defmodule Astarte.RealmManagement.API.Web.InterfaceController do
   action_fallback Astarte.RealmManagement.API.Web.FallbackController
 
   def index(conn, _params) do
-    interfaces = RealmManagement.API.Interfaces.list_interfaces()
+    interfaces = Astarte.RealmManagement.API.Interfaces.list_interfaces()
     render(conn, "index.json", interfaces: interfaces)
   end
 
   def create(conn, %{"interface" => interface_params}) do
-    with {:ok, %Interface{} = interface} <- RealmManagement.API.Interfaces.create_interface(interface_params) do
+    with {:ok, %Interface{} = interface} <- Astarte.RealmManagement.API.Interfaces.create_interface(interface_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", interface_path(conn, :show, interface))
@@ -21,21 +21,21 @@ defmodule Astarte.RealmManagement.API.Web.InterfaceController do
   end
 
   def show(conn, %{"id" => id}) do
-    interface = RealmManagement.API.Interfaces.get_interface!(id)
+    interface = Astarte.RealmManagement.API.Interfaces.get_interface!(id)
     render(conn, "show.json", interface: interface)
   end
 
   def update(conn, %{"id" => id, "interface" => interface_params}) do
-    interface = RealmManagement.API.Interfaces.get_interface!(id)
+    interface = Astarte.RealmManagement.API.Interfaces.get_interface!(id)
 
-    with {:ok, %Interface{} = interface} <- RealmManagement.API.Interfaces.update_interface(interface, interface_params) do
+    with {:ok, %Interface{} = interface} <- Astarte.RealmManagement.API.Interfaces.update_interface(interface, interface_params) do
       render(conn, "show.json", interface: interface)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    interface = RealmManagement.API.Interfaces.get_interface!(id)
-    with {:ok, %Interface{}} <- RealmManagement.API.Interfaces.delete_interface(interface) do
+    interface = Astarte.RealmManagement.API.Interfaces.get_interface!(id)
+    with {:ok, %Interface{}} <- Astarte.RealmManagement.API.Interfaces.delete_interface(interface) do
       send_resp(conn, :no_content, "")
     end
   end
