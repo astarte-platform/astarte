c2e4b579d8ad1eb1f99f3920d9573761606cf4d0
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-07-12 18:08:47 +0200

    amqp_server: handle DoesRealmExist call

--- apps/astarte_housekeeping/lib/astarte_housekeeping/rpc/amqp_server.ex
+++ apps/astarte_housekeeping/lib/astarte_housekeeping/rpc/amqp_server.ex
@@ -2,7 +2,7 @@ defmodule Astarte.Housekeeping.RPC.AMQPServer do
   use Astarte.RPC.AMQPServer,
     queue: Application.fetch_env!(:astarte_housekeeping, :rpc_queue),
     amqp_options: Application.get_env(:astarte_housekeeping, :amqp_connection, [])
-  alias Astarte.RPC.Protocol.Housekeeping.{Call,CreateRealm}
+  use Astarte.RPC.Protocol.Housekeeping
 
   def process_rpc(payload) do
     process_decoded_call(Call.decode(payload))
@@ -24,5 +24,22 @@ defmodule Astarte.Housekeeping.RPC.AMQPServer do
 
   defp process_call_tuple({:create_realm, %CreateRealm{realm: realm}}) do
     Astarte.Housekeeping.Engine.create_realm(realm)
+  end
+
+  defp process_call_tuple({:does_realm_exist, %DoesRealmExist{realm: realm}}) do
+    exists = Astarte.Housekeeping.Engine.realm_exists?(realm)
+
+    %DoesRealmExistReply{exists: exists}
+    |> encode_reply(:does_realm_exist_reply)
+    |> ok_wrap
+  end
+
+  defp encode_reply(reply, reply_type) do
+    %Reply{reply: {reply_type, reply}}
+    |> Reply.encode
+  end
+
+  defp ok_wrap(result) do
+    {:ok, result}
   end
 end
