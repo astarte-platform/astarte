6a702b26c1e727ed2fadd35ec50c92b1f220677b
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-07-17 19:14:30 +0200

    realm_controller: fix context name
    Generator fail

--- apps/astarte_housekeeping_api/lib/astarte_housekeeping_api/web/controllers/realm_controller.ex
+++ apps/astarte_housekeeping_api/lib/astarte_housekeeping_api/web/controllers/realm_controller.ex
@@ -7,12 +7,12 @@ defmodule Astarte.Housekeeping.API.Web.RealmController do
   action_fallback Astarte.Housekeeping.API.Web.FallbackController
 
   def index(conn, _params) do
-    realms = Housekeeping.API.Realms.list_realms()
+    realms = Realms.list_realms()
     render(conn, "index.json", realms: realms)
   end
 
   def create(conn, %{"realm" => realm_params}) do
-    with {:ok, %Realm{} = realm} <- Housekeeping.API.Realms.create_realm(realm_params) do
+    with {:ok, %Realm{} = realm} <- Realms.create_realm(realm_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", realm_path(conn, :show, realm))
@@ -21,21 +21,21 @@ defmodule Astarte.Housekeeping.API.Web.RealmController do
   end
 
   def show(conn, %{"id" => id}) do
-    realm = Housekeeping.API.Realms.get_realm!(id)
+    realm = Realms.get_realm!(id)
     render(conn, "show.json", realm: realm)
   end
 
   def update(conn, %{"id" => id, "realm" => realm_params}) do
-    realm = Housekeeping.API.Realms.get_realm!(id)
+    realm = Realms.get_realm!(id)
 
-    with {:ok, %Realm{} = realm} <- Housekeeping.API.Realms.update_realm(realm, realm_params) do
+    with {:ok, %Realm{} = realm} <- Realms.update_realm(realm, realm_params) do
       render(conn, "show.json", realm: realm)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    realm = Housekeeping.API.Realms.get_realm!(id)
-    with {:ok, %Realm{}} <- Housekeeping.API.Realms.delete_realm(realm) do
+    realm = Realms.get_realm!(id)
+    with {:ok, %Realm{}} <- Realms.delete_realm(realm) do
       send_resp(conn, :no_content, "")
     end
   end
