0d87e1b129248ff899f44759ed5f2f76ecb496ce
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-07-18 18:10:16 +0200

    realm: use realm_name instead of name
    Reflect the JSON schema

--- apps/astarte_housekeeping_api/lib/astarte_housekeeping_api/realms/realm.ex
+++ apps/astarte_housekeeping_api/lib/astarte_housekeeping_api/realms/realm.ex
@@ -2,16 +2,16 @@ defmodule Astarte.Housekeeping.API.Realms.Realm do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @required_fields [:name]
+  @required_fields [:realm_name]
 
   embedded_schema do
-    field :name
+    field :realm_name
   end
 
   def changeset(realm, params \\ %{}) do
     realm
     |> cast(params, @required_fields)
     |> validate_required(@required_fields)
-    |> validate_format(:name, ~r/^[a-z][a-z0-9]*$/)
+    |> validate_format(:realm_name, ~r/^[a-z][a-z0-9]*$/)
   end
 end
