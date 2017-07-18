9ee1940843daa738f0a30e6ccb2ca4204d09995d
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-07-18 16:22:21 +0200

    amqp_client: add create_realm call

--- apps/astarte_housekeeping_api/lib/astarte_housekeeping_api/realms/rpc/amqp_client.ex
+++ apps/astarte_housekeeping_api/lib/astarte_housekeeping_api/realms/rpc/amqp_client.ex
@@ -4,5 +4,16 @@ defmodule Astarte.Housekeeping.API.Realms.RPC.AMQPClient do
     amqp_options: Application.get_env(:astarte_housekeeping_api, :amqp_connection, [])
 
   use Astarte.RPC.Protocol.Housekeeping
+  alias Astarte.Housekeeping.API.Realms.Realm
 
+  def create_realm(%Realm{name: realm_name}) do
+    %CreateRealm{realm: realm_name}
+    |> encode_call(:create_realm)
+    |> rpc_call()
+  end
+
+  defp encode_call(call = %CreateRealm{}, :create_realm) do
+    %Call{call: {:create_realm, call}}
+    |> Call.encode()
+  end
 end
