49b46095d9873200da9250456ef9b9310b6e9faa
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-07-19 16:26:15 +0200

    data_case: start the mocks

--- apps/astarte_housekeeping_api/test/support/data_case.ex
+++ apps/astarte_housekeeping_api/test/support/data_case.ex
@@ -1,3 +1,9 @@
 defmodule Astarte.Housekeeping.API.DataCase do
   use ExUnit.CaseTemplate
+
+  setup_all do
+    Astarte.Housekeeping.Mock.start_link()
+    Astarte.Housekeeping.Mock.DB.start_link()
+    :ok
+  end
 end
