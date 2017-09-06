20a16e92487a2b6a1261715b07afe9d0d3513e08
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-06 17:43:50 +0200

    Move astarte_realm_management_api.ex to...
    ...astarte_realm_management_api/application.ex

--- apps/astarte_realm_management_api/lib/astarte_realm_management_api/application.ex
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api/application.ex
@@ -1,4 +1,4 @@
-defmodule Astarte.RealmManagement.API do
+defmodule Astarte.RealmManagement.API.Application do
   use Application
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
