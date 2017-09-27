b4a0b9bb1e349bd0a10c8c3b2f678fadc47e8053
Davide Bettio <davide.bettio@ispirata.com>
2017-09-27 16:56:38 +0200

    tests: implement tests for Device.get_interface_values and Device.list_interfaces

--- apps/astarte_appengine_api/test/astarte_appengine_api_web/controllers/interface_values_controller_test.exs
+++ apps/astarte_appengine_api/test/astarte_appengine_api_web/controllers/interface_values_controller_test.exs
@@ -1,77 +1,28 @@
 defmodule AstarteAppengineApiWeb.InterfaceValuesControllerTest do
   use AstarteAppengineApiWeb.ConnCase
 
-  alias AstarteAppengineApi.Device
-  alias AstarteAppengineApi.Device.InterfaceValues
-
-  @create_attrs %{}
-  @update_attrs %{}
-  @invalid_attrs %{}
-
-  def fixture(:interface_values) do
-    {:ok, interface_values} = Device.create_interface_values(@create_attrs)
-    interface_values
-  end
-
   setup %{conn: conn} do
+    {:ok, _client} = Astarte.RealmManagement.DatabaseTestHelper.create_test_keyspace()
+
+    on_exit fn ->
+      Astarte.RealmManagement.DatabaseTestHelper.destroy_local_test_keyspace()
+    end
+
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
   describe "index" do
     test "lists all interfaces", %{conn: conn} do
-      conn = get conn, interface_values_path(conn, :index)
-      assert json_response(conn, 200)["data"] == []
-    end
-  end
-
-  describe "create interface_values" do
-    test "renders interface_values when data is valid", %{conn: conn} do
-      conn = post conn, interface_values_path(conn, :create), interface_values: @create_attrs
-      assert %{"id" => id} = json_response(conn, 201)["data"]
-
-      conn = get conn, interface_values_path(conn, :show, id)
-      assert json_response(conn, 200)["data"] == %{
-        "id" => id}
+      conn = get conn, interface_values_path(conn, :index, "autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ")
+      assert json_response(conn, 200)["data"] == ["com.test.LCDMonitor"]
     end
 
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post conn, interface_values_path(conn, :create), interface_values: @invalid_attrs
-      assert json_response(conn, 422)["errors"] != %{}
+    test "get interface values", %{conn: conn} do
+      conn = get conn, interface_values_path(conn, :show, "autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor")
+      assert json_response(conn, 200)["data"] == %{"time" => %{"from" => 8, "to" => 20}}
+
+      conn = get conn, interface_values_path(conn, :show, "autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor", %{ "path" =>"time/to"})
+      assert json_response(conn, 200)["data"] == 20
     end
-  end
-
-  describe "update interface_values" do
-    setup [:create_interface_values]
-
-    test "renders interface_values when data is valid", %{conn: conn, interface_values: %InterfaceValues{id: id} = interface_values} do
-      conn = put conn, interface_values_path(conn, :update, interface_values), interface_values: @update_attrs
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-      conn = get conn, interface_values_path(conn, :show, id)
-      assert json_response(conn, 200)["data"] == %{
-        "id" => id}
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, interface_values: interface_values} do
-      conn = put conn, interface_values_path(conn, :update, interface_values), interface_values: @invalid_attrs
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "delete interface_values" do
-    setup [:create_interface_values]
-
-    test "deletes chosen interface_values", %{conn: conn, interface_values: interface_values} do
-      conn = delete conn, interface_values_path(conn, :delete, interface_values)
-      assert response(conn, 204)
-      assert_error_sent 404, fn ->
-        get conn, interface_values_path(conn, :show, interface_values)
-      end
-    end
-  end
-
-  defp create_interface_values(_) do
-    interface_values = fixture(:interface_values)
-    {:ok, interface_values: interface_values}
   end
 end
