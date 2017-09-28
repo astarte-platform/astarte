952d21ff4c536f574c3c04943edce34fa267cb24
Davide Bettio <davide.bettio@ispirata.com>
2017-09-28 17:12:21 +0200

    test: add more tests and update existing tests
    Test exceptions and update expected returned values.

--- apps/astarte_appengine_api/test/astarte_appengine_api/device/device_test.exs
+++ apps/astarte_appengine_api/test/astarte_appengine_api/device/device_test.exs
@@ -1,6 +1,10 @@
 defmodule Astarte.AppEngine.API.DeviceTest do
   use ExUnit.Case
   alias Astarte.AppEngine.API.Device
+  alias Astarte.AppEngine.API.Device.DeviceNotFoundError
+  alias Astarte.AppEngine.API.Device.EndpointNotFoundError
+  alias Astarte.AppEngine.API.Device.InterfaceNotFoundError
+  alias Astarte.AppEngine.API.Device.PathNotFoundError
 
   setup do
     {:ok, _client} = Astarte.RealmManagement.DatabaseTestHelper.create_test_keyspace()
@@ -15,8 +19,26 @@ defmodule Astarte.AppEngine.API.DeviceTest do
   end
 
   test "get_interface_values! returns interfaces values" do
-    assert Device.get_interface_values!("autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor") ==  %{"time" => %{"from" => 8, "to" => 20}}
+    expected_reply = %{"time" => %{"from" => 8, "to" => 20}, "lcdCommand" => "SWITCH_ON", "weekSchedule" => %{"2" => %{"start" => 12, "stop" => 15}, "3" => %{"start" => 15, "stop" => 16}, "4" => %{"start" => 16, "stop" => 18}}}
+    assert Device.get_interface_values!("autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor") == expected_reply
+
     assert Device.get_interface_values!("autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor", "time") ==  %{"from" => 8, "to" => 20}
     assert Device.get_interface_values!("autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor", "time/from") ==  8
+
+    assert_raise DeviceNotFoundError, fn ->
+      Device.get_interface_values!("autotestrealm", "g0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor", "time/from")
+    end
+
+    assert_raise InterfaceNotFoundError, fn ->
+      Device.get_interface_values!("autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.Missing", "weekSchedule/3/start")
+    end
+
+    assert_raise EndpointNotFoundError, fn ->
+      Device.get_interface_values!("autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor", "time/missing")
+    end
+
+    assert_raise PathNotFoundError, fn ->
+      Device.get_interface_values!("autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor", "weekSchedule/9/start")
+    end
   end
 end
--- apps/astarte_appengine_api/test/astarte_appengine_api_web/controllers/interface_values_controller_test.exs
+++ apps/astarte_appengine_api/test/astarte_appengine_api_web/controllers/interface_values_controller_test.exs
@@ -18,8 +18,9 @@ defmodule Astarte.AppEngine.APIWeb.InterfaceValuesControllerTest do
     end
 
     test "get interface values", %{conn: conn} do
+      expected_reply = %{"time" => %{"from" => 8, "to" => 20}, "lcdCommand" => "SWITCH_ON", "weekSchedule" => %{"2" => %{"start" => 12, "stop" => 15}, "3" => %{"start" => 15, "stop" => 16}, "4" => %{"start" => 16, "stop" => 18}}}
       conn = get conn, interface_values_path(conn, :show, "autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor")
-      assert json_response(conn, 200)["data"] == %{"time" => %{"from" => 8, "to" => 20}}
+      assert json_response(conn, 200)["data"] == expected_reply
 
       conn = get conn, "/v1/autotestrealm/devices/f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ/interfaces/com.test.LCDMonitor/time/to"
       assert json_response(conn, 200)["data"] == 20
