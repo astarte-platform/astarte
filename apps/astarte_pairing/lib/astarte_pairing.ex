5346cbf6ea69f04d2f4fe260e088ae6f36d7f537
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-02 11:59:55 +0200

    astarte_pairing: remove boilerplate hello world

--- apps/astarte_pairing/lib/astarte_pairing.ex
+++ apps/astarte_pairing/lib/astarte_pairing.ex
@@ -2,17 +2,4 @@ defmodule Astarte.Pairing do
   @moduledoc """
   Documentation for Astarte.Pairing.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Astarte.Pairing.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
