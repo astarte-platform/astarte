1854362cded5c64d659c00da0684a75addcb1fe5
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-02 17:14:18 +0200

    engine: add get_info function

--- apps/astarte_pairing/lib/astarte_pairing/engine.ex
+++ apps/astarte_pairing/lib/astarte_pairing/engine.ex
@@ -2,4 +2,13 @@ defmodule Astarte.Pairing.Engine do
   @moduledoc """
   This module performs the pairing operations requested via RPC.
   """
+
+  alias Astarte.Pairing.Config
+
+  @version Mix.Project.config[:version]
+
+  def get_info do
+    %{version: @version,
+      url: Config.broker_url!()}
+  end
 end
