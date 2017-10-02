30d234787ee75f5b78a7eaeb271763196f0220cd
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-02 17:06:00 +0200

    config: add rpc_queue! function

--- apps/astarte_pairing/lib/astarte_pairing/config.ex
+++ apps/astarte_pairing/lib/astarte_pairing/config.ex
@@ -2,4 +2,13 @@ defmodule Astarte.Pairing.Config do
   @moduledoc """
   This module helps the access to the runtime configuration of Astarte Pairing
   """
+
+  @doc """
+  Returns the rpc_queue contained in the config.
+
+  Raises if it doesn't exist since it's required.
+  """
+  def rpc_queue! do
+    Application.fetch_env!(:astarte_pairing, :rpc_queue)
+  end
 end
