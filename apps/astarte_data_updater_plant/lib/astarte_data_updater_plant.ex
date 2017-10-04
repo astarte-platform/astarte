b46ea620ad166b68b297b41d95a7437c192e9257
Davide Bettio <davide.bettio@ispirata.com>
2017-10-04 16:51:59 +0200

    mix new astarte_data_updater_plant --module Astarte.DataUpdaterPlant --sup

---
+++ apps/astarte_data_updater_plant/lib/astarte_data_updater_plant.ex
@@ -0,0 +1,18 @@
+defmodule Astarte.DataUpdaterPlant do
+  @moduledoc """
+  Documentation for Astarte.DataUpdaterPlant.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Astarte.DataUpdaterPlant.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/astarte_data_updater_plant/test/astarte_data_updater_plant_test.exs
@@ -0,0 +1,8 @@
+defmodule Astarte.DataUpdaterPlantTest do
+  use ExUnit.Case
+  doctest Astarte.DataUpdaterPlant
+
+  test "greets the world" do
+    assert Astarte.DataUpdaterPlant.hello() == :world
+  end
+end
