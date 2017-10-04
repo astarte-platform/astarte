ff855f393c3316e3da1a5d1f2e21555dde7a9807
Davide Bettio <davide.bettio@ispirata.com>
2017-10-04 17:00:23 +0200

    Fix mix.exs + add coveralls

--- apps/astarte_data_updater_plant/mix.exs
+++ apps/astarte_data_updater_plant/mix.exs
@@ -1,3 +1,21 @@
+#
+# Copyright (C) 2017 Ispirata Srl
+#
+# This file is part of Astarte.
+# Astarte is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Astarte is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 defmodule Astarte.DataUpdaterPlant.Mixfile do
   use Mix.Project
 
@@ -5,17 +23,15 @@ defmodule Astarte.DataUpdaterPlant.Mixfile do
     [
       app: :astarte_data_updater_plant,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
       elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -23,12 +39,10 @@ defmodule Astarte.DataUpdaterPlant.Mixfile do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true},
+      {:distillery, "~> 1.4", runtime: false},
+      {:excoveralls, "~> 0.6", only: :test},
     ]
   end
 end
