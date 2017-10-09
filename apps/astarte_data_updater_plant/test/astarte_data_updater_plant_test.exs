94a67b538ceab7b6e173a7948562cebd95bbdc88
Davide Bettio <davide.bettio@ispirata.com>
2017-10-09 15:39:12 +0200

    Project cleanup: remove example code

--- apps/astarte_data_updater_plant/test/astarte_data_updater_plant_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Astarte.DataUpdaterPlantTest do
-  use ExUnit.Case
-  doctest Astarte.DataUpdaterPlant
-
-  test "greets the world" do
-    assert Astarte.DataUpdaterPlant.hello() == :world
-  end
-end
