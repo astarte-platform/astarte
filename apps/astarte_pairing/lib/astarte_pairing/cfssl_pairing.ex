21ee9305a6e95affa776be68d78b8e47d4213104
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-13 16:41:49 +0200

    cfssl_pairing: implement pairing

--- apps/astarte_pairing/lib/astarte_pairing/cfssl_pairing.ex
+++ apps/astarte_pairing/lib/astarte_pairing/cfssl_pairing.ex
@@ -2,4 +2,33 @@ defmodule Astarte.Pairing.CFSSLPairing do
   @moduledoc """
   Module implementing pairing using CFSSL
   """
+
+  alias Astarte.Pairing.Config
+  alias CFXXL.CertUtils
+  alias CFXXL.Client
+  alias CFXXL.Subject
+
+  @doc """
+  Perform the pairing for the device
+  """
+  def pair(csr, realm, extended_id) do
+    device_common_name = "#{realm}/#{extended_id}"
+    subject = %Subject{CN: device_common_name}
+
+    {:ok, %{"certificate" => cert}} =
+      Client.new(Config.cfssl_url())
+      |> CFXXL.sign(csr, subject: subject, profile: "device")
+
+    aki = CertUtils.authority_key_identifier!(cert)
+    serial = CertUtils.serial_number!(cert)
+    cn = CertUtils.common_name!(cert)
+
+    if cn == device_common_name do
+      {:ok, %{cert: cert,
+              aki: aki,
+              serial: serial}}
+    else
+      {:error, :invalid_common_name}
+    end
+  end
 end
