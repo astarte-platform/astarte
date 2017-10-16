de6391573a9ac434a302d043f686e41289d2461c
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-16 11:19:18 +0200

    cfssl_pairing: add revoke function
    Alway succeeds even if the revoke fails

--- apps/astarte_pairing/lib/astarte_pairing/cfssl_pairing.ex
+++ apps/astarte_pairing/lib/astarte_pairing/cfssl_pairing.ex
@@ -8,6 +8,8 @@ defmodule Astarte.Pairing.CFSSLPairing do
   alias CFXXL.Client
   alias CFXXL.Subject
 
+  require Logger
+
   @doc """
   Perform the pairing for the device
   """
@@ -29,6 +31,21 @@ defmodule Astarte.Pairing.CFSSLPairing do
               serial: serial}}
     else
       {:error, :invalid_common_name}
+    end
+  end
+
+  # If it was not present in the DB, no need to revoke it
+  def revoke(:null, :null), do: :ok
+  def revoke(serial, aki) do
+    client = Client.new(Config.cfssl_url())
+
+    case CFXXL.revoke(client, serial, aki, "superseded") do
+      # Don't fail even if we couldn't revoke, just warn
+      {:error, reason} ->
+        Logger.warn("Failed to revoke certificate with serial #{serial} and AKI #{aki}: #{reason}")
+        :ok
+
+      :ok -> :ok
     end
   end
 end
