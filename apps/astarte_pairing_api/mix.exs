6e1962a042e15000d369592675c49bae5460c0b4
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-18 18:19:50 +0200

    Add ecto dep
    The project was created with --no-ecto because we don't want a repo

--- apps/astarte_pairing_api/mix.exs
+++ apps/astarte_pairing_api/mix.exs
@@ -35,7 +35,8 @@ defmodule Astarte.Pairing.API.Mixfile do
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:ecto, "~> 2.1"}
     ]
   end
 end
