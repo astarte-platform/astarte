71b42eb869e9f396d106a9831033504043630171
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-19 14:59:31 +0200

    config: add rpc_queue function

--- apps/astarte_pairing_api/lib/astarte_pairing_api/config.ex
+++ apps/astarte_pairing_api/lib/astarte_pairing_api/config.ex
@@ -2,4 +2,11 @@ defmodule Astarte.Pairing.API.Config do
   @moduledoc """
   This module contains functions to access the configuration
   """
+
+  @doc """
+  Returns the AMQP queue for the RPC
+  """
+  def rpc_queue do
+    Application.get_env(:astarte_pairing_api, :rpc_queue)
+  end
 end
