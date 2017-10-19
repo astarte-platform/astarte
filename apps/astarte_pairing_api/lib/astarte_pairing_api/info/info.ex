cf8c23f6d864dad7ac174f87480866ca1718eba1
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-19 16:32:55 +0200

    info: remove unused functions

--- apps/astarte_pairing_api/lib/astarte_pairing_api/info/info.ex
+++ apps/astarte_pairing_api/lib/astarte_pairing_api/info/info.ex
@@ -3,26 +3,10 @@ defmodule Astarte.Pairing.API.Info do
   The Info context.
   """
 
-  import Ecto.Query, warn: false
-  alias Astarte.Pairing.API.Repo
-
   alias Astarte.Pairing.API.Info.BrokerInfo
 
   @doc """
-  Returns the list of broker_info.
-
-  ## Examples
-
-      iex> list_broker_info()
-      [%BrokerInfo{}, ...]
-
-  """
-  def list_broker_info do
-    raise "TODO"
-  end
-
-  @doc """
-  Gets a single broker_info.
+  Gets broker_info.
 
   Raises if the Broker info does not exist.
 
@@ -33,65 +17,4 @@ defmodule Astarte.Pairing.API.Info do
 
   """
   def get_broker_info!(id), do: raise "TODO"
-
-  @doc """
-  Creates a broker_info.
-
-  ## Examples
-
-      iex> create_broker_info(%{field: value})
-      {:ok, %BrokerInfo{}}
-
-      iex> create_broker_info(%{field: bad_value})
-      {:error, ...}
-
-  """
-  def create_broker_info(attrs \\ %{}) do
-    raise "TODO"
-  end
-
-  @doc """
-  Updates a broker_info.
-
-  ## Examples
-
-      iex> update_broker_info(broker_info, %{field: new_value})
-      {:ok, %BrokerInfo{}}
-
-      iex> update_broker_info(broker_info, %{field: bad_value})
-      {:error, ...}
-
-  """
-  def update_broker_info(%BrokerInfo{} = broker_info, attrs) do
-    raise "TODO"
-  end
-
-  @doc """
-  Deletes a BrokerInfo.
-
-  ## Examples
-
-      iex> delete_broker_info(broker_info)
-      {:ok, %BrokerInfo{}}
-
-      iex> delete_broker_info(broker_info)
-      {:error, ...}
-
-  """
-  def delete_broker_info(%BrokerInfo{} = broker_info) do
-    raise "TODO"
-  end
-
-  @doc """
-  Returns a datastructure for tracking broker_info changes.
-
-  ## Examples
-
-      iex> change_broker_info(broker_info)
-      %Todo{...}
-
-  """
-  def change_broker_info(%BrokerInfo{} = broker_info) do
-    raise "TODO"
-  end
 end
