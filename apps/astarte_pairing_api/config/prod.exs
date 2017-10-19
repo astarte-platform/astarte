6b67044d69076dcea361efe8257162059ed166ae
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-19 10:15:59 +0200

    config/prod: don't import production secret
    We don't use it for now and the CI has to be able to build releases

--- apps/astarte_pairing_api/config/prod.exs
+++ apps/astarte_pairing_api/config/prod.exs
@@ -61,4 +61,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
