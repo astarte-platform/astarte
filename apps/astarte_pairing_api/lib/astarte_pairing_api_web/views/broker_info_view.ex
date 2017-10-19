d3e8238b6135dc63c1c43f814027c63683cb9e2c
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-19 16:33:56 +0200

    broker_info_view: remove unused render_many and implement view

--- apps/astarte_pairing_api/lib/astarte_pairing_api_web/views/broker_info_view.ex
+++ apps/astarte_pairing_api/lib/astarte_pairing_api_web/views/broker_info_view.ex
@@ -2,15 +2,12 @@ defmodule Astarte.Pairing.APIWeb.BrokerInfoView do
   use Astarte.Pairing.APIWeb, :view
   alias Astarte.Pairing.APIWeb.BrokerInfoView
 
-  def render("index.json", %{broker_info: broker_info}) do
-    %{data: render_many(broker_info, BrokerInfoView, "broker_info.json")}
-  end
-
   def render("show.json", %{broker_info: broker_info}) do
     %{data: render_one(broker_info, BrokerInfoView, "broker_info.json")}
   end
 
   def render("broker_info.json", %{broker_info: broker_info}) do
-    %{id: broker_info.id}
+    %{url: broker_info.url,
+      version: broker_info.version}
   end
 end
