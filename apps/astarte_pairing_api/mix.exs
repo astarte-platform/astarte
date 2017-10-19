415772ec8d653848a6e15fbe3fab61f86de014ff
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-19 15:47:38 +0200

    Bump minimum Elixir version to 1.5
    We're using child_spec

--- apps/astarte_pairing_api/mix.exs
+++ apps/astarte_pairing_api/mix.exs
@@ -5,7 +5,7 @@ defmodule Astarte.Pairing.API.Mixfile do
     [
       app: :astarte_pairing_api,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
