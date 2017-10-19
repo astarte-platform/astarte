f8fc04048d2e9048406513a148d34c7a93d4007d
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-19 17:25:47 +0200

    broker_info_controller: call get_info! without arguments

--- apps/astarte_pairing_api/lib/astarte_pairing_api_web/controllers/broker_info_controller.ex
+++ apps/astarte_pairing_api/lib/astarte_pairing_api_web/controllers/broker_info_controller.ex
@@ -2,12 +2,11 @@ defmodule Astarte.Pairing.APIWeb.BrokerInfoController do
   use Astarte.Pairing.APIWeb, :controller
 
   alias Astarte.Pairing.API.Info
-  alias Astarte.Pairing.API.Info.BrokerInfo
 
   action_fallback Astarte.Pairing.APIWeb.FallbackController
 
   def show(conn, %{}) do
-    broker_info = Info.get_broker_info!(id)
+    broker_info = Info.get_broker_info!()
     render(conn, "show.json", broker_info: broker_info)
   end
 end
