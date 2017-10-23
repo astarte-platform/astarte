e0f83e20d00cf485a8edf7bc5ac81e4dd7b882c9
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-23 16:57:26 +0200

    api_key_controller: remove unused functions

--- apps/astarte_pairing_api/lib/astarte_pairing_api_web/controllers/api_key_controller.ex
+++ apps/astarte_pairing_api/lib/astarte_pairing_api_web/controllers/api_key_controller.ex
@@ -6,37 +6,12 @@ defmodule Astarte.Pairing.APIWeb.APIKeyController do
 
   action_fallback Astarte.Pairing.APIWeb.FallbackController
 
-  def index(conn, _params) do
-    api_keys = Pairing.API.Agent.list_api_keys()
-    render(conn, "index.json", api_keys: api_keys)
-  end
-
   def create(conn, %{"api_key" => api_key_params}) do
     with {:ok, %APIKey{} = api_key} <- Pairing.API.Agent.create_api_key(api_key_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", api_key_path(conn, :show, api_key))
       |> render("show.json", api_key: api_key)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    api_key = Pairing.API.Agent.get_api_key!(id)
-    render(conn, "show.json", api_key: api_key)
-  end
-
-  def update(conn, %{"id" => id, "api_key" => api_key_params}) do
-    api_key = Pairing.API.Agent.get_api_key!(id)
-
-    with {:ok, %APIKey{} = api_key} <- Pairing.API.Agent.update_api_key(api_key, api_key_params) do
-      render(conn, "show.json", api_key: api_key)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    api_key = Pairing.API.Agent.get_api_key!(id)
-    with {:ok, %APIKey{}} <- Pairing.API.Agent.delete_api_key(api_key) do
-      send_resp(conn, :no_content, "")
     end
   end
 end
