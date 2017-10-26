3c38c8c95d1fd1d39eb5bb2522335a7f9728f919
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-26 11:22:20 +0200

    Add test_jwt_producer
    Generates JWT for the tests

---
+++ apps/astarte_pairing_api/test/support/test_jwt_producer.ex
@@ -0,0 +1,22 @@
+defmodule Astarte.Pairing.API.TestJWTProducer do
+  use Guardian, otp_app: :astarte_pairing_api
+
+  alias Astarte.Pairing.API.Agent.Realm
+
+  def build_claims(claims, %Realm{realm_name: realm_name}, _opts) do
+    new_claims =
+      claims
+      |> Map.delete("sub")
+      |> Map.put("routingTopic", realm_name)
+
+    {:ok, new_claims}
+  end
+
+  def subject_for_token(%Realm{realm_name: realm_name}, _claims) do
+    {:ok, realm_name}
+  end
+
+  def resource_from_claims(claims) do
+    {:ok, %Realm{realm_name: claims["routingTopic"]}}
+  end
+end
