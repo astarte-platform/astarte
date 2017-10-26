e731a485978aff7172543483c963cef43fec1875
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-26 11:21:58 +0200

    astarte_pairing_mock: implement generate_api_key

--- apps/astarte_pairing_api/test/support/astarte_pairing_mock.ex
+++ apps/astarte_pairing_api/test/support/astarte_pairing_mock.ex
@@ -3,6 +3,7 @@ defmodule Astarte.Pairing.Mock do
 
   @test_broker_url "ssl://broker.example.com:9000"
   @test_version "1"
+  @test_api_key "testapikey"
 
   use Astarte.RPC.AMQPServer,
     queue: Config.rpc_queue(),
@@ -17,6 +18,10 @@ defmodule Astarte.Pairing.Mock do
     @test_version
   end
 
+  def api_key(realm, hw_id) do
+    @test_api_key <> realm <> hw_id
+  end
+
   def process_rpc(payload) do
     extract_call_tuple(Call.decode(payload))
     |> execute_rpc()
@@ -29,6 +34,11 @@ defmodule Astarte.Pairing.Mock do
   defp execute_rpc({:get_info, %GetInfo{}}) do
     %GetInfoReply{url: @test_broker_url, version: @test_version}
     |> encode_reply(:get_info_reply)
+    |> ok_wrap()
+  end
+  defp execute_rpc({:generate_api_key, %GenerateAPIKey{realm: realm, hw_id: hw_id}}) do
+    %GenerateAPIKeyReply{api_key: @test_api_key <> realm <> hw_id}
+    |> encode_reply(:generate_api_key_reply)
     |> ok_wrap()
   end
 
