1a637ad5e0c02d67b29f33bf244b3cbe152ed3a1
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-10-26 11:59:47 +0200

    agent: change create_api_key name to generate_api_key

--- apps/astarte_pairing_api/lib/astarte_pairing_api/agent/agent.ex
+++ apps/astarte_pairing_api/lib/astarte_pairing_api/agent/agent.ex
@@ -5,7 +5,7 @@ defmodule Astarte.Pairing.API.Agent do
 
   alias Astarte.Pairing.API.Agent.APIKey
 
-  def create_api_key(attrs \\ %{}) do
+  def generate_api_key(attrs \\ %{}) do
     raise "TODO"
   end
 end
