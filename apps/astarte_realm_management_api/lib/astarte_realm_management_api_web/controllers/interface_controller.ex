8da3759cebcbf7bb5315201bb4fa83b95fd38b0b
Davide Bettio <davide.bettio@ispirata.com>
2017-10-26 15:31:15 +0200

    Revert "InterfaceController: accept data as map when creating an interface"
    This reverts commit 8dfde5ae6a47a9b8e7d9e2f92916d1f4234fd241.

--- apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/controllers/interface_controller.ex
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/controllers/interface_controller.ex
@@ -27,16 +27,7 @@ defmodule Astarte.RealmManagement.APIWeb.InterfaceController do
     render(conn, "index.json", interfaces: interfaces)
   end
 
-  def create(conn, %{"realm_name" => realm_name, "data" => data}) do
-    interface_source =
-      cond do
-        is_bitstring(data) ->
-          data
-
-        is_map(data) ->
-          Poison.encode!(data, pretty: true)
-      end
-
+  def create(conn, %{"realm_name" => realm_name, "data" => interface_source}) do
     case Astarte.Core.InterfaceDocument.from_json(interface_source) do
       :error ->
         {:error, :invalid}
