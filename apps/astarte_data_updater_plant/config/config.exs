f7ee4a2aabeb33cbff2dbee5cd79e19386346724
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-07 17:59:33 +0100

    config: remove comments

--- apps/astarte_data_updater_plant/config/config.exs
+++ apps/astarte_data_updater_plant/config/config.exs
@@ -2,23 +2,4 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
-
-# You can configure your application as:
-#
-#     config :astarte_data_updater_plant, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:astarte_data_updater_plant, :key)
-#
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
 import_config "#{Mix.env}.exs"
