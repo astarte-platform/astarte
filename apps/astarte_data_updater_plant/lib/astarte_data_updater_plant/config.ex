63880e23ca3c7ffdfa8f62449041ba2b3ad65c6b
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-07 18:08:38 +0100

    config: add queue_name function

--- apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/config.ex
+++ apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/config.ex
@@ -2,4 +2,11 @@ defmodule Astarte.DataUpdaterPlant.Config do
   @moduledoc """
   This module handles the configuration of DataUpdaterPlant
   """
+
+  @doc """
+  Returns the AMQP queue name from which DUP consumes
+  """
+  def queue_name do
+    Application.get_env(:astarte_data_updater_plant, :queue_name)
+  end
 end
