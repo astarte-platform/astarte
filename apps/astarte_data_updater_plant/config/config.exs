2f77c17b3e33556b436646161b254495fcdac1e9
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-07 18:16:07 +0100

    config: add amqp_options

--- apps/astarte_data_updater_plant/config/config.exs
+++ apps/astarte_data_updater_plant/config/config.exs
@@ -5,4 +5,11 @@ use Mix.Config
 config :astarte_data_updater_plant, :queue_name,
   "vmq_all"
 
+config :astarte_data_updater_plant, :amqp_options,
+  host: "localhost",
+  username: "guest",
+  password: "guest",
+  virtual_host: "/",
+  port: 5672
+
 import_config "#{Mix.env}.exs"
