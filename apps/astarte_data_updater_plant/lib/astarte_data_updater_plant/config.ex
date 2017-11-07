58f9ce600f4eb61ad93e67a9927236d142ac67eb
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-07 18:16:43 +0100

    config: add amqp_options function

--- apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/config.ex
+++ apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/config.ex
@@ -4,6 +4,13 @@ defmodule Astarte.DataUpdaterPlant.Config do
   """
 
   @doc """
+  Returns the AMQP connection options
+  """
+  def amqp_options do
+    Application.get_env(:astarte_data_updater_plant, :amqp_options, [])
+  end
+
+  @doc """
   Returns the AMQP queue name from which DUP consumes
   """
   def queue_name do
