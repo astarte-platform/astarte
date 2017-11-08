d1065a7a8d460261bf9f2c4c35924449014337ac
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-08 15:41:23 +0100

    config: take rabbitmq host from system env in test env

--- apps/astarte_data_updater_plant/config/test.exs
+++ apps/astarte_data_updater_plant/config/test.exs
@@ -4,4 +4,4 @@ config :cqerl,
   cassandra_nodes: [{System.get_env("CASSANDRA_DB_HOST") || "scylladb-scylla", System.get_env("CASSANDRA_DB_PORT") || 9042}]
 
 config :astarte_data_updater_plant, :amqp_options,
-  host: "rabbitmq"
+  host: System.get_env("RABBITMQ_HOST") || "rabbitmq"
