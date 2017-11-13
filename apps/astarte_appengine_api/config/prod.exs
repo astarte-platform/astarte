873168c85d1580bb76ede9f3aacccdc7dbec8b44
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-13 16:40:09 +0100

    config: remove comments

--- apps/astarte_appengine_api/config/prod.exs
+++ apps/astarte_appengine_api/config/prod.exs
@@ -21,47 +21,6 @@ config :astarte_appengine_api, Astarte.AppEngine.APIWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :astarte_appengine_api, Astarte.AppEngine.APIWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [:inet6,
-#               port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :astarte_appengine_api, Astarte.AppEngine.APIWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :astarte_appengine_api, Astarte.AppEngine.APIWeb.Endpoint, server: true
-#
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-
 # TODO: handle secret configuration with Conform
 # import_config "prod.secret.exs"
 
