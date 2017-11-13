74a67198d871e927c2d951e0d4065340c9fbb048
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-13 17:42:30 +0100

    mqtt_client: manually generate client_id
    GenMQTT does the same thing but we want control over the client_id since
    it must not contain the / character

--- apps/astarte_appengine_api/lib/astarte_appengine_api/data_transmitter/mqtt_client.ex
+++ apps/astarte_appengine_api/lib/astarte_appengine_api/data_transmitter/mqtt_client.ex
@@ -4,10 +4,20 @@ defmodule Astarte.AppEngine.API.DataTransmitter.MQTTClient do
   @exactly_once_qos 2
 
   def start_link(opts \\ []) do
-    GenMQTT.start_link(__MODULE__, [], Keyword.put(opts, :name, __MODULE__))
+    full_opts =
+      opts
+      |> Keyword.put(:name, __MODULE__)
+      |> Keyword.put(:client, generate_client_id())
+
+    GenMQTT.start_link(__MODULE__, [], full_opts)
   end
 
   def publish(topic, payload) do
     GenMQTT.publish(__MODULE__, topic, payload, @exactly_once_qos, false)
+  end
+
+  defp generate_client_id do
+    :crypto.strong_rand_bytes(10)
+    |> Base.encode16()
   end
 end
