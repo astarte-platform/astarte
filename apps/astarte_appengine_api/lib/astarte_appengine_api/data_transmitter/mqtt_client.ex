5f21589339b2cc4b3cf431168280859b8193e3b4
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-13 15:03:13 +0100

    mqtt_client: add publish function

--- apps/astarte_appengine_api/lib/astarte_appengine_api/data_transmitter/mqtt_client.ex
+++ apps/astarte_appengine_api/lib/astarte_appengine_api/data_transmitter/mqtt_client.ex
@@ -1,7 +1,13 @@
 defmodule Astarte.AppEngine.API.DataTransmitter.MQTTClient do
   use GenMQTT
 
+  @exactly_once_qos 2
+
   def start_link(opts \\ []) do
     GenMQTT.start_link(__MODULE__, [], Keyword.put(opts, :name, __MODULE__))
+  end
+
+  def publish(topic, payload) do
+    GenMQTT.publish(__MODULE__, topic, payload, @exactly_once_qos, false)
   end
 end
