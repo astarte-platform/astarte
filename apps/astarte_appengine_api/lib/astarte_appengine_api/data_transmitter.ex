d9bdc917d7c1de1674769377d6cf7dc616bf3ebe
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-13 15:04:39 +0100

    data_transmitter: implement the functions

--- apps/astarte_appengine_api/lib/astarte_appengine_api/data_transmitter.ex
+++ apps/astarte_appengine_api/lib/astarte_appengine_api/data_transmitter.ex
@@ -3,6 +3,8 @@ defmodule Astarte.AppEngine.API.DataTransmitter do
   This module allows Astarte to push data to the devices
   """
 
+  alias Astarte.AppEngine.API.DataTransmitter.MQTTClient
+
   @doc """
   Pushes a payload on a datastream interface.
 
@@ -12,7 +14,16 @@ defmodule Astarte.AppEngine.API.DataTransmitter do
   * `metadata`: a map of metadata that is added in the BSON object inside the `m` key
   """
   def push_datastream(realm, device_id, interface, path, payload, opts \\ []) do
-    :ok
+    timestamp = Keyword.get(opts, :timestamp)
+    metadata = Keyword.get(opts, :metadata)
+
+    bson_payload =
+      make_payload_map(payload, timestamp, metadata)
+      |> Bson.encode()
+
+    topic = make_topic(realm, device_id, interface, path)
+
+    MQTTClient.publish(topic, bson_payload)
   end
 
   @doc """
@@ -24,13 +35,48 @@ defmodule Astarte.AppEngine.API.DataTransmitter do
   * `metadata`: a map of metadata that is added in the BSON object inside the `m` key
   """
   def set_property(realm, device_id, interface, path, payload, opts \\ []) do
-    :ok
+    timestamp = Keyword.get(opts, :timestamp)
+    metadata = Keyword.get(opts, :metadata)
+
+    bson_payload =
+      make_payload_map(payload, timestamp, metadata)
+      |> Bson.encode()
+
+    topic = make_topic(realm, device_id, interface, path)
+
+    MQTTClient.publish(topic, bson_payload)
   end
 
   @doc """
   Pushes an unset message on a properties interface.
   """
   def unset_property(realm, device_id, interface, path) do
-    :ok
+    topic = make_topic(realm, device_id, interface, path)
+
+    MQTTClient.publish(topic, "")
+  end
+
+  defp make_payload_map(payload, nil, nil) do
+    %{v: payload}
+  end
+
+  defp make_payload_map(payload, timestamp, nil) do
+    %{v: payload, t: timestamp}
+  end
+
+  defp make_payload_map(payload, nil, metadata) do
+    %{v: payload, m: metadata}
+  end
+
+  defp make_payload_map(payload, timestamp, metadata) do
+    %{v: payload, t: timestamp, m: metadata}
+  end
+
+  defp make_topic(realm, device_id, interface, "/" <> _rest = path_with_slash) do
+    "#{realm}/#{device_id}/#{interface}#{path_with_slash}"
+  end
+
+  defp make_topic(realm, device_id, interface, no_slash_path) do
+    "#{realm}/#{device_id}/#{interface}/#{no_slash_path}"
   end
 end
