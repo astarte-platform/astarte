138e5146f75b29071aae0d355b2f3f554a7a3fe6
Davide Bettio <davide.bettio@ispirata.com>
2017-11-15 18:17:23 +0100

    mix.exs: make umbrella optional

--- apps/astarte_trigger_engine/mix.exs
+++ apps/astarte_trigger_engine/mix.exs
@@ -5,17 +5,25 @@ defmodule Astarte.TriggerEngine.Mixfile do
     [
       app: :astarte_trigger_engine,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
       elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      deps: deps() ++ astarte_required_modules(System.get_env("ASTARTE_IN_UMBRELLA"))
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  defp astarte_required_modules("true") do
+    [
+    ]
+  end
+
+  defp astarte_required_modules(_) do
+    [
+    ]
+  end
+
   def application do
     [
       extra_applications: [:logger],
@@ -23,12 +31,8 @@ defmodule Astarte.TriggerEngine.Mixfile do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true},
     ]
   end
 end
