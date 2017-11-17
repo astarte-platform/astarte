656064c19ec96c663c1d2f2c87d2830b2e1353ec
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-17 16:15:56 +0100

    config: add amqp_producer_options
    Fallback to the consumer options if nothing is set

--- apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/config.ex
+++ apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/config.ex
@@ -11,6 +11,13 @@ defmodule Astarte.DataUpdaterPlant.Config do
   end
 
   @doc """
+  Returns the AMQP trigger producer connection options
+  """
+  def amqp_producer_options do
+    Application.get_env(:astarte_data_updater_plant, :amqp_producer_options, amqp_consumer_options())
+  end
+
+  @doc """
   Returns the AMQP queue name from which DUP consumes
   """
   def queue_name do
