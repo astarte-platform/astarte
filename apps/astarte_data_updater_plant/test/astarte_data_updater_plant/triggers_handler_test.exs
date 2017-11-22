108f46b5d89e3b47715e0806ea9269c568e217db
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-22 10:30:37 +0100

    triggers_handler_test: add test for on_path_removed

--- apps/astarte_data_updater_plant/test/astarte_data_updater_plant/triggers_handler_test.exs
+++ apps/astarte_data_updater_plant/test/astarte_data_updater_plant/triggers_handler_test.exs
@@ -6,6 +6,7 @@ defmodule Astarte.DataUpdaterPlant.TriggersHandlerTest do
   alias AMQP.Queue
   alias Astarte.Core.Triggers.SimpleEvents.IncomingDataEvent
   alias Astarte.Core.Triggers.SimpleEvents.PathCreatedEvent
+  alias Astarte.Core.Triggers.SimpleEvents.PathRemovedEvent
   alias Astarte.Core.Triggers.SimpleEvents.SimpleEvent
   alias Astarte.Core.Triggers.SimpleEvents.ValueChangeAppliedEvent
   alias Astarte.Core.Triggers.SimpleEvents.ValueChangeEvent
@@ -127,6 +128,46 @@ defmodule Astarte.DataUpdaterPlant.TriggersHandlerTest do
     assert Map.get(headers_map, "x_astarte_realm") == @realm
     assert Map.get(headers_map, "x_astarte_device_id") == @device_id
     assert Map.get(headers_map, "x_astarte_event_type") == "path_created_event"
+    assert Map.get(headers_map, static_header_key) == static_header_value
+  end
+
+  test "on_path_removed AMQPTarget handling" do
+    simple_trigger_id = :uuid.get_v4()
+    parent_trigger_id = :uuid.get_v4()
+    static_header_key = "important_metadata_path_removed"
+    static_header_value = "test_meta_path_removed"
+    static_headers = [{static_header_key, static_header_value}]
+
+    target =
+      %AMQPTriggerTarget{
+        simple_trigger_id: simple_trigger_id,
+        parent_trigger_id: parent_trigger_id,
+        static_headers: static_headers,
+        routing_key: @routing_key
+      }
+
+    TriggersHandler.on_path_removed(target, @realm, @device_id, @interface, @path)
+
+    assert_receive {:event, payload, meta}
+
+    assert %SimpleEvent{
+      device_id: @device_id,
+      parent_trigger_id: ^parent_trigger_id,
+      simple_trigger_id: ^simple_trigger_id,
+      realm: @realm,
+      event: {:path_removed_event, path_removed_event}
+    } = SimpleEvent.decode(payload)
+
+    assert %PathRemovedEvent{
+      interface: @interface,
+      path: @path,
+    } = path_removed_event
+
+    headers_map = amqp_headers_to_map(meta.headers)
+
+    assert Map.get(headers_map, "x_astarte_realm") == @realm
+    assert Map.get(headers_map, "x_astarte_device_id") == @device_id
+    assert Map.get(headers_map, "x_astarte_event_type") == "path_removed_event"
     assert Map.get(headers_map, static_header_key) == static_header_value
   end
 
