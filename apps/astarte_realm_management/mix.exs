92e62b24793d36a3c2e30108d1cfdecb4f9770c6
Dario Freddi <dario.freddi@ispirata.com>
2017-12-19 10:31:49 +0100

    Add new astarte_data_access (used for Conform only for now)

--- apps/astarte_realm_management/mix.exs
+++ apps/astarte_realm_management/mix.exs
@@ -22,14 +22,16 @@ defmodule Astarte.RealmManagement.Mixfile do
   defp astarte_required_modules("true") do
     [
       {:astarte_core, in_umbrella: true},
-      {:astarte_rpc, in_umbrella: true}
+      {:astarte_rpc, in_umbrella: true},
+      {:astarte_data_access, in_umbrella: true}
     ]
   end
 
   defp astarte_required_modules(_) do
     [
       {:astarte_core, git: "https://git.ispirata.com/Astarte-NG/astarte_core"},
-      {:astarte_rpc, git: "https://git.ispirata.com/Astarte-NG/astarte_rpc"}
+      {:astarte_rpc, git: "https://git.ispirata.com/Astarte-NG/astarte_rpc"},
+      {:astarte_data_access, git: "https://git.ispirata.com/Astarte-NG/astarte_data_access"}
     ]
   end
 
