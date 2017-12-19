d604c00c4a2d108910e4de85fe17d1ae37a63ae3
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-12-19 10:38:50 +0100

    config: make cqerl conf coherent with everything else
    Aesthetics

--- apps/astarte_housekeeping/config/test.exs
+++ apps/astarte_housekeeping/config/test.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
-config :cqerl, 
-  cassandra_nodes: [{System.get_env("CASSANDRA_DB_HOST") || "scylladb-scylla", System.get_env("CASSANDRA_DB_PORT") || 9042}]
+config :cqerl, :cassandra_nodes,
+  [{System.get_env("CASSANDRA_DB_HOST") || "scylladb-scylla", System.get_env("CASSANDRA_DB_PORT") || 9042}]
 
 config :astarte_rpc, :amqp_connection,
   host: "rabbitmq"
