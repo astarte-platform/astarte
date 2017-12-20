c97d773bca62caa5ffe0a3d9373635caaddb6649
Dario Freddi <dario.freddi@ispirata.com>
2017-12-20 12:39:55 +0100

    Add conform

--- apps/astarte_data_updater_plant/mix.exs
+++ apps/astarte_data_updater_plant/mix.exs
@@ -56,6 +56,7 @@ defmodule Astarte.DataUpdaterPlant.Mixfile do
       {:amqp, "~> 1.0.0-pre.2"},
       {:cqex, github: "ispirata/cqex"},
       {:bson, "~> 0.4.4"},
+      {:conform, "~> 2.2"},
 
       {:distillery, "~> 1.4", runtime: false},
       {:excoveralls, "~> 0.6", only: :test},
