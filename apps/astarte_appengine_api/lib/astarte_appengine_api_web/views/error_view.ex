752f22170adaa2dd3a97d49ec352e06d90f3a079
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-01-24 18:44:01 +0100

    error_view: add unauthorized error_view

--- apps/astarte_appengine_api/lib/astarte_appengine_api_web/views/error_view.ex
+++ apps/astarte_appengine_api/lib/astarte_appengine_api_web/views/error_view.ex
@@ -28,6 +28,10 @@ defmodule Astarte.AppEngine.APIWeb.ErrorView do
     %{errors: %{detail: "Internal server error"}}
   end
 
+  def render("401.json", _assigns) do
+    %{errors: %{detail: "Unauthorized"}}
+  end
+
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
