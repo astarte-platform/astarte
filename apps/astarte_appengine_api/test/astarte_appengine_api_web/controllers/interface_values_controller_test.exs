d890bdc0bc5be40c8e28e6adaaea7490145f1483
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-01-26 16:09:29 +0100

    interface_values_controller_test: do not reuse conn for multiple requests

--- apps/astarte_appengine_api/test/astarte_appengine_api_web/controllers/interface_values_controller_test.exs
+++ apps/astarte_appengine_api/test/astarte_appengine_api_web/controllers/interface_values_controller_test.exs
@@ -26,11 +26,11 @@ defmodule Astarte.AppEngine.APIWeb.InterfaceValuesControllerTest do
 
     test "get interface values", %{conn: conn} do
       expected_reply = %{"time" => %{"from" => 8, "to" => 20}, "lcdCommand" => "SWITCH_ON", "weekSchedule" => %{"2" => %{"start" => 12, "stop" => 15}, "3" => %{"start" => 15, "stop" => 16}, "4" => %{"start" => 16, "stop" => 18}}}
-      conn = get conn, interface_values_path(conn, :show, "autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor")
-      assert json_response(conn, 200)["data"] == expected_reply
+      from_path_conn = get conn, interface_values_path(conn, :show, "autotestrealm", "f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ", "com.test.LCDMonitor")
+      assert json_response(from_path_conn, 200)["data"] == expected_reply
 
-      conn = get conn, "/v1/autotestrealm/devices/f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ/interfaces/com.test.LCDMonitor/time/to"
-      assert json_response(conn, 200)["data"] == 20
+      property_conn = get conn, "/v1/autotestrealm/devices/f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ/interfaces/com.test.LCDMonitor/time/to"
+      assert json_response(property_conn, 200)["data"] == 20
 
       expected_reply = [
         %{"timestamp" => "2017-09-28T04:05:00.000Z", "value" => 0},
@@ -39,16 +39,16 @@ defmodule Astarte.AppEngine.APIWeb.InterfaceValuesControllerTest do
         %{"timestamp" => "2017-09-29T05:07:00.000Z", "value" => 3},
         %{"timestamp" => "2017-09-30T07:10:00.000Z", "value" => 4}
       ]
-      conn = get conn, "/v1/autotestrealm/devices/f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ/interfaces/com.test.SimpleStreamTest/0/value"
-      assert json_response(conn, 200)["data"] == expected_reply
+      datastream_conn = get conn, "/v1/autotestrealm/devices/f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ/interfaces/com.test.SimpleStreamTest/0/value"
+      assert json_response(datastream_conn, 200)["data"] == expected_reply
 
       expected_reply = [
         %{"string" => "aaa", "timestamp" => "2017-09-30T07:10:00.000Z", "value" => 1.1},
         %{"string" => "bbb", "timestamp" => "2017-09-30T07:12:00.000Z", "value" => 2.2},
         %{"string" => "ccc", "timestamp" => "2017-09-30T07:13:00.000Z", "value" => 3.3}
       ]
-      conn = get conn, "/v1/autotestrealm/devices/f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ/interfaces/com.example.TestObject/"
-      assert json_response(conn, 200)["data"] == expected_reply
+      object_conn = get conn, "/v1/autotestrealm/devices/f0VMRgIBAQAAAAAAAAAAAAIAPgABAAAAsCVAAAAAAABAAAAAAAAAADDEAAAAAAAAAAAAAEAAOAAJ/interfaces/com.example.TestObject/"
+      assert json_response(object_conn, 200)["data"] == expected_reply
     end
   end
 end
