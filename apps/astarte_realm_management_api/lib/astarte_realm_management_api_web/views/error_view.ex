065d0c194d4cf8b3f6635d2863bcad1e0171fef9
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-01-31 16:48:01 +0100

    error_view: add 401 and 403 views

--- apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/views/error_view.ex
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/views/error_view.ex
@@ -28,6 +28,14 @@ defmodule Astarte.RealmManagement.APIWeb.ErrorView do
     %{errors: %{detail: "Internal server error"}}
   end
 
+  def render("401.json", _assigns) do
+    %{errors: %{detail: "Unauthorized"}}
+  end
+
+  def render("403.json", _assigns) do
+    %{errors: %{detail: "Forbidden"}}
+  end
+
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
