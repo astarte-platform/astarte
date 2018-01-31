9425fbcc9d5d1943a1f4c20b39ed443bda2c0a0b
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-01-31 12:03:08 +0100

    queries: add get_jwt_public_key_pem

--- apps/astarte_realm_management/lib/astarte_realm_management/queries.ex
+++ apps/astarte_realm_management/lib/astarte_realm_management/queries.ex
@@ -22,6 +22,7 @@ defmodule Astarte.RealmManagement.Queries do
   require Logger
   alias Astarte.Core.StorageType
   alias CQEx.Query, as: DatabaseQuery
+  alias CQEx.Result, as: DatabaseResult
 
   @insert_into_interfaces """
     INSERT INTO interfaces
@@ -118,6 +119,12 @@ defmodule Astarte.RealmManagement.Queries do
 
   @query_interfaces """
     SELECT DISTINCT name FROM interfaces;
+  """
+
+  @query_jwt_public_key_pem """
+    SELECT blobAsVarchar(value)
+    FROM kv_store
+    WHERE group='auth' AND key='jwt_public_key_pem';
   """
 
   defp create_interface_table(:individual, :multi, interface_descriptor, _mappings) do
@@ -338,4 +345,18 @@ defmodule Astarte.RealmManagement.Queries do
     end
   end
 
+  def get_jwt_public_key_pem(client) do
+    query =
+      DatabaseQuery.new()
+      |> DatabaseQuery.statement(@query_jwt_public_key_pem)
+
+    with {:ok, result} <- DatabaseQuery.call(client, query),
+         ["system.blobasvarchar(value)": pem] <- DatabaseResult.head(result) do
+
+      {:ok, pem}
+    else
+      _ ->
+        {:error, :public_key_not_found}
+    end
+  end
 end
