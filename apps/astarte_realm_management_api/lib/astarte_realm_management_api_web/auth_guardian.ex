ae8d33bd1044c75fd3b5a7837267f66c44392312
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-01-31 17:45:21 +0100

    Add AuthGuardian

---
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/auth_guardian.ex
@@ -0,0 +1,18 @@
+defmodule Astarte.RealmManagement.APIWeb.AuthGuardian do
+  use Guardian, otp_app: :astarte_realm_management_api
+
+  alias Astarte.RealmManagement.API.Auth.User
+
+  def subject_for_token(%User{id: id}, _claims) do
+    {:ok, to_string(id)}
+  end
+
+  def resource_from_claims(claims) do
+    {:ok,
+      %User{
+        id: claims["sub"],
+        authorizations: Map.get(claims, "a_rma", [])
+      }
+    }
+  end
+end
