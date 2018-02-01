c65aba8e9793de64a9740aab2767c76f64e86197
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-02-01 10:53:56 +0100

    Add auth tests

---
+++ apps/astarte_realm_management_api/test/astarte_realm_management_api_web/auth/auth_test.exs
@@ -0,0 +1,83 @@
+defmodule Astarte.RealmManagement.APIWeb.AuthTest do
+  use Astarte.RealmManagement.APIWeb.ConnCase
+
+  alias Astarte.RealmManagement.API.JWTTestHelper
+
+  @realm "testrealm"
+  @request_path "/v1/#{@realm}/interfaces"
+  @valid_auth_path "^interfaces$"
+  @non_exact_match_valid_auth_path "^interf.*$"
+  @non_matching_auth_path "^stats.*$"
+
+  @expected_data []
+
+  require Logger
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "JWT" do
+    test "no token returns 401", %{conn: conn} do
+      conn = get(conn, @request_path)
+      assert json_response(conn, 401)["errors"]["detail"] == "Unauthorized"
+    end
+
+    test "all access token returns the data", %{conn: conn} do
+      conn =
+        put_req_header(conn, "authorization", "bearer #{JWTTestHelper.gen_jwt_all_access_token()}")
+        |> get(@request_path)
+
+      assert json_response(conn, 200) == @expected_data
+    end
+
+    test "valid token returns the data", %{conn: conn} do
+      conn =
+        put_req_header(conn, "authorization", "bearer #{JWTTestHelper.gen_jwt_token(["^GET$::#{@valid_auth_path}"])}")
+        |> get(@request_path)
+
+      assert json_response(conn, 200) == @expected_data
+    end
+
+    test "token for another path returns 403", %{conn: conn} do
+      conn =
+        put_req_header(conn, "authorization", "bearer #{JWTTestHelper.gen_jwt_token(["^GET$::#{@non_matching_auth_path}"])}")
+        |> get(@request_path)
+
+      assert json_response(conn, 403)["errors"]["detail"] == "Forbidden"
+    end
+
+    test "token for both paths returns the data", %{conn: conn} do
+
+      conn =
+        put_req_header(conn, "authorization", "bearer #{JWTTestHelper.gen_jwt_token(["^GET$::#{@non_matching_auth_path}", "^GET$::#{@valid_auth_path}"])}")
+        |> get(@request_path)
+
+      assert json_response(conn, 200) == @expected_data
+    end
+
+    test "token for another method returns 403", %{conn: conn} do
+      conn =
+        put_req_header(conn, "authorization", "bearer #{JWTTestHelper.gen_jwt_token(["^POST$::#{@valid_auth_path}"])}")
+        |> get(@request_path)
+
+      assert json_response(conn, 403)["errors"]["detail"] == "Forbidden"
+    end
+    
+    test "token for both methods returns the data", %{conn: conn} do
+      conn =
+        put_req_header(conn, "authorization", "bearer #{JWTTestHelper.gen_jwt_token(["^POST$::#{@valid_auth_path}", "^GET$::#{@valid_auth_path}"])}")
+        |> get(@request_path)
+
+      assert json_response(conn, 200) == @expected_data
+    end
+
+    test "token with generic matching regexp returns the data", %{conn: conn} do
+      conn =
+        put_req_header(conn, "authorization", "bearer #{JWTTestHelper.gen_jwt_token(["^.*$::#{@non_exact_match_valid_auth_path}"])}")
+        |> get(@request_path)
+
+      assert json_response(conn, 200) == @expected_data
+    end
+  end
+end
