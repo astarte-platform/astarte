65f445a6426b6b9ac7e01e7762c5f4bbdac7d5f1
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-02-01 13:10:31 +0100

    config: add authentication_disabled? function

--- apps/astarte_appengine_api/lib/astarte_appengine_api/config.ex
+++ apps/astarte_appengine_api/lib/astarte_appengine_api/config.ex
@@ -9,4 +9,11 @@ defmodule Astarte.AppEngine.API.Config do
   def mqtt_options do
     Application.get_env(:astarte_appengine_api, :mqtt_options, [])
   end
+
+  @doc """
+  Returns true if the authentication is disabled
+  """
+  def authentication_disabled? do
+    Application.get_env(:astarte_appengine_api, :disable_authentication, false)
+  end
 end
