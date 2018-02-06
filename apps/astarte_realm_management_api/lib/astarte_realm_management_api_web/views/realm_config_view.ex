850c3fc7503ac2c3fb655aefbcf2dafe24bdecdf
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-02-06 10:37:35 +0100

    Add realm_config_view module

---
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/views/realm_config_view.ex
@@ -0,0 +1,31 @@
+#
+# This file is part of Astarte.
+#
+# Astarte is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Astarte is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2018 Ispirata Srl
+#
+
+defmodule Astarte.RealmManagement.APIWeb.RealmConfigView do
+  use Astarte.RealmManagement.APIWeb, :view
+  alias Astarte.RealmManagement.APIWeb.RealmConfigView
+
+  def render("show.json", %{auth_config: auth_config}) do
+    %{data: render_one(auth_config, RealmConfigView, "auth_config.json", auth_config: auth_config)}
+  end
+
+  def render("auth_config.json", %{auth_config: auth_config}) do
+    auth_config
+  end
+end
