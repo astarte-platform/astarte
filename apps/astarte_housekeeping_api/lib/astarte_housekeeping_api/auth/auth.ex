79dbd50c62c31f6ff3d6c847d82666c7dc2a52f7
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-02-06 18:29:37 +0100

    Add auth module

---
+++ apps/astarte_housekeeping_api/lib/astarte_housekeeping_api/auth/auth.ex
@@ -0,0 +1,31 @@
+#
+# This file is part of Astarte.
+#
+# Astarte is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Astarte is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2018 Ispirata Srl
+#
+
+defmodule Astarte.Housekeeping.API.Auth do
+  alias Astarte.Housekeeping.API.Config
+
+  def fetch_public_key do
+    case Config.jwt_public_key_pem() do
+      nil ->
+        {:error, :no_jwt_public_key_pem_configured}
+      pem ->
+        {:ok, pem}
+    end
+  end
+end
