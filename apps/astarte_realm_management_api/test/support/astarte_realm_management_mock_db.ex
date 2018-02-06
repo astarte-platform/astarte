f79b39d2e61b3df46a23a9e000d4ee9010b57548
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-02-06 11:26:26 +0100

    mock_db: add put_jwt_public_key_pem function

--- apps/astarte_realm_management_api/test/support/astarte_realm_management_mock_db.ex
+++ apps/astarte_realm_management_api/test/support/astarte_realm_management_mock_db.ex
@@ -12,4 +12,8 @@ defmodule Astarte.RealmManagement.Mock.DB do
   def get_jwt_public_key_pem(realm) do
     Agent.get(__MODULE__, &Map.get(&1, "jwt_public_key_pem_#{realm}"))
   end
+
+  def put_jwt_public_key_pem(realm, jwt_public_key_pem) do
+    Agent.update(__MODULE__, &Map.put(&1, "jwt_public_key_pem_#{realm}", jwt_public_key_pem))
+  end
 end
