8c55e06722c87f6997d0ebbd46619ba07e605baa
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-02-06 11:25:16 +0100

    mock_db: make it an Agent

--- apps/astarte_realm_management_api/test/support/astarte_realm_management_mock_db.ex
+++ apps/astarte_realm_management_api/test/support/astarte_realm_management_mock_db.ex
@@ -2,8 +2,7 @@ defmodule Astarte.RealmManagement.Mock.DB do
   alias Astarte.RealmManagement.API.JWTTestHelper
 
   def start_link do
-    # TODO: this will be an Agent some day
-    :ok
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
   def interfaces_list(_realm) do
