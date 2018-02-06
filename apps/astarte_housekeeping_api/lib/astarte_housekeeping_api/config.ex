3fc342dc930ebe8a2e359944fcd3bae33a6ad029
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-02-06 17:35:03 +0100

    Add config module

---
+++ apps/astarte_housekeeping_api/lib/astarte_housekeeping_api/config.ex
@@ -0,0 +1,38 @@
+#
+# This file is part of Astarte.
+#
+# Astarte is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Astarte is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2018 Ispirata Srl
+#
+
+defmodule Astarte.Housekeeping.API.Config do
+  @moduledoc """
+  This module contains functions to access the configuration
+  """
+
+  @doc """
+  Returns true if the authentication is disabled
+  """
+  def jwt_public_key_pem do
+    Application.get_env(:astarte_housekeeping_api, :jwt_public_key_pem)
+  end
+
+  @doc """
+  Returns true if the authentication is disabled
+  """
+  def authentication_disabled? do
+    Application.get_env(:astarte_housekeeping_api, :disable_authentication, false)
+  end
+end
