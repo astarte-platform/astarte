3f4290e601807afa58802c163f879109fb4a7761
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-02-13 17:12:05 +0100

    config: allow setting limit <= 0 to indicate no limit

--- apps/astarte_appengine_api/config/astarte_appengine_api.schema.exs
+++ apps/astarte_appengine_api/config/astarte_appengine_api.schema.exs
@@ -57,7 +57,7 @@ See the moduledoc for `Conform.Schema.Validator` for more details and examples.
       datatype: :integer,
       default: 10000,
       env_var: "APPENGINE_MAX_RESULTS_LIMIT",
-      doc: "The max number of data points returned by AppEngine API with a single call. Defaults to 10000",
+      doc: "The max number of data points returned by AppEngine API with a single call. Defaults to 10000. If <= 0, results are unlimited.",
       hidden: false,
       to: "astarte_appengine_api.max_results_limit"
     ],
@@ -258,6 +258,16 @@ See the moduledoc for `Conform.Schema.Validator` for more details and examples.
       case :inet.parse_address(charlist_ip) do
         {:ok, tuple_ip} -> tuple_ip
         _ -> raise "Invalid IP address in bind_address"
+      end
+    end,
+    "astarte_appengine_api.max_results_limit": fn conf ->
+      [{_, results}] = Conform.Conf.get(conf, "astarte_appengine_api.max_results_limit")
+
+      if results > 0 do
+        results
+      else
+        # If <= 0 we fix it to 0 to indicate no limit
+        0
       end
     end
   ],
