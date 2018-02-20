f78c1abb72a84c32d5da95edbfafc5ec727822c2
Davide Bettio <davide.bettio@ispirata.com>
2018-02-20 17:38:01 +0100

    EventsConsumer: read trigger action config from database kv_store table
    Retrieve trigger and its action from kv_store, this change allows triggers testing.
    
    For testing purposes a test trigger has been stored using the following example query:
    INSERT INTO kv_store (group, key, value) VALUES ('triggers', '6a08e52a-62dc-4e43-b3fd-b4c11614871d', 0x080112106a08e52a62dc4e43b3fdb4c11614871d222a7b22687474705f706f73745f75726c223a2022687474703a2f2f6c6f63616c686f73743a39383736227d);
    and it can be retrieved using:
    SELECT value FROM kv_store WHERE group='triggers' AND key='6a08e52a-62dc-4e43-b3fd-b4c11614871d';

--- apps/astarte_trigger_engine/lib/astarte_trigger_engine/events_consumer.ex
+++ apps/astarte_trigger_engine/lib/astarte_trigger_engine/events_consumer.ex
@@ -1,12 +1,20 @@
 defmodule Astarte.TriggerEngine.EventsConsumer do
 
   alias Astarte.Core.Triggers.SimpleEvents.SimpleEvent
+  alias Astarte.Core.Triggers.Trigger
+  alias CQEx.Client, as: DatabaseClient
+  alias CQEx.Query, as: DatabaseQuery
+  alias CQEx.Result, as: DatabaseResult
   require Logger
 
   def consume(payload, headers) do
+    headers_map = Enum.into(headers, %{})
+
+    {:ok, realm} = Map.fetch(headers_map, "x_astarte_realm")
+
     decoded_payload = SimpleEvent.decode(payload)
 
-    Logger.debug "consume: payload: #{inspect(decoded_payload)}, headers: #{inspect(headers)}"
+    Logger.debug("consume: payload: #{inspect(decoded_payload)}, headers: #{inspect(headers)}")
 
     %SimpleEvent{
       device_id: device_id,
@@ -17,23 +25,31 @@ defmodule Astarte.TriggerEngine.EventsConsumer do
       version: 1
     } = decoded_payload
 
-    process_simple_event(device_id, headers, event_type, event)
+    handle_simple_event(realm, device_id, headers_map, event_type, event)
   end
 
-  def process_simple_event(device_id, headers, :value_change_event, event) do
+  def handle_simple_event(realm, device_id, headers_map, event_type, event) do
+    {:ok, trigger_id} = Map.fetch(headers_map, "x_astarte_parent_trigger_id")
+
+    {:ok, action} = retrieve_trigger_configuration(realm, trigger_id)
+
+    process_simple_event(realm, device_id, event_type, event, action)
+  end
+
+  def process_simple_event(realm, device_id, :value_change_event, event, action) do
     generated_payload =  %{
-      "event_type": "value_change",
-      "device_id": device_id,
-      "interface": event.interface,
-      "path": event.path,
-      "new_value": decode_bson_value(event.new_bson_value),
-      "old_value": decode_bson_value(event.new_bson_value)
+      "event_type" => "value_change",
+      "device_id" => device_id,
+      "interface" => event.interface,
+      "path" => event.path,
+      "new_value" => decode_bson_value(event.new_bson_value),
+      "old_value" => decode_bson_value(event.old_bson_value)
     }
 
     {:ok, json_payload} = Poison.encode(generated_payload)
 
-    url = "http://localhost:9876/"
-    {status, response} = HTTPoison.post(url, json_payload)
+    {:ok, url} = Map.fetch(action, "http_post_url")
+    {status, response} = HTTPoison.post(url, json_payload, ["Astarte-Realm": realm])
 
     Logger.debug "http request status: #{inspect status}, got response: #{inspect response} from #{url}"
   end
@@ -45,6 +61,26 @@ defmodule Astarte.TriggerEngine.EventsConsumer do
 
       any_decoded ->
         any_decoded
+    end
+  end
+
+  def retrieve_trigger_configuration(realm_name, trigger_id) do
+    client =
+      DatabaseClient.new!(List.first(Application.get_env(:cqerl, :cassandra_nodes)), [keyspace: realm_name])
+
+    query =
+      DatabaseQuery.new()
+      |> DatabaseQuery.statement("SELECT value FROM kv_store WHERE group='triggers' AND key=:trigger_id;")
+      |> DatabaseQuery.put(:trigger_id, trigger_id)
+
+    with {:ok, result} <- DatabaseQuery.call(client, query),
+         ["value": trigger_data] <- DatabaseResult.head(result),
+         trigger  <- Trigger.decode(trigger_data),
+         {:ok, action} <- Poison.decode(trigger.action) do
+      {:ok, action}
+    else
+      _ ->
+        {:error, :trigger_not_found}
     end
   end
 
