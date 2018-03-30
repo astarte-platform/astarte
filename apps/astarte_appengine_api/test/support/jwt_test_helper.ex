a21bd7908c02d8f8d4eeb01a94e04cc968156e24
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-03-30 18:33:13 +0200

    jwt_test_helper: add helpers for channels tests

--- apps/astarte_appengine_api/test/support/jwt_test_helper.ex
+++ apps/astarte_appengine_api/test/support/jwt_test_helper.ex
@@ -44,4 +44,24 @@ defmodule Astarte.AppEngine.API.JWTTestHelper do
   def gen_jwt_all_access_token do
     gen_jwt_token([".*::.*"])
   end
+
+  def gen_channels_jwt_token(authorization_paths) do
+    jwk =
+      Application.get_env(:astarte_appengine_api, :test_priv_key)
+      |> JOSE.JWK.from_map()
+
+    {:ok, jwt, _claims} =
+      %User{id: "testuser"}
+      |> AuthGuardian.encode_and_sign(
+        %{a_ch: authorization_paths},
+        secret: jwk,
+        allowed_algos: ["RS256"]
+      )
+
+    jwt
+  end
+
+  def gen_channels_jwt_all_access_token do
+    gen_channels_jwt_token(["JOIN::.*", "WATCH::.*"])
+  end
 end
