546bd729ae1e903e8bf697b097b90de14e08fcba
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-04-24 13:53:50 +0200

    Add Utils module
    UUID utils for now

---
+++ apps/astarte_appengine_api/lib/astarte_appengine_api/utils.ex
@@ -0,0 +1,33 @@
+#
+# This file is part of Astarte.
+#
+# Astarte is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Astarte is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2018 Ispirata Srl
+#
+
+defmodule Astarte.AppEngine.API.Utils do
+  @moduledoc """
+  Utility module
+  """
+
+  def get_uuid() do
+    :uuid.get_v4()
+  end
+
+  def uuid_to_string(uuid) do
+    :uuid.uuid_to_string(uuid)
+    |> to_string()
+  end
+end
