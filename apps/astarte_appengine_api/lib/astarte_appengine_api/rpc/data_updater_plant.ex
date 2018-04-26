5cde2efc9e33ee7af5fe4fbffffe6fff9d238873
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-04-26 16:58:38 +0200

    data_updater_plant: add install_volatile_trigger

--- apps/astarte_appengine_api/lib/astarte_appengine_api/rpc/data_updater_plant.ex
+++ apps/astarte_appengine_api/lib/astarte_appengine_api/rpc/data_updater_plant.ex
@@ -23,4 +23,55 @@ defmodule Astarte.AppEngine.API.RPC.DataUpdaterPlant do
   """
 
   use Astarte.RPC.Protocol.DataUpdaterPlant
+
+  alias Astarte.AppEngine.API.Config
+  alias Astarte.AppEngine.API.RPC.VolatileTrigger
+
+  @rpc_client Config.rpc_client()
+
+  def install_volatile_trigger(realm_name, device_id, %VolatileTrigger{} = volatile_trigger) do
+    %VolatileTrigger{
+      object_id: object_id,
+      object_type: object_type,
+      serialized_simple_trigger: serialized_simple_trigger,
+      parent_id: parent_id,
+      simple_trigger_id: simple_trigger_id,
+      serialized_trigger_target: serialized_trigger_target
+    } = volatile_trigger
+
+    %InstallVolatileTrigger{
+      realm_name: realm_name,
+      device_id: device_id,
+      object_id: object_id,
+      object_type: object_type,
+      parent_id: parent_id,
+      simple_trigger: serialized_simple_trigger,
+      simple_trigger_id: simple_trigger_id,
+      trigger_target: serialized_trigger_target
+    }
+    |> encode_call(:install_volatile_trigger)
+    |> @rpc_client.rpc_call()
+    |> decode_reply()
+    |> extract_reply()
+  end
+
+  defp encode_call(call, callname) do
+    %Call{call: {callname, call}}
+    |> Call.encode()
+  end
+
+  defp decode_reply({:ok, encoded_reply}) when is_binary(encoded_reply) do
+    %Reply{reply: reply} = Reply.decode(encoded_reply)
+    reply
+  end
+
+  defp extract_reply({:generic_ok_reply, %GenericOkReply{}}) do
+    :ok
+  end
+
+  defp extract_reply({:generic_error_reply, error_struct = %GenericErrorReply{}}) do
+    error_map = Map.from_struct(error_struct)
+
+    {:error, error_map}
+  end
 end
