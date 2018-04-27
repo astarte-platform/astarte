3b899475b864b600456c5b6efbe8a238ce09ad35
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-04-27 17:26:21 +0200

    Add amqp_client

---
+++ apps/astarte_appengine_api/lib/astarte_appengine_api/rooms/amqp_client.ex
@@ -0,0 +1,134 @@
+#
+# This file is part of Astarte.
+#
+# Astarte is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Astarte is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2018 Ispirata Srl
+#
+
+defmodule Astarte.AppEngine.API.Rooms.AMQPClient do
+  require Logger
+  use GenServer
+
+  alias AMQP.Basic
+  alias AMQP.Channel
+  alias AMQP.Connection
+  alias AMQP.Exchange
+  alias AMQP.Queue
+  alias Astarte.AppEngine.API.Config
+  alias Astarte.AppEngine.API.Rooms.EventsDispatcher
+
+  @connection_backoff 10000
+
+  # API
+
+  def start_link(args \\ []) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def ack(delivery_tag) do
+    GenServer.call(__MODULE__, {:ack, delivery_tag})
+  end
+
+  # Server callbacks
+
+  def init(_args) do
+    rabbitmq_connect(false)
+  end
+
+  def terminate(_reason, %Channel{conn: conn} = chan) do
+    Channel.close(chan)
+    Connection.close(conn)
+  end
+
+  def handle_call({:ack, delivery_tag}, _from, chan) do
+    res = Basic.ack(chan, delivery_tag)
+    {:reply, res, chan}
+  end
+
+  # Confirmation sent by the broker after registering this process as a consumer
+  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, chan) do
+    {:noreply, chan}
+  end
+
+  # Sent by the broker when the consumer is unexpectedly cancelled (such as after a queue deletion)
+  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, chan) do
+    {:noreply, chan}
+  end
+
+  # Confirmation sent by the broker to the consumer process after a Basic.cancel
+  def handle_info({:basic_cancel_ok, %{consumer_tag: _consumer_tag}}, chan) do
+    {:noreply, chan}
+  end
+
+  # Message consumed
+  def handle_info({:basic_deliver, payload, meta}, chan) do
+    Logger.debug("got event, payload: #{inspect(payload)} meta: #{inspect(meta)}")
+
+    EventsDispatcher.dispatch(payload)
+
+    # TODO: should we ack manually?
+    Basic.ack(chan, meta.delivery_tag)
+
+    {:noreply, chan}
+  end
+
+  def handle_info({:try_to_connect}, _state) do
+    {:ok, new_state} = rabbitmq_connect()
+    {:noreply, new_state}
+  end
+
+  def handle_info({:DOWN, _, :process, _pid, reason}, _state) do
+    Logger.warn("RabbitMQ connection lost: #{inspect(reason)}. Trying to reconnect...")
+    {:ok, new_state} = rabbitmq_connect()
+    {:noreply, new_state}
+  end
+
+  defp rabbitmq_connect(retry \\ true) do
+    with {:ok, conn} <- Connection.open(Config.rooms_amqp_options()),
+         # Get notifications when the connection goes down
+         Process.monitor(conn.pid),
+         {:ok, chan} <- Channel.open(conn),
+         :ok <- Exchange.declare(chan, Config.rooms_events_routing_key(), :direct, durable: true),
+         {:ok, _queue} <- Queue.declare(chan, Config.rooms_events_queue_name(), durable: true),
+         :ok <-
+           Queue.bind(
+             chan,
+             Config.rooms_events_queue_name(),
+             Config.events_exchange_name(),
+             routing_key: Config.rooms_events_routing_key()
+           ),
+         {:ok, _consumer_tag} <- Basic.consume(chan, Config.rooms_events_queue_name()) do
+      {:ok, chan}
+    else
+      {:error, reason} ->
+        Logger.warn("RabbitMQ Connection error: #{inspect(reason)}")
+        maybe_retry(retry)
+
+      :error ->
+        Logger.warn("Unknown RabbitMQ connection error")
+        maybe_retry(retry)
+    end
+  end
+
+  defp maybe_retry(retry) do
+    if retry do
+      Logger.warn("Retrying connection in #{@connection_backoff} ms")
+      :erlang.send_after(@connection_backoff, :erlang.self(), {:try_to_connect})
+      {:ok, :not_connected}
+    else
+      {:stop, :connection_failed}
+    end
+  end
+end
