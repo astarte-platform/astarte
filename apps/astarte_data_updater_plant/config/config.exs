3c556eaa03425dc3c8c442506ca9f467aea5b99f
Davide Bettio <davide.bettio@ispirata.com>
2018-05-14 15:51:05 +0200

    Disable lager logger
    Fix issue introduced by amqp 1.0.2 update.

--- apps/astarte_data_updater_plant/config/config.exs
+++ apps/astarte_data_updater_plant/config/config.exs
@@ -2,6 +2,11 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+# lager is used by rabbit_common.
+# Silent it by setting the higher loglevel.
+config :lager,
+  handlers: [level: :critical]
+
 config :astarte_data_updater_plant, :queue_name,
   "vmq_all"
 
