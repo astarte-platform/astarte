0791d32a2d3f192dd65a8c85231994a8fa413002
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-05-18 17:14:32 +0200

    test: adapt support to new implementation

--- apps/astarte_pairing/test/support/database_test_helper.exs
+++ apps/astarte_pairing/test/support/database_test_helper.exs
@@ -14,11 +14,14 @@
 # You should have received a copy of the GNU General Public License
 # along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2017 Ispirata Srl
+# Copyright (C) 2017-2018 Ispirata Srl
 #
 
 defmodule Astarte.Pairing.DatabaseTestHelper do
+  alias Astarte.Core.Device
   alias Astarte.Pairing.Config
+  alias Astarte.Pairing.TestHelper
+  alias Astarte.Pairing.CredentialsSecret
   alias CQEx.Query
   alias CQEx.Client
 
@@ -38,18 +41,18 @@ defmodule Astarte.Pairing.DatabaseTestHelper do
     protocol_revision int,
     triggers set<ascii>,
     metadata map<ascii, text>,
-    inhibit_pairing boolean,
-    api_key ascii,
+    inhibit_credentials_request boolean,
+    credentials_secret ascii,
     cert_serial ascii,
     cert_aki ascii,
-    first_pairing timestamp,
+    first_credentials_request timestamp,
     last_connection timestamp,
     last_disconnection timestamp,
     connected boolean,
     pending_empty_cache boolean,
     total_received_msgs bigint,
     total_received_bytes bigint,
-    last_pairing_ip inet,
+    last_credentials_request_ip inet,
     last_seen_ip inet,
 
     PRIMARY KEY (device_id)
@@ -62,9 +65,45 @@ defmodule Astarte.Pairing.DatabaseTestHelper do
 
   @test_realm "autotestrealm"
 
+  @unregistered_hw_id TestHelper.random_hw_id()
+
+  @registered_not_confirmed_hw_id TestHelper.random_hw_id()
+  @registered_not_confirmed_credentials_secret CredentialsSecret.generate()
+
+  @registered_and_confirmed_hw_id TestHelper.random_hw_id()
+  @registered_and_confirmed_credentials_secret CredentialsSecret.generate()
+
+  @registered_and_inhibited_hw_id TestHelper.random_hw_id()
+  @registered_and_inhibited_credentials_secret CredentialsSecret.generate()
+
+  @insert_device """
+  INSERT INTO devices
+  (device_id, extended_id, credentials_secret, inhibit_credentials_request,
+  protocol_revision, total_received_bytes, total_received_msgs, first_credentials_request)
+  VALUES (:device_id, :extended_id, :credentials_secret, :inhibit_credentials_request,
+  1, 0, 0, :first_credentials_request)
+  """
+
   def test_realm(), do: @test_realm
 
-  def seed_db do
+  def unregistered_hw_id(), do: @unregistered_hw_id
+
+  def registered_not_confirmed_hw_id(), do: @registered_not_confirmed_hw_id
+
+  def registered_not_confirmed_credentials_secret(),
+    do: @registered_not_confirmed_credentials_secret
+
+  def registered_and_confirmed_hw_id(), do: @registered_and_confirmed_hw_id
+
+  def registered_and_confirmed_credentials_secret(),
+    do: @registered_and_confirmed_credentials_secret
+
+  def registered_and_inhibited_hw_id(), do: @registered_and_inhibited_hw_id
+
+  def registered_and_inhibited_credentials_secret(),
+    do: @registered_and_inhibited_credentials_secret
+
+  def create_db do
     client =
       Config.cassandra_node()
       |> Client.new!()
@@ -75,6 +114,76 @@ defmodule Astarte.Pairing.DatabaseTestHelper do
     else
       %{msg: msg} -> {:error, msg}
     end
+  end
+
+  def seed_devices do
+    client =
+      Config.cassandra_node()
+      |> Client.new!(keyspace: @test_realm)
+
+    {:ok, registered_not_confirmed_device_id} =
+      Device.decode_device_id(@registered_not_confirmed_hw_id, allow_extended_id: true)
+
+    secret_hash = CredentialsSecret.hash(@registered_not_confirmed_credentials_secret)
+
+    registered_not_confirmed_query =
+      Query.new()
+      |> Query.statement(@insert_device)
+      |> Query.put(:device_id, registered_not_confirmed_device_id)
+      |> Query.put(:extended_id, @registered_not_confirmed_hw_id)
+      |> Query.put(:credentials_secret, secret_hash)
+      |> Query.put(:inhibit_credentials_request, false)
+      |> Query.put(:first_credentials_request, nil)
+
+    {:ok, registered_and_confirmed_device_id} =
+      Device.decode_device_id(@registered_and_confirmed_hw_id, allow_extended_id: true)
+
+    secret_hash = CredentialsSecret.hash(@registered_and_confirmed_credentials_secret)
+
+    registered_and_confirmed_query =
+      Query.new()
+      |> Query.statement(@insert_device)
+      |> Query.put(:device_id, registered_and_confirmed_device_id)
+      |> Query.put(:extended_id, @registered_and_confirmed_hw_id)
+      |> Query.put(:credentials_secret, secret_hash)
+      |> Query.put(:inhibit_credentials_request, false)
+      |> Query.put(
+        :first_credentials_request,
+        DateTime.utc_now() |> DateTime.to_unix(:milliseconds)
+      )
+
+    {:ok, registered_and_inhibited_device_id} =
+      Device.decode_device_id(@registered_and_inhibited_hw_id, allow_extended_id: true)
+
+    secret_hash = CredentialsSecret.hash(@registered_and_inhibited_credentials_secret)
+
+    registered_and_inhibited_query =
+      Query.new()
+      |> Query.statement(@insert_device)
+      |> Query.put(:device_id, registered_and_inhibited_device_id)
+      |> Query.put(:extended_id, @registered_and_inhibited_hw_id)
+      |> Query.put(:credentials_secret, secret_hash)
+      |> Query.put(:inhibit_credentials_request, true)
+      |> Query.put(
+        :first_credentials_request,
+        DateTime.utc_now() |> DateTime.to_unix(:milliseconds)
+      )
+
+    with {:ok, _} <- Query.call(client, registered_not_confirmed_query),
+         {:ok, _} <- Query.call(client, registered_and_confirmed_query),
+         {:ok, _} <- Query.call(client, registered_and_inhibited_query) do
+      :ok
+    end
+  end
+
+  def clean_devices do
+    client =
+      Config.cassandra_node()
+      |> Client.new!(keyspace: @test_realm)
+
+    Query.call!(client, "TRUNCATE devices")
+
+    :ok
   end
 
   def drop_db do
--- apps/astarte_pairing/test/support/test_helper.ex
+++ apps/astarte_pairing/test/support/test_helper.ex
@@ -14,25 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2017 Ispirata Srl
+# Copyright (C) 2017-2018 Ispirata Srl
 #
 
 defmodule Astarte.Pairing.TestHelper do
-  @valid_fallback_api_key "validfallbackapikey"
-
   def random_hw_id do
     :crypto.strong_rand_bytes(32) |> Base.url_encode64(padding: false)
-  end
-
-  def valid_fallback_api_key do
-    @valid_fallback_api_key
-  end
-
-  def fallback_verify_key(@valid_fallback_api_key, _salt) do
-    {:ok, "testrealm", :uuid.get_v4()}
-  end
-
-  def fallback_verify_key(_invalid_api_key, _salt) do
-    {:error, :invalid_api_key}
   end
 end
