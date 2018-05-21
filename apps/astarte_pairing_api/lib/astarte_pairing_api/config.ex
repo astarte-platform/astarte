27d717803006393edb112ac6f65f40b32e8223dc
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-05-21 12:36:03 +0200

    config: add rpc_client function

--- apps/astarte_pairing_api/lib/astarte_pairing_api/config.ex
+++ apps/astarte_pairing_api/lib/astarte_pairing_api/config.ex
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2017 Ispirata Srl
+# Copyright (C) 2017-2018 Ispirata Srl
 #
 
 defmodule Astarte.Pairing.API.Config do
@@ -27,5 +27,12 @@ defmodule Astarte.Pairing.API.Config do
   """
   def jwt_public_key do
     Application.get_env(:astarte_pairing_api, :jwt_public_key)
+  end
+
+  @doc """
+  Returns the RPC Client
+  """
+  def rpc_client do
+    Application.get_env(:astarte_pairing_api, :rpc_client, Astarte.RPC.AMQP.Client)
   end
 end
