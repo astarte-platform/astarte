0482e6cb9a8d3aa3337c89425e82ceeae839608b
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-05-24 17:57:44 +0200

    cert_verifier: change API
    Return {:error, reason} only if we couldn't verify the certificate, if
    we verified it and it's invalid return {:ok, %{valid: false}}

--- apps/astarte_pairing/lib/astarte_pairing/cert_verifier.ex
+++ apps/astarte_pairing/lib/astarte_pairing/cert_verifier.ex
@@ -28,29 +28,31 @@ defmodule Astarte.Pairing.CertVerifier do
   Verifies the validity of the given `pem_certificate` against `ca_pem_cert`.
 
   Returns:
-  - `{:ok, %{timestamp: timestamp, until: until}` if the certificate is valid.
+  - `{:ok, %{valid: true, timestamp: timestamp, until: until}` if the certificate is valid.
   `timestamp` contains the timestamp of the verification, `until` contains the certificate
   expiration. Both are represented in milliseconds from epoch UTC.
-  - `{:error, reason}` if the certificate isn't valid.
+  - `{:ok, %{valid: false, timestamp: timestamp, reason: reason}` if the certificate isn't valid.
+  - `{:error, :invalid_certificate}` if the certificate can't be decoded.
   """
   def verify(pem_cert, ca_pem_cert) do
+    timestamp =
+      DateTime.utc_now()
+      |> DateTime.to_unix(:milliseconds)
+
     with {:ok, der_cert} <- extract_der(pem_cert),
-         {:ok, ca_der_cert} <- extract_der(ca_pem_cert) do
-      case :public_key.pkix_path_validation(ca_der_cert, [der_cert], []) do
-        {:ok, _} ->
-          timestamp =
-            DateTime.utc_now()
-            |> DateTime.to_unix(:milliseconds)
+         {:ok, ca_der_cert} <- extract_der(ca_pem_cert),
+         {:ok, _} <- :public_key.pkix_path_validation(ca_der_cert, [der_cert], []) do
+      until =
+        CertUtils.not_after!(pem_cert)
+        |> DateTime.to_unix(:milliseconds)
 
-          until =
-            CertUtils.not_after!(pem_cert)
-            |> DateTime.to_unix(:milliseconds)
+      {:ok, %{valid: true, timestamp: timestamp, until: until}}
+    else
+      {:error, {:bad_cert, reason}} ->
+        {:ok, %{valid: false, timestamp: timestamp, reason: reason}}
 
-          {:ok, %{timestamp: timestamp, until: until}}
-
-        {:error, {:bad_cert, reason}} ->
-          {:error, reason}
-      end
+      {:error, reason} ->
+        {:ok, %{valid: false, timestamp: timestamp, reason: reason}}
     end
   end
 
