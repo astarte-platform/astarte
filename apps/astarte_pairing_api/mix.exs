0acb03f545191c55256338e95994e1d961a65f12
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-05-28 18:18:09 +0200

    Add astarte_core dep

--- apps/astarte_pairing_api/mix.exs
+++ apps/astarte_pairing_api/mix.exs
@@ -51,12 +51,14 @@ defmodule Astarte.Pairing.API.Mixfile do
 
   defp astarte_required_modules("true") do
     [
+      {:astarte_core, in_umbrella: true},
       {:astarte_rpc, in_umbrella: true},
     ]
   end
 
   defp astarte_required_modules(_) do
     [
+      {:astarte_core, git: "https://git.ispirata.com/Astarte-NG/astarte_core"},
       {:astarte_rpc, git: "https://git.ispirata.com/Astarte-NG/astarte_rpc"},
     ]
   end
