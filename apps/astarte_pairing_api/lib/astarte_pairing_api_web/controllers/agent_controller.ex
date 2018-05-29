db429f8faf0989725b94b88d4d912f0354c4a648
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-05-29 17:11:56 +0200

    s/api_key_controller/agent_controller/g
    Use new auth

--- apps/astarte_pairing_api/lib/astarte_pairing_api_web/controllers/agent_controller.ex
+++ apps/astarte_pairing_api/lib/astarte_pairing_api_web/controllers/agent_controller.ex
@@ -14,35 +14,24 @@
 # You should have received a copy of the GNU General Public License
 # along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2017 Ispirata Srl
+# Copyright (C) 2017-2018 Ispirata Srl
 #
 
-defmodule Astarte.Pairing.APIWeb.APIKeyController do
+defmodule Astarte.Pairing.APIWeb.AgentController do
   use Astarte.Pairing.APIWeb, :controller
 
   alias Astarte.Pairing.API.Agent
-  alias Astarte.Pairing.API.Agent.APIKey
-  alias Astarte.Pairing.API.Agent.Realm
-  alias Astarte.Pairing.APIWeb.AgentGuardian
-
-  plug Guardian.Plug.Pipeline,
-    otp_app: :astarte_pairing_api,
-    module: Astarte.Pairing.APIWeb.AgentGuardian,
-    error_handler: Astarte.Pairing.APIWeb.FallbackController
-  plug Guardian.Plug.VerifyHeader, realm: :none
-  plug Guardian.Plug.EnsureAuthenticated
-  plug Guardian.Plug.LoadResource
+  alias Astarte.Pairing.API.Agent.DeviceRegistrationResponse
 
   action_fallback Astarte.Pairing.APIWeb.FallbackController
 
-  def create(conn, %{"hwId" => hw_id}) do
-    # hwId is spelled this way to preserve backwards compatibility
-    with %Realm{realm_name: realm} <- AgentGuardian.Plug.current_resource(conn),
-         {:ok, %APIKey{} = api_key} <-
-           Agent.generate_api_key(%{"hw_id" => hw_id, "realm" => realm}) do
+  plug Astarte.Pairing.APIWeb.Plug.AuthorizePath
+
+  def create(conn, %{"realm_name" => realm, "data" => params}) do
+    with {:ok, %DeviceRegistrationResponse{} = response} <- Agent.register_device(realm, params) do
       conn
       |> put_status(:created)
-      |> render("show.json", api_key)
+      |> render("show.json", device_registration_response: response)
     end
   end
 end
