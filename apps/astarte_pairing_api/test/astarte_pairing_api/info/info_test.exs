956c5d7e23e2d29b58a270c9ed7a6538f12bbaa2
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-05-30 15:52:19 +0200

    Adapt info_test

--- apps/astarte_pairing_api/test/astarte_pairing_api/info/info_test.exs
+++ apps/astarte_pairing_api/test/astarte_pairing_api/info/info_test.exs
@@ -14,21 +14,90 @@
 # You should have received a copy of the GNU General Public License
 # along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2017 Ispirata Srl
+# Copyright (C) 2017-2018 Ispirata Srl
 #
 
 defmodule Astarte.Pairing.API.InfoTest do
   use Astarte.Pairing.API.DataCase
 
-  alias Astarte.Pairing.API.Info.BrokerInfo
+  alias Astarte.Pairing.API.Info.DeviceInfo
   alias Astarte.Pairing.API.Info
   alias Astarte.Pairing.Mock
 
-  describe "broker_info" do
-    test "get_broker_info! returns valid broker_info with given id" do
-      assert %BrokerInfo{url: url, version: version} = Info.get_broker_info!()
-      assert url == Mock.broker_url()
-      assert version == Mock.version()
+  alias Astarte.RPC.Protocol.Pairing.{
+    AstarteMQTTV1Status,
+    Call,
+    GenericErrorReply,
+    GetInfo,
+    GetInfoReply,
+    ProtocolStatus,
+    Reply
+  }
+
+  import Mox
+
+  @test_realm "testrealm"
+  @test_hw_id "PDL3KNj7RVifHZD-1w_6wA"
+
+  @credentials_secret "7wfs9MIBysBGG/v6apqNVBXXQii6Bris6CeU7FdCgWU="
+  @wrong_credentials_secret "8wfs9MIBysBGG/v6apqNVBXXQii6Bris6CeU7FdCgWU="
+  @version "0.1.0"
+  @status "confirmed"
+  @broker_url "ssl://broker.example.com:8883"
+
+  @encoded_info_response %Reply{
+                           reply:
+                             {:get_info_reply,
+                              %GetInfoReply{
+                                version: @version,
+                                device_status: @status,
+                                protocols: [
+                                  %ProtocolStatus{
+                                    status:
+                                      {:astarte_mqtt_v1,
+                                       %AstarteMQTTV1Status{broker_url: @broker_url}}
+                                  }
+                                ]
+                              }}
+                         }
+                         |> Reply.encode()
+  @encoded_forbidden_response %Reply{
+                                reply:
+                                  {:generic_error_reply,
+                                   %GenericErrorReply{error_name: "forbidden"}}
+                              }
+                              |> Reply.encode()
+
+  @rpc_destination Astarte.RPC.Protocol.Pairing.amqp_queue()
+
+  describe "device_info" do
+    test "returns valid info with authorized call" do
+      MockRPCClient
+      |> expect(:rpc_call, fn serialized_call, @rpc_destination ->
+        assert %Call{call: {:get_info, %GetInfo{} = get_info_call}} = Call.decode(serialized_call)
+
+        assert %GetInfo{
+                 realm: @test_realm,
+                 hw_id: @test_hw_id,
+                 secret: @credentials_secret
+               } = get_info_call
+
+        {:ok, @encoded_info_response}
+      end)
+
+      assert {:ok, %DeviceInfo{status: @status, version: @version, protocols: protocols}} =
+               Info.get_device_info(@test_realm, @test_hw_id, @credentials_secret)
+
+      assert %{astarte_mqtt_v1: %{broker_url: @broker_url}} = protocols
+    end
+
+    test "returns forbidden with forbidden call" do
+      MockRPCClient
+      |> expect(:rpc_call, fn _serialized_call, @rpc_destination ->
+        {:ok, @encoded_forbidden_response}
+      end)
+
+      assert {:error, :forbidden} = Info.get_device_info(@test_realm, @test_hw_id, @wrong_credentials_secret)
     end
   end
 end
