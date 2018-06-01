ad8d73aa4ece40e5400c06fe1b6304ea1541654c
Davide Bettio <davide.bettio@ispirata.com>
2018-06-01 18:01:27 +0200

    DataUpdater.Impl: do not compare BSON payloads
    Do not compare BSON payloads, just compare the values.

--- apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/data_updater/impl.ex
+++ apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/data_updater/impl.ex
@@ -209,10 +209,13 @@ defmodule Astarte.DataUpdaterPlant.DataUpdater.Impl do
          device_id_string,
          interface_name,
          path,
-         old_bson_value,
-         payload
+         previous_value,
+         value
        ) do
-    if old_bson_value != payload do
+    old_bson_value = Bson.encode(%{v: previous_value})
+    payload = Bson.encode(%{v: value})
+
+    if previous_value != value do
       Enum.each(value_change_triggers, fn trigger ->
         TriggersHandler.value_change(
           trigger.trigger_targets,
@@ -235,24 +238,27 @@ defmodule Astarte.DataUpdaterPlant.DataUpdater.Impl do
          device,
          interface,
          path,
-         old_bson_value,
-         payload
+         previous_value,
+         value
        ) do
-    if old_bson_value == <<>> and payload != <<>> do
+    old_bson_value = Bson.encode(%{v: previous_value})
+    payload = Bson.encode(%{v: value})
+
+    if previous_value == nil and value != nil do
       Enum.each(path_created_triggers, fn trigger ->
         targets = trigger.trigger_targets
         TriggersHandler.path_created(targets, realm, device, interface, path, payload)
       end)
     end
 
-    if old_bson_value != <<>> and payload == <<>> do
+    if previous_value != nil and value == nil do
       Enum.each(path_removed_triggers, fn trigger ->
         targets = trigger.trigger_targets
         TriggersHandler.path_removed(targets, realm, device, interface, path)
       end)
     end
 
-    if old_bson_value != payload do
+    if previous_value != value do
       Enum.each(value_change_applied_triggers, fn trigger ->
         targets = trigger.trigger_targets
 
@@ -304,7 +310,7 @@ defmodule Astarte.DataUpdaterPlant.DataUpdater.Impl do
       {has_change_triggers, change_triggers} =
         get_value_change_triggers(new_state, interface_id, endpoint_id, path, value)
 
-      old_bson_value =
+      previous_value =
         with {:has_change_triggers, :ok} <- {:has_change_triggers, has_change_triggers},
              {:ok, property_value} <-
                Data.fetch_property(
@@ -314,13 +320,13 @@ defmodule Astarte.DataUpdaterPlant.DataUpdater.Impl do
                  endpoint,
                  path
                ) do
-          Bson.encode(%{v: property_value})
+          property_value
         else
           {:has_change_triggers, _not_ok} ->
             nil
 
           {:error, :property_not_set} ->
-            <<>>
+            nil
         end
 
       if has_change_triggers == :ok do
@@ -331,8 +337,8 @@ defmodule Astarte.DataUpdaterPlant.DataUpdater.Impl do
             device_id_string,
             interface_descriptor.name,
             path,
-            old_bson_value,
-            payload
+            previous_value,
+            value
           )
       end
 
@@ -389,8 +395,8 @@ defmodule Astarte.DataUpdaterPlant.DataUpdater.Impl do
             device_id_string,
             interface_descriptor.name,
             path,
-            old_bson_value,
-            payload
+            previous_value,
+            value
           )
       end
 
