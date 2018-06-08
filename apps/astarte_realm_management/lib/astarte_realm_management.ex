f23f2e78452659fd48a9f9efa5eec7de00509b4c
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-06-08 15:55:52 +0200

    astarte_realm_management: use inline Supervisor and adapt to new RPC
    Remove supervisor module

--- apps/astarte_realm_management/lib/astarte_realm_management.ex
+++ apps/astarte_realm_management/lib/astarte_realm_management.ex
@@ -1,7 +1,17 @@
 defmodule Astarte.RealmManagement do
   use Application
 
+  alias Astarte.RPC.Protocol.RealmManagement, as: Protocol
+
+  alias Astarte.RealmManagement.RPC.Handler
+
   def start(_type, _args) do
-    Astarte.RealmManagement.Supervisor.start_link()
+
+    children = [
+      {Astarte.RPC.AMQP.Server, [amqp_queue: Protocol.amqp_queue(), handler: Handler]}
+    ]
+
+    opts = [strategy: :one_for_one, name: Astarte.RealmManagement.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
