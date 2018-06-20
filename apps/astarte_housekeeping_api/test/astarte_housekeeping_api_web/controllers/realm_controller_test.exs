8e14e6b685d842ac3fb5c8169bbe974f368bfde9
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-06-20 17:19:11 +0200

    tests: add replication_factor tests

--- apps/astarte_housekeeping_api/test/astarte_housekeeping_api_web/controllers/realm_controller_test.exs
+++ apps/astarte_housekeeping_api/test/astarte_housekeeping_api_web/controllers/realm_controller_test.exs
@@ -37,8 +37,10 @@ defmodule Astarte.Housekeeping.APIWeb.RealmControllerTest do
   """
 
   @create_attrs %{"data" => %{"realm_name" => "testrealm", "jwt_public_key_pem" => @pubkey}}
+  @explicit_replication_attrs %{"data" => %{"realm_name" => "testrealm2", "jwt_public_key_pem" => @pubkey, "replication_factor" => 3}}
   @update_attrs %{"data" => %{}}
   @invalid_name_attrs %{"data" => %{"realm_name" => "0invalid", "jwt_public_key_pem" => @pubkey}}
+  @invalid_replication_attrs %{"data" => %{"realm_name" => "testrealm", "jwt_public_key_pem" => @pubkey, "replication_factor" => -3}}
   @no_pubkey_attrs %{"data" => %{"realm_name" => "valid"}}
   @invalid_pubkey_attrs %{"data" => %{"realm_name" => "valid", "jwt_public_key_pem" => "invalid"}}
   @malformed_pubkey_attrs %{
@@ -68,9 +70,13 @@ defmodule Astarte.Housekeeping.APIWeb.RealmControllerTest do
 
   test "lists all entries on index", %{conn: conn} do
     conn = get(conn, realm_path(conn, :index))
-    resp1 = %{"data" => []}
-    resp2 = %{"data" => ["testrealm"]}
-    assert json_response(conn, 200) == resp1 || json_response(conn, 200) == resp2
+    assert json_response(conn, 200) == %{"data" => []}
+
+    conn = post(conn, realm_path(conn, :create), @create_attrs)
+    assert response(conn, 201)
+
+    conn = get(conn, realm_path(conn, :index))
+    assert json_response(conn, 200) == %{"data" => ["testrealm"]}
   end
 
   test "creates realm and renders realm when data is valid", %{conn: conn} do
@@ -82,7 +88,23 @@ defmodule Astarte.Housekeeping.APIWeb.RealmControllerTest do
     assert json_response(conn, 200) == %{
              "data" => %{
                "realm_name" => @create_attrs["data"]["realm_name"],
-               "jwt_public_key_pem" => @create_attrs["data"]["jwt_public_key_pem"]
+               "jwt_public_key_pem" => @create_attrs["data"]["jwt_public_key_pem"],
+               "replication_factor" => 1
+             }
+           }
+  end
+
+  test "creates realm and renders realm with explicit replication_factor", %{conn: conn} do
+    conn = post(conn, realm_path(conn, :create), @explicit_replication_attrs)
+    assert response(conn, 201)
+
+    conn = get(conn, realm_path(conn, :show, @explicit_replication_attrs["data"]["realm_name"]))
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "realm_name" => @explicit_replication_attrs["data"]["realm_name"],
+               "jwt_public_key_pem" => @explicit_replication_attrs["data"]["jwt_public_key_pem"],
+               "replication_factor" => @explicit_replication_attrs["data"]["replication_factor"]
              }
            }
   end
@@ -110,6 +132,11 @@ defmodule Astarte.Housekeeping.APIWeb.RealmControllerTest do
 
   test "does not create realm and renders errors when public key is malformed", %{conn: conn} do
     conn = post(conn, realm_path(conn, :create), @malformed_pubkey_attrs)
+    assert json_response(conn, 422)["errors"] != %{}
+  end
+
+  test "does not create realm and renders errors when replication_factor is invalid", %{conn: conn} do
+    conn = post(conn, realm_path(conn, :create), @invalid_replication_attrs)
     assert json_response(conn, 422)["errors"] != %{}
   end
 
