600669679d1594d0d38f823abfd1f260f226062c
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-06-20 15:41:41 +0200

    config: silence lager

--- apps/astarte_housekeeping/config/config.exs
+++ apps/astarte_housekeeping/config/config.exs
@@ -2,4 +2,9 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+# lager is used by rabbit_common.
+# Silent it by setting the higher loglevel.
+config :lager,
+  handlers: [level: :critical]
+
 import_config "#{Mix.env}.exs"
