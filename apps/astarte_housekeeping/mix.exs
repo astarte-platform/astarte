4941d349955f91cd8fd187197c02136be53201ac
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-06-20 15:44:48 +0200

    mix.exs: use different elixirc_paths based on the Env

--- apps/astarte_housekeeping/mix.exs
+++ apps/astarte_housekeeping/mix.exs
@@ -10,6 +10,7 @@ defmodule Astarte.Housekeeping.Mixfile do
       deps_path: "deps",
       lockfile: "mix.lock",
       elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
@@ -29,6 +30,9 @@ defmodule Astarte.Housekeeping.Mixfile do
       mod: {Astarte.Housekeeping, []}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp astarte_required_modules("true") do
     [
