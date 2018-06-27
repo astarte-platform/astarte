5d239ef14e5f87f59eee4045787817f4e57ecd22
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-06-27 14:27:24 +0200

    mix.exs: bump to Elixir 1.6

--- apps/astarte_trigger_engine/mix.exs
+++ apps/astarte_trigger_engine/mix.exs
@@ -24,7 +24,7 @@ defmodule Astarte.TriggerEngine.Mixfile do
     [
       app: :astarte_trigger_engine,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
