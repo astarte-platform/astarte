e3d60b69e4f022c4006d41cc0f8fc27bfa698aba
Davide Bettio <davide.bettio@ispirata.com>
2018-06-27 11:09:41 +0200

    .gitlab-ci.ym/.travis.yml/Dockerfile: use Elixir 1.6.5 (with OTP 20)
    Freeze elixir version to 1.6.5 on OTP 20 (OTP 21 is not yet supported).

--- apps/astarte_realm_management/mix.exs
+++ apps/astarte_realm_management/mix.exs
@@ -5,7 +5,7 @@ defmodule Astarte.RealmManagement.Mixfile do
     [
       app: :astarte_realm_management,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
