a6e3eb85556c9896b7ad347094f58a860b45115d
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-07-20 16:28:37 +0200

    fallback_controller: handle realm_not_found and interface_not_found

--- apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/controllers/fallback_controller.ex
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/controllers/fallback_controller.ex
@@ -49,6 +49,18 @@ defmodule Astarte.RealmManagement.APIWeb.FallbackController do
     |> render(Astarte.RealmManagement.APIWeb.ErrorView, :invalid_major)
   end
 
+  def call(conn, {:error, :realm_not_found}) do
+    conn
+    |> put_status(:forbidden)
+    |> render(Astarte.RealmManagement.APIWeb.ErrorView, :"403")
+  end
+
+  def call(conn, {:error, :interface_not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(Astarte.RealmManagement.APIWeb.ErrorView, :interface_not_found)
+  end
+
   # This is called when no JWT token is present
   def auth_error(conn, {:unauthenticated, _reason}, _opts) do
     conn
