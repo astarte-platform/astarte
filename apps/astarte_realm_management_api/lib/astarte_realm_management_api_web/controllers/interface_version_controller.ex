f2bf11aa082d63df29de0f7967d91a649594ef20
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-07-20 17:41:54 +0200

    Adapt calls to non-raising versions

--- apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/controllers/interface_version_controller.ex
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api_web/controllers/interface_version_controller.ex
@@ -20,14 +20,15 @@
 defmodule Astarte.RealmManagement.APIWeb.InterfaceVersionController do
   use Astarte.RealmManagement.APIWeb, :controller
 
+  alias Astarte.RealmManagement.API.Interfaces
+
   plug Astarte.RealmManagement.APIWeb.Plug.AuthorizePath
 
   action_fallback Astarte.RealmManagement.APIWeb.FallbackController
 
   def index(conn, %{"realm_name" => realm_name, "id" => id}) do
-    interfaces =
-      Astarte.RealmManagement.API.Interfaces.list_interface_major_versions!(realm_name, id)
-
-    render(conn, "index.json", interfaces: interfaces)
+    with {:ok, interfaces} <- Interfaces.list_interface_major_versions(realm_name, id) do
+      render(conn, "index.json", interfaces: interfaces)
+    end
   end
 end
