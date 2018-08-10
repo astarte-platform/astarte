45b7f8af74945d738275b86427507b4454d90e45
Davide Bettio <davide.bettio@ispirata.com>
2018-08-10 18:31:51 +0200

    Prepare beta.2 release
    Increase version number to 0.10.0-beta.2

--- apps/astarte_trigger_engine/mix.exs
+++ apps/astarte_trigger_engine/mix.exs
@@ -23,7 +23,7 @@ defmodule Astarte.TriggerEngine.Mixfile do
   def project do
     [
       app: :astarte_trigger_engine,
-      version: "0.10.0-beta.1",
+      version: "0.10.0-beta.2",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env() == :prod,
