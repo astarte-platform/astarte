9314680363c098ffe673d32859dfe1be57d87034
Davide Bettio <davide.bettio@ispirata.com>
2018-08-10 17:19:29 +0200

    Update version number to 0.10.0-dev and add CHANGELOG
    mix.exs: update version number to 0.10.0-dev and add CHANGELOG.md file.

--- apps/astarte_data_updater_plant/mix.exs
+++ apps/astarte_data_updater_plant/mix.exs
@@ -22,7 +22,7 @@ defmodule Astarte.DataUpdaterPlant.Mixfile do
   def project do
     [
       app: :astarte_data_updater_plant,
-      version: "0.1.0",
+      version: "0.10.0-dev",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
