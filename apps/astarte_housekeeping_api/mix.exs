ebb592cbc0824b367a57df8006a3f77de2ad7a8a
Davide Bettio <davide@uninstall.it>
2018-08-12 12:19:14 +0200

    Update version number to 0.10.0-dev and add CHANGELOG
    mix.exs: update version number to 0.10.0-dev and add CHANGELOG.md file.

--- apps/astarte_housekeeping_api/mix.exs
+++ apps/astarte_housekeeping_api/mix.exs
@@ -3,7 +3,7 @@ defmodule Astarte.Housekeeping.API.Mixfile do
 
   def project do
     [app: :astarte_housekeeping_api,
-     version: "0.0.1",
+     version: "0.10.0-dev",
      elixir: "~> 1.6",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
