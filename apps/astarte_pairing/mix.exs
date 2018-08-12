fb9f26b93a46167344223d13feb14a43a8dff5a3
Davide Bettio <davide@uninstall.it>
2018-08-12 11:10:10 +0200

    Update version number to 0.10.0-dev and add CHANGELOG
    mix.exs: update version number to 0.10.0-dev and add CHANGELOG.md file.

--- apps/astarte_pairing/mix.exs
+++ apps/astarte_pairing/mix.exs
@@ -4,7 +4,7 @@ defmodule Astarte.Pairing.Mixfile do
   def project do
     [
       app: :astarte_pairing,
-      version: "0.1.0",
+      version: "0.10.0-dev",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env() == :prod,
