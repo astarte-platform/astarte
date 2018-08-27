a2682f05609cd62d23d5e8527a35f2daaff6827f
Davide Bettio <davide.bettio@ispirata.com>
2018-08-27 12:26:55 +0200

    Prepare next devel cycle
    doc/mix.exs: update version number to 0.11.0-dev.

--- doc/mix.exs
+++ doc/mix.exs
@@ -4,7 +4,7 @@ defmodule Doc.MixProject do
   def project do
     [
       app: :doc,
-      version: "0.10.0-dev",
+      version: "0.11.0-dev",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
