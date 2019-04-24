5485b0e3e65d5f583fa39e8c4647703a229b7f23
Davide Bettio <davide.bettio@ispirata.com>
2019-04-24 15:58:42 +0200

    Update excoveralls and dialyxir
    Switch dialyxir to ~> v0.5 and excoveralls to ~> v0.10.

--- mix.exs
+++ mix.exs
@@ -66,8 +66,8 @@ defmodule Astarte.DataAccess.Mixfile do
        github: "matehat/cqerl", ref: "6e44b42df1cb0fcf82d8ab4df032c2e7cacb96f9", override: true},
       {:cqex, github: "matehat/cqex", ref: "a2c45667108f9b1e8a9c73c5250a04020bf72a30"},
       {:conform, "== 2.5.2"},
-      {:dialyxir, "== 0.5.1", only: [:dev], runtime: false},
-      {:excoveralls, "== 0.9.1", only: :test}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
