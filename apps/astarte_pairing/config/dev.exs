d2ae78c1beb0265b387a1b217a3d2dba30949847
Riccardo Binetti <riccardo.binetti@ispirata.com>
2019-07-16 11:55:37 +0200

    Update .formatter.exs and run mix format

--- apps/astarte_pairing/config/dev.exs
+++ apps/astarte_pairing/config/dev.exs
@@ -18,11 +18,11 @@
 
 use Mix.Config
 
-config :astarte_pairing, :broker_url,
-  "ssl://broker.beta.astarte.cloud:8883/"
+config :astarte_pairing, :broker_url, "ssl://broker.beta.astarte.cloud:8883/"
 
-config :astarte_pairing, :cfssl_url,
-  "http://localhost:8888"
+config :astarte_pairing, :cfssl_url, "http://localhost:8888"
 
-config :cqerl, :cassandra_nodes,
-  [{System.get_env("CASSANDRA_DB_HOST") || "localhost", System.get_env("CASSANDRA_DB_PORT") || 9042}]
+config :cqerl, :cassandra_nodes, [
+  {System.get_env("CASSANDRA_DB_HOST") || "localhost",
+   System.get_env("CASSANDRA_DB_PORT") || 9042}
+]
