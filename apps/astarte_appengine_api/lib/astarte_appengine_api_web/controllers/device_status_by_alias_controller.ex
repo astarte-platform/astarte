028e817b787eab030548f772756bb2577322f595
Riccardo Binetti <riccardo.binetti@ispirata.com>
2019-07-19 16:33:34 +0200

    Remove all warnings

--- apps/astarte_appengine_api/lib/astarte_appengine_api_web/controllers/device_status_by_alias_controller.ex
+++ apps/astarte_appengine_api/lib/astarte_appengine_api_web/controllers/device_status_by_alias_controller.ex
@@ -38,7 +38,9 @@ defmodule Astarte.AppEngine.APIWeb.DeviceStatusByAliasController do
     with {:ok, device_id} <- Device.device_alias_to_device_id(realm_name, device_alias),
          encoded_device_id <- Base.url_encode64(device_id, padding: false),
          {:ok, device_status_by_alias} <- Device.get_device_status!(realm_name, encoded_device_id) do
-      render(conn, DeviceStatusView, "show.json", device_status: device_status_by_alias)
+      conn
+      |> put_view(DeviceStatusView)
+      |> render("show.json", device_status: device_status_by_alias)
     end
   end
 
@@ -54,7 +56,9 @@ defmodule Astarte.AppEngine.APIWeb.DeviceStatusByAliasController do
            :ok <- Device.merge_device_status!(realm_name, encoded_device_id, data),
            {:ok, %DeviceStatus{} = device_status} <-
              Device.get_device_status!(realm_name, encoded_device_id) do
-        render(conn, DeviceStatusView, "show.json", device_status: device_status)
+        conn
+        |> put_view(DeviceStatusView)
+        |> render("show.json", device_status: device_status)
       end
     else
       {:error, :patch_mimetype_not_supported}
