75b91bde72d8b43985b3ba3cd0788766a179c876
Riccardo Binetti <riccardo.binetti@ispirata.com>
2019-10-23 11:09:16 +0200

    device_status_by_alias_controller: adapt to new merge_device_status

--- apps/astarte_appengine_api/lib/astarte_appengine_api_web/controllers/device_status_by_alias_controller.ex
+++ apps/astarte_appengine_api/lib/astarte_appengine_api_web/controllers/device_status_by_alias_controller.ex
@@ -51,9 +51,8 @@ defmodule Astarte.AppEngine.APIWeb.DeviceStatusByAliasController do
     if get_req_header(conn, "content-type") == ["application/merge-patch+json"] do
       with {:ok, device_id} <- Device.device_alias_to_device_id(realm_name, device_alias),
            encoded_device_id <- Base.url_encode64(device_id, padding: false),
-           :ok <- Device.merge_device_status!(realm_name, encoded_device_id, data),
            {:ok, %DeviceStatus{} = device_status} <-
-             Device.get_device_status!(realm_name, encoded_device_id) do
+             Device.merge_device_status(realm_name, encoded_device_id, data) do
         conn
         |> put_view(DeviceStatusView)
         |> render("show.json", device_status: device_status)
