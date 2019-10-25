245e494f389df5566f9398d5fdffa35f429e4e00
Davide Bettio <davide.bettio@ispirata.com>
2019-10-25 18:28:27 +0200

    Remove unused "require Logger"
    Logger is not required when Logger.info/warn/etc... is not used.

--- apps/astarte_realm_management_api/lib/astarte_realm_management_api/auth/auth.ex
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api/auth/auth.ex
@@ -19,8 +19,6 @@
 defmodule Astarte.RealmManagement.API.Auth do
   alias Astarte.RealmManagement.API.RPC.RealmManagement
 
-  require Logger
-
   def fetch_public_key(realm) do
     RealmManagement.get_jwt_public_key_pem(realm)
   end
