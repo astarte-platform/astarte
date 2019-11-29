95da2f5e6893c4efc2f50da488d157c3116177cf
Riccardo Binetti <riccardo.binetti@ispirata.com>
2019-11-29 17:41:25 +0100

    Add xandra

--- apps/astarte_housekeeping/mix.exs
+++ apps/astarte_housekeeping/mix.exs
@@ -80,6 +80,7 @@ defmodule Astarte.Housekeeping.Mixfile do
       {:cqex, github: "matehat/cqex", ref: "a2c45667108f9b1e8a9c73c5250a04020bf72a30"},
       {:cqerl,
        github: "matehat/cqerl", ref: "6e44b42df1cb0fcf82d8ab4df032c2e7cacb96f9", override: true},
+      {:xandra, "~> 0.13"},
       {:conform, "== 2.5.2"},
       {:distillery, "~> 1.5", runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
