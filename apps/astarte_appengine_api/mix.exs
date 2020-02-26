af60ac2a4ff334fa26169bcb61369c9ec552b9bb
Davide Bettio <davide.bettio@ispirata.com>
2020-02-26 18:09:52 +0100

    Bump version numbers to 0.11.0-rc.0
    Update version numbers and changelog release date.
    
    Signed-off-by: Davide Bettio <davide.bettio@ispirata.com>

--- apps/astarte_appengine_api/mix.exs
+++ apps/astarte_appengine_api/mix.exs
@@ -1,7 +1,7 @@
 #
 # This file is part of Astarte.
 #
-# Copyright 2017 Ispirata Srl
+# Copyright 2017-2020 Ispirata Srl
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ defmodule Astarte.AppEngine.API.Mixfile do
   def project do
     [
       app: :astarte_appengine_api,
-      version: "0.11.0-beta.3",
+      version: "0.11.0-rc.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
