d0314be328657da0f416a35ffe7c8e9044c084c7
Shubham Wadhwa <shubham.wadhwa@ispirata.com>
2020-02-28 15:59:21 +0530

    Added example of sensor sampling rate
    Displays Sensor rate and status data
    Can Update status and sampling period configuration
    Uses "SamplingRate", "Values" interfaces in this example
    Fix #99
    
    Signed-off-by: Shubham Wadhwa <shubham.wadhwa@ispirata.com>

---
+++ examples/sensor-samplingrate/src/components/SensorSamplingUpdate.jsx
@@ -0,0 +1,255 @@
+import React, { Component } from "react";
+import {
+  Accordion,
+  Button,
+  Col,
+  FormCheck,
+  FormControl,
+  FormGroup,
+  Row,
+  Spinner
+} from "react-bootstrap";
+import CredentialsModal from "./CredentialsModal";
+import SensorItems from "./SensorItem";
+
+class SensorSamplingUpdate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      visible: false,
+      current: null
+    };
+  }
+
+  componentDidMount() {
+    const { sensorValues } = this.props;
+    const keys = Object.keys(sensorValues);
+    if (keys.length > 0) this.setState({ current: keys[0], sensor: keys[0] });
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSensor = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+      current: e.target.value
+    });
+  };
+  handleCheckbox = e => {
+    this.setState({ [e.target.name]: e.target.checked });
+  };
+  handleError = err => {
+    if (err.response.status === 403) {
+      this.setState({ visible: true, loading: false });
+    }
+  };
+
+  handleSubmit = () => {
+    this.updateSamplingRateStatus();
+    this.updateSensorRateValue();
+  };
+
+  updateSensorRateValue = () => {
+    const { sensor, status, samplingRate, samplingRateStatus } = this.state;
+    const {
+      data,
+      astarte,
+      samplingRateInterface,
+      refreshSamplingRate
+    } = this.props;
+    if (sensor && (status === "enable" || samplingRateStatus)) {
+      this.setState({ loading: true });
+      astarte
+        .setSensorSamplingById({
+          id: data.id,
+          interfaces: samplingRateInterface,
+          sensor_id: sensor,
+          key: "samplingPeriod",
+          unset: samplingRateStatus,
+          params: {
+            data: parseInt(samplingRate)
+          }
+        })
+        .then(() => {
+          refreshSamplingRate(data.id, samplingRateInterface);
+          this.setState({ loading: false });
+        })
+        .catch(err => this.handleError(err));
+    }
+  };
+
+  updateSamplingRateStatus = () => {
+    const { sensor, status } = this.state;
+    const {
+      data,
+      astarte,
+      samplingRateInterface,
+      refreshSamplingRate
+    } = this.props;
+    if (sensor && status) {
+      this.setState({ loading: true });
+      astarte
+        .setSensorSamplingById({
+          id: data.id,
+          interfaces: samplingRateInterface,
+          sensor_id: sensor,
+          key: "enable",
+          unset: status === "unset",
+          params: {
+            data: status === "enable"
+          }
+        })
+        .then(() => {
+          refreshSamplingRate(data.id, samplingRateInterface);
+          this.setState({ loading: false });
+        })
+        .catch(err => this.handleError(err));
+    }
+  };
+
+  render() {
+    const { sensorValues } = this.props;
+    const {
+      loading,
+      visible,
+      current,
+      status,
+      samplingRateStatus
+    } = this.state;
+    return (
+      <Col xs={12} className="border-separate">
+        <div className="sensor-sampling-div">
+          <Row className="main-row col-sm-9 align-items-center mx-auto my-3">
+            <Col sm={3} xs={12} className={"p-0"}>
+              <span>Sensor:</span>
+            </Col>
+            <Col sm={9} xs={12}>
+              <FormGroup>
+                <FormControl
+                  as="select"
+                  name="sensor"
+                  required
+                  onChange={this.handleSensor}
+                >
+                  {Object.keys(sensorValues).map((item, index) => {
+                    return (
+                      <option selected={!index} key={index}>
+                        {item}
+                      </option>
+                    );
+                  })}
+                </FormControl>
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row className="main-row col-sm-9 align-items-center mx-auto my-3">
+            <Col sm={3} xs={12} className={"p-0"}>
+              <span>Sampling:</span>
+            </Col>
+            <Col sm={9} xs={12}>
+              <FormGroup>
+                <FormCheck
+                  onChange={this.handleChange}
+                  value="enable"
+                  name="status"
+                  inline
+                  label="Enabled"
+                  type="radio"
+                />
+                <FormCheck
+                  onChange={this.handleChange}
+                  value="disable"
+                  name="status"
+                  inline
+                  label="Disabled"
+                  type="radio"
+                />
+                <FormCheck
+                  onChange={this.handleChange}
+                  value="unset"
+                  name="status"
+                  inline
+                  label="Auto"
+                  type="radio"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          {status === "enable" ? (
+            <Row className="main-row col-sm-9 mx-auto my-3">
+              <Col sm={3} xs={12} className={"p-0 pt-3"}>
+                <span>Period:</span>
+              </Col>
+              <Col sm={9} xs={12}>
+                <FormGroup>
+                  <FormCheck
+                    onChange={this.handleCheckbox}
+                    inline
+                    name="samplingRateStatus"
+                    label="Auto"
+                    type="checkbox"
+                    className="mb-3"
+                  />
+                  {!samplingRateStatus ? (
+                    <FormControl
+                      onChange={this.handleChange}
+                      name="samplingRate"
+                      type="number"
+                      placeholder="Sampling period"
+                      min={0}
+                    />
+                  ) : (
+                    ""
+                  )}
+                </FormGroup>
+              </Col>
+            </Row>
+          ) : (
+            ""
+          )}
+          <Row className="main-row col-sm-9 mx-auto my-3  justify-content-center">
+            <Col sm={6} xs={12}>
+              <FormGroup>
+                <Button
+                  disabled={loading}
+                  className="text-uppercase my-3 mx-auto"
+                  type="primary"
+                  onClick={this.handleSubmit}
+                >
+                  {loading ? (
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    "Update"
+                  )}
+                </Button>
+              </FormGroup>
+            </Col>
+          </Row>
+          <CredentialsModal
+            setCredentials={this.props.setCredentials}
+            visible={visible}
+          />
+          <Row className="main-row col-sm-9 mx-auto my-3">
+            <Accordion className="col-12 pl-0">
+              <SensorItems
+                current={current}
+                sensorValues={this.props.sensorValues}
+                sensorSamplingRate={this.props.sensorSamplingRate}
+              />
+            </Accordion>
+          </Row>
+        </div>
+      </Col>
+    );
+  }
+}
+
+export default SensorSamplingUpdate;
---
+++ examples/sensor-samplingrate/src/index.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/cast.css";
+
+import ReactDOM from "react-dom";
+import SensorViewer from "./components/SensorViewer";
+import React from "react";
+
+ReactDOM.render(
+  <div>
+    <SensorViewer />
+  </div>,
+  document.getElementById("root")
+);
