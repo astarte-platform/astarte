5a29822fb8606711037696de7ee8ba36c3d04f32
Davide Bettio <davide.bettio@ispirata.com>
2020-04-06 18:12:01 +0200

    Update astarte_core to v0.11.0
    Update astarte_core dep in mix.exs && mix deps.update astarte_core.

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Astarte.DataAccess.Mixfile do
 
   defp astarte_required_modules(_) do
     [
-      {:astarte_core, github: "astarte-platform/astarte_core", tag: "v0.11.0-rc.1"}
+      {:astarte_core, github: "astarte-platform/astarte_core", tag: "v0.11.0"}
     ]
   end
 
