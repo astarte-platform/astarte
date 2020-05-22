69707fd1ba835515a2592676c9f3bc617d3d2eef
Davide Bettio <davide.bettio@ispirata.com>
2020-05-22 16:48:46 +0200

    RMA: Do not use ecto_morph
    Sadly ecto_morph doesn't work for us, let's fall back to a different
    approach.

--- apps/astarte_realm_management_api/lib/astarte_realm_management_api/triggers/trigger.ex
+++ apps/astarte_realm_management_api/lib/astarte_realm_management_api/triggers/trigger.ex
@@ -20,21 +20,90 @@ defmodule Astarte.RealmManagement.API.Triggers.Trigger do
   use Ecto.Schema
   import Ecto.Changeset
   alias Astarte.Core.Triggers.SimpleTriggerConfig
-  alias Astarte.RealmManagement.API.Triggers.{Action, Trigger}
+  alias Astarte.RealmManagement.API.Triggers.{AMQPAction, HttpAction, Trigger}
 
   @derive {Phoenix.Param, key: :name}
   @primary_key false
   embedded_schema do
     field :name, :string
-    field :action, Action
+    field :action, :any, virtual: true
+    embeds_one :amqp_action, AMQPAction
+    embeds_one :http_action, HttpAction
     embeds_many :simple_triggers, SimpleTriggerConfig
   end
 
   @doc false
   def changeset(%Trigger{} = trigger, attrs) do
+    attrs =
+      attrs
+      |> propagate_realm()
+      |> move_action()
+
     trigger
-    |> cast(attrs, [:name, :action])
-    |> validate_required([:name, :action])
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> cast_embed(:amqp_action, required: false)
+    |> cast_embed(:http_action, required: false)
     |> cast_embed(:simple_triggers, required: true)
+    |> normalize()
+  end
+
+  def propagate_realm(%{realm_name: realm_name, action: action} = attrs) do
+    updated_action = Map.put(action, :realm_name, realm_name)
+    Map.put(attrs, :action, updated_action)
+  end
+
+  def propagate_realm(%{"realm_name" => realm_name, "action" => action} = attrs) do
+    updated_action = Map.put(action, "realm_name", realm_name)
+    Map.put(attrs, "action", updated_action)
+  end
+
+  def propagate_realm(attrs) do
+    attrs
+  end
+
+  def move_action(%{action: action} = attrs) do
+    choose_action_type(attrs, action)
+  end
+
+  def move_action(%{"action" => action} = attrs) do
+    choose_action_type(attrs, action)
+  end
+
+  def choose_action_type(map, action) do
+    type =
+      case action do
+        %{"amqp_exchange" => _} -> "amqp_action"
+        %{amqp_exchange: _} -> :amqp_action
+        %{"http_url" => _} -> "http_action"
+        %{http_url: _} -> :http_action
+        %{"http_post_url" => _} -> "http_action"
+        %{http_post_url: _} -> :http_action
+        _ -> nil
+      end
+
+    map
+    |> Map.delete(type)
+    |> Map.put(type, action)
+  end
+
+  def normalize(changeset) do
+    amqp_action = get_field(changeset, :amqp_action)
+    http_action = get_field(changeset, :http_action)
+
+    case {amqp_action, http_action} do
+      {nil, nil} ->
+        changeset
+
+      {_, nil} ->
+        changeset
+        |> delete_change(:amqp_action)
+        |> put_change(:action, amqp_action)
+
+      {nil, _} ->
+        changeset
+        |> delete_change(:http_action)
+        |> put_change(:action, http_action)
+    end
   end
 end
