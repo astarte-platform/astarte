3fafad0a7e9fc07dffa6ff1aa863194394a882f1
Mattia Pavinati <mattia.pavinati@ispirata.com>
2020-09-21 14:40:13 +0200

    Fix webpack config format and JS minimizer
    - reformat with prettier
    - CSS in production loaded as external file
    - add JS minifier
    
    Signed-off-by: Mattia Pavinati <mattia.pavinati@ispirata.com>

--- astarte-dashboard/webpack.common.js
+++ astarte-dashboard/webpack.common.js
@@ -1,67 +1,49 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-
-const ASSET_PATH = process.env.ASSET_PATH || '/';
-
-const outputPath = path.join(__dirname, 'dist');
-const outputFilename = '[name]-[hash].js';
-
+const Autoprefixer = require('autoprefixer');
 
 module.exports = {
-    output: {
-        path: outputPath,
-        filename: `static/js/${outputFilename}`,
-        publicPath: ASSET_PATH
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.elm'],
-        modules: ['node_modules']
-    },
-    module: {
-        //noParse: /\.elm$/,
-        rules: [{
-            test: /\.(ts|js)x?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: "babel-loader",
-        },
-        {
-            test: /\.(eot|ttf|woff|woff2|svg)$/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    options:
-                    {
-                        name (file) {
-                            return '[name]-[hash].[ext]';
-                        },
-                        context: ''
-                    }
-                }
-            ]
-        }]
-    },
-    plugins: [
-        // This makes it possible for us to safely use env vars on our code
-        new webpack.DefinePlugin({
-            'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
-        }),
-        new webpack.LoaderOptionsPlugin({
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'static/js/[name]-[hash].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.elm', '.scss'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: [/\.ttf$/, /\.woff2?$/, /\.eot$/, /\.svg$/],
+        use: [
+          {
+            loader: 'file-loader',
             options: {
-                postcss: [autoprefixer()]
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: 'src/static/index.html',
-            inject: 'body',
-            filename: 'index.html'
-        })
+              name: '[name]-[hash].[ext]',
+            },
+          },
+        ],
+      },
     ],
-    node: {
-        console: true,
-        fs: "empty",
-        net: "empty",
-        tls: "empty"
-    }
-}
+  },
+  plugins: [
+    Autoprefixer,
+    new HtmlWebpackPlugin({
+      template: 'src/static/index.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+  ],
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
+};
