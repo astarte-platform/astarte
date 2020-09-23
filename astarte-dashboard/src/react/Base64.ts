astarte-platform/astarte

commit d5c561a18d899d7ff68eac8ed0db668f1d0ac0ce

Add Typescript support
- Add typescript dependency
- Use Babel's plugin for typescript
- Setup Webpack to recognize typescript's files and to build it
- Add checks on Typescript code for the Github workflow
- Add types on src/react/Base64.ts so the Typescript compiler doesn't
  error out because it can't find files to type-check

Signed-off-by: Davide Briani <davide.briani@ispirata.com>

astarte-dashboard/src/react/Base64.ts
@@ -42,19 +42,16 @@ const charset = (() => {
   return newCharset;
 })();
 
-export function byteArrayToUrlSafeBase64(bytes) {
+export function byteArrayToUrlSafeBase64(bytes: number[]) {
   const binaryArray = bytes.map((b) => b.toString(2));
 
   const padding = '0'.padEnd(6 - ((bytes.length * 8) % 6), '0');
   const binaryString = binaryArray.map((b) => b.padStart(8, '0')).join('') + padding;
 
-  return binaryString
-    .match(/.{6}/g)
-    .map((b) => charset[parseInt(b, 2)])
-    .join('');
+  return binaryString.match(/.{6}/g) || [].map((b) => charset[parseInt(b, 2)]).join('');
 }
 
-export function urlSafeBase64ToByteArray(base64string) {
+export function urlSafeBase64ToByteArray(base64string: string) {
   let binaryString = '';
   for (let i = 0; i < base64string.length; i += 1) {
     binaryString += charset.indexOf(base64string[i]).toString(2).padStart(6, '0');

astarte-dashboard/.babelrc
@@ -1,3 +1,4 @@
 {
-  "presets": ["@babel/env", "@babel/preset-react"]
+  "presets": ["@babel/env", "@babel/react", ["@babel/typescript", { "isTSX": true, "allExtensions": true }]],
+  "plugins": ["@babel/proposal-class-properties", "@babel/proposal-object-rest-spread", "@babel/proposal-optional-chaining"]
 }

astarte-dashboard/.github/workflows/code-quality.yml
@@ -13,7 +13,7 @@ jobs:
     runs-on: ubuntu-latest
     strategy:
       matrix:
-        node-version: [12.x, 14.x]
+        node-version: [14.x]
     steps:
       - name: Checkout repository
         uses: actions/checkout@v2
@@ -33,3 +33,5 @@ jobs:
         run: npm run check-format
       - name: Check linting
         run: npm run check-lint
+      - name: Check Typescript
+        run: npm run check-types

astarte-dashboard/webpack.common.js
@@ -16,16 +16,15 @@ module.exports = {
         publicPath: ASSET_PATH
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.elm'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.elm'],
         modules: ['node_modules']
     },
     module: {
         //noParse: /\.elm$/,
         rules: [{
-            test: /\.(js|jsx)$/,
+            test: /\.(ts|js)x?$/,
             exclude: /(node_modules|bower_components)/,
             loader: "babel-loader",
-            options: { presets: ["@babel/env"] }
         },
         {
             test: /\.(eot|ttf|woff|woff2|svg)$/,

commit a30a608baf54b915e657609ef1a8a01f90bc5e0b

Base64.ts: fix device-id generation
Fix a regression introduced in d5c561a18d899d7ff68eac8ed0d

Signed-off-by: Mattia Pavinati <mattia.pavinati@ispirata.com>

astarte-dashboard/src/react/Base64.ts
@@ -47,8 +47,9 @@ export function byteArrayToUrlSafeBase64(bytes: number[]) {
 
   const padding = '0'.padEnd(6 - ((bytes.length * 8) % 6), '0');
   const binaryString = binaryArray.map((b) => b.padStart(8, '0')).join('') + padding;
+  const octects = binaryString.match(/.{6}/g) || [];
 
-  return binaryString.match(/.{6}/g) || [].map((b) => charset[parseInt(b, 2)]).join('');
+  return octects.map((b) => charset[parseInt(b, 2)]).join('');
 }
 
 export function urlSafeBase64ToByteArray(base64string: string) {