52e05f68380294c5da465aefc617733925dfbf3f
Davide Briani <davide.briani@ispirata.com>
2020-09-30 15:12:37 +0200

    Use consistent login process in tests to prevent flakiness
    Rework the cy.login() command to be sure to get an authenticated
    context after the command has finished running.
    A session object is injected directly in localStorage so that:
    - There are fewer page loadings needed
    - We don't have to use random waiting times in order to guess wheter
      the app has already saved the authenticated session for later queries
    
    Signed-off-by: Davide Briani <davide.briani@ispirata.com>

--- astarte-dashboard/cypress/support/index.js
+++ astarte-dashboard/cypress/support/index.js
@@ -1,6 +1,22 @@
 Cypress.Commands.add('login', () => {
+  cy.server();
+  cy.route('/user-config/config.json').as('getUserConfig');
   cy.fixture('realm').then((realm) => {
-    cy.visit(`/auth?realm=${realm.name}#access_token=${realm.infinite_token}`);
-    cy.wait(500);
+    const session = {
+      login_type: 'TokenLogin',
+      api_config: {
+        secure_connection: true,
+        realm_management_url: 'api.example.com/realmmanagement',
+        appengine_url: 'api.example.com/appengine',
+        pairing_url: 'api.example.com/pairing',
+        flow_url: 'api.example.com/flow',
+        realm: realm.name,
+        token: realm.infinite_token,
+        enable_flow_preview: true,
+      },
+    };
+    localStorage.session = JSON.stringify(session);
+    cy.visit('/');
+    cy.wait('@getUserConfig');
   });
 });
