c425cdd05db768ca88c007dc9e1bc87619f01b8f
Davide Briani <davide.briani@ispirata.com>
2020-10-14 18:31:13 +0200

    Add tests to check correct handling of network errors
    Add Cypress tests for DeviceInterfaceValues page to ensure
    the Dashboard properly handles errors occurred during
    network requests and shows meaningful alerts
    
    Signed-off-by: Davide Briani <davide.briani@ispirata.com>

--- astarte-dashboard/cypress/integration/interface_value_page_test.js
+++ astarte-dashboard/cypress/integration/interface_value_page_test.js
@@ -20,6 +20,93 @@ describe('Interface values page tests', () => {
       });
     });
 
+    it('correctly handles error while fetching interface', () => {
+      cy.fixture('test_aggregated_object_interface_values').as('interface_data');
+      cy.fixture('test.astarte.AggregatedObjectInterface')
+        .as('interface')
+        .then((interface) => {
+          cy.fixture('device_detailed')
+            .as('device')
+            .then((device) => {
+              const interfaceName = interface.data.interface_name;
+              const deviceId = device.data.id;
+              cy.server();
+              cy.route('GET', `/appengine/v1/*/devices/${deviceId}`, '@device');
+              cy.route(
+                'GET',
+                `/appengine/v1/*/devices/${deviceId}/interfaces/${interfaceName}`,
+                '@interface_data',
+              );
+              cy.route({
+                method: 'GET',
+                url: `/realmmanagement/v1/*/interfaces/${interfaceName}/*`,
+                status: 418,
+                response: '',
+              });
+              cy.visit(`/devices/${deviceId}/interfaces/${interfaceName}`);
+              cy.get('.main-content .card-body [role="alert"]').contains(
+                'Could not retrieve interface properties.',
+              );
+            });
+        });
+    });
+
+    it('correctly handles error while fetching device', () => {
+      cy.fixture('test_aggregated_object_interface_values').as('interface_data');
+      cy.fixture('test.astarte.AggregatedObjectInterface')
+        .as('interface')
+        .then((interface) => {
+          cy.fixture('device_detailed')
+            .as('device')
+            .then((device) => {
+              const interfaceName = interface.data.interface_name;
+              const deviceId = device.data.id;
+              cy.server();
+              cy.route('GET', `/realmmanagement/v1/*/interfaces/${interfaceName}/*`, '@interface');
+              cy.route(
+                'GET',
+                `/appengine/v1/*/devices/${deviceId}/interfaces/${interfaceName}`,
+                '@interface_data',
+              );
+              cy.route({
+                method: 'GET',
+                url: `/appengine/v1/*/devices/${deviceId}`,
+                status: 418,
+                response: '',
+              });
+              cy.visit(`/devices/${deviceId}/interfaces/${interfaceName}`);
+              cy.get('.main-content .card-body [role="alert"]').contains('Device not found.');
+            });
+        });
+    });
+
+    it('correctly handles error while fetching interface data', () => {
+      cy.fixture('test_aggregated_object_interface_values').as('interface_data');
+      cy.fixture('test.astarte.AggregatedObjectInterface')
+        .as('interface')
+        .then((interface) => {
+          cy.fixture('device_detailed')
+            .as('device')
+            .then((device) => {
+              const interfaceName = interface.data.interface_name;
+              const deviceId = device.data.id;
+              cy.server();
+              cy.route('GET', `/realmmanagement/v1/*/interfaces/${interfaceName}/*`, '@interface');
+              cy.route('GET', `/appengine/v1/*/devices/${deviceId}`, '@device');
+              cy.route({
+                method: 'GET',
+                url: `/appengine/v1/*/devices/${deviceId}/interfaces/${interfaceName}`,
+                status: 418,
+                response: '',
+              });
+              cy.visit(`/devices/${deviceId}/interfaces/${interfaceName}`);
+              cy.get('.main-content .card-body [role="alert"]').contains(
+                'Could not retrieve interface data.',
+              );
+            });
+        });
+    });
+
     context('aggregated datastream interface', () => {
       beforeEach(() => {
         cy.fixture('test.astarte.AggregatedObjectInterface').as('interface');
