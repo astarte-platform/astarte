45206a536a451fda5e663ca70daeb20efa70836d
Mattia Mazzucato <matt.mazzucato@gmail.com>
2020-10-16 17:46:08 +0200

    Add bamboo to deps
    Signed-off-by: Mattia Mazzucato <matt.mazzucato@gmail.com>

--- tools/astarte_e2e/mix.exs
+++ tools/astarte_e2e/mix.exs
@@ -62,6 +62,7 @@ defmodule AstarteE2E.MixProject do
       {:logfmt, "~> 3.3"},
       {:pretty_log, "~> 0.1"},
       {:observer_cli, "~> 1.5"},
+      {:bamboo, "~> 1.6"},
       {:dialyzex, github: "Comcast/dialyzex", only: [:dev, :ci]}
     ]
   end
