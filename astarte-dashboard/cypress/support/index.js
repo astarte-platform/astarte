57cd40339f21914194ab22238cbae29fa74f4b83
Mattia Pavinati <mattia.pavinati@ispirata.com>
2020-10-19 13:01:48 +0200

    Add tests for HTTP/HTTPS session handling
    verify that after login, API calls match the protocol specified
    in the dashboard config.
    
    Signed-off-by: Mattia Pavinati <mattia.pavinati@ispirata.com>

--- astarte-dashboard/cypress/support/index.js
+++ astarte-dashboard/cypress/support/index.js
@@ -1,20 +1,23 @@
 // This hook runs before each test of every test suite
 // So this query will be already mocked in every test
 beforeEach(() => {
-  cy.fixture('user_config').then((userConfig) => {
+  // unless overwritten, test expecting https connections
+  cy.fixture('config/https').then((userConfig) => {
     cy.server();
     cy.route('/user-config/config.json', userConfig);
   });
 });
 
-Cypress.Commands.add('login', () => {
-  cy.fixture('user_config').then((userConfig) => {
+Cypress.Commands.add('login', (params = {}) => {
+  const { secure = true } = params;
+  const configFixture = secure ? 'config/https' : 'config/http';
+  cy.fixture(configFixture).then((userConfig) => {
     cy.fixture('realm').then((realm) => {
       const apiUrl = new URL(userConfig.astarte_api_url).hostname;
       const session = {
         login_type: 'TokenLogin',
         api_config: {
-          secure_connection: true,
+          secure_connection: secure,
           realm_management_url: `${apiUrl}/realmmanagement`,
           appengine_url: `${apiUrl}/appengine`,
           pairing_url: `${apiUrl}/pairing`,
