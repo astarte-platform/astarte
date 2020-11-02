4587aca0a77ac74307f7afd6d57bd10a83c76876
Mattia Mazzucato <matt.mazzucato@gmail.com>
2020-11-02 18:53:49 +0100

    Drop `ASTARTE_` prefix from env vars
    Signed-off-by: Mattia Mazzucato <matt.mazzucato@gmail.com>

--- tools/astarte_e2e/lib/astarte_e2e/config.ex
+++ tools/astarte_e2e/lib/astarte_e2e/config.ex
@@ -46,85 +46,85 @@ defmodule AstarteE2E.Config do
 
   @envdoc "Astarte Pairing URL (e.g. https://api.astarte.example.com/pairing)."
   app_env :pairing_url, :astarte_e2e, :pairing_url,
-    os_env: "ASTARTE_E2E_PAIRING_URL",
+    os_env: "E2E_PAIRING_URL",
     type: :binary,
     required: true
 
   @envdoc "An Astarte device ID, which is a valid UUID encoded with standard Astarte device_id encoding (Base64 url encoding, no padding)."
   app_env :device_id, :astarte_e2e, :device_id,
-    os_env: "ASTARTE_E2E_DEVICE_ID",
+    os_env: "E2E_DEVICE_ID",
     type: AstarteDeviceID,
     required: true
 
   @envdoc "Credentials secret."
   app_env :credentials_secret, :astarte_e2e, :credentials_secret,
-    os_env: "ASTARTE_E2E_CREDENTIALS_SECRET",
+    os_env: "E2E_CREDENTIALS_SECRET",
     type: :binary,
     required: true
 
   @envdoc "Ignore SSL errors. Defaults to false. Changing the value to true is not advised for production environments unless you're aware of what you're doing."
   app_env :ignore_ssl_errors, :astarte_e2e, :ignore_ssl_errors,
-    os_env: "ASTARTE_E2E_IGNORE_SSL_ERRORS",
+    os_env: "E2E_IGNORE_SSL_ERRORS",
     type: :boolean,
     default: false
 
   @envdoc "Astarte AppEngine URL (e.g. https://api.astarte.example.com/appengine)."
   app_env :appengine_url, :astarte_e2e, :appengine_url,
-    os_env: "ASTARTE_E2E_APPENGINE_URL",
+    os_env: "E2E_APPENGINE_URL",
     type: :binary,
     required: true
 
   @envdoc "Realm name."
   app_env :realm, :astarte_e2e, :realm,
-    os_env: "ASTARTE_E2E_REALM",
+    os_env: "E2E_REALM",
     type: :binary,
     required: true
 
   @envdoc "The Astarte JWT employed to access Astarte APIs. The token can be generated with: `$ astartectl utils gen-jwt <service> -k <your-private-key>.pem`."
   app_env :jwt, :astarte_e2e, :jwt,
-    os_env: "ASTARTE_E2E_JWT",
+    os_env: "E2E_JWT",
     type: :binary,
     required: true
 
   @envdoc "Time interval between consecutive checks (in seconds)."
   app_env :check_interval_s, :astarte_e2e, :check_interval_s,
-    os_env: "ASTARTE_E2E_CHECK_INTERVAL_SECONDS",
+    os_env: "E2E_CHECK_INTERVAL_SECONDS",
     type: :integer,
     default: 60
 
   @envdoc "The port used to expose AstarteE2E's metrics. Defaults to 4010."
   app_env :port, :astarte_e2e, :port,
-    os_env: "ASTARTE_E2E_PORT",
+    os_env: "E2E_PORT",
     type: :integer,
     default: 4010
 
   @envdoc "Overall number of consistency checks repetitions. Defaults to 0, corresponding to infinite checks."
   app_env :check_repetitions, :astarte_e2e, :check_repetitions,
-    os_env: "ASTARTE_E2E_CHECK_REPETITIONS",
+    os_env: "E2E_CHECK_REPETITIONS",
     type: PositiveIntegerOrInfinity,
     default: :infinity
 
   @envdoc "The amount of time (in seconds) the websocket client is allowed to wait for an incoming message. Defaults to 10 seconds."
   app_env :client_timeout_s, :astarte_e2e, :client_timeout_s,
-    os_env: "ASTARTE_E2E_CLIENT_TIMEOUT_SECONDS",
+    os_env: "E2E_CLIENT_TIMEOUT_SECONDS",
     type: :pos_integer,
     default: 10
 
   @envdoc "The maximum number of consecutive timeouts before the websocket client is allowed to crash. Defaults to 10."
   app_env :client_max_timeouts, :astarte_e2e, :client_max_timeouts,
-    os_env: "ASTARTE_E2E_CLIENT_MAX_TIMEOUTS",
+    os_env: "E2E_CLIENT_MAX_TIMEOUTS",
     type: :pos_integer,
     default: 10
 
   @envdoc "The comma-separated email recipients."
   app_env :mailer_to_address, :astarte_e2e, :mailer_to_address,
-    os_env: "ASTARTE_E2E_MAILER_TO_ADDRESS",
+    os_env: "E2E_MAILER_TO_ADDRESS",
     type: ListOfStrings,
     default: ""
 
   @envdoc "The notification email sender."
   app_env :mailer_from_address, :astarte_e2e, :mailer_from_address,
-    os_env: "ASTARTE_E2E_MAILER_FROM_ADDRESS",
+    os_env: "E2E_MAILER_FROM_ADDRESS",
     type: :binary,
     default: ""
 
@@ -133,21 +133,21 @@ defmodule AstarteE2E.Config do
   service.
   """
   app_env :mail_api_key, :astarte_e2e, :mail_api_key,
-    os_env: "ASTARTE_E2E_MAIL_API_KEY",
+    os_env: "E2E_MAIL_API_KEY",
     type: :binary
 
   @envdoc """
   The mail domain. This env var must be set and valid to use the mailgun service.
   """
   app_env :mail_domain, :astarte_e2e, :mail_domain,
-    os_env: "ASTARTE_E2E_MAIL_DOMAIN",
+    os_env: "E2E_MAIL_DOMAIN",
     type: :binary
 
   @envdoc """
   The mail API base URI. This env var must be set and valid to use the mail service.
   """
   app_env :mail_api_base_uri, :astarte_e2e, :mail_api_base_uri,
-    os_env: "ASTARTE_E2E_MAIL_API_BASE_URI",
+    os_env: "E2E_MAIL_API_BASE_URI",
     type: :binary
 
   @envdoc """
@@ -155,7 +155,7 @@ defmodule AstarteE2E.Config do
   This env var must be set and valid to use the mail service.
   """
   app_env :mail_service, :astarte_e2e, :mail_service,
-    os_env: "ASTARTE_E2E_MAIL_SERVICE",
+    os_env: "E2E_MAIL_SERVICE",
     type: BambooMailAdapter
 
   @spec websocket_url() :: {:ok, String.t()}
