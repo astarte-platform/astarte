7c2d88332c3754062f57bd34d58c5aca7bf81cb4
Davide Briani <davide.briani@ispirata.com>
2021-01-13 18:04:28 +0100

    Enhance content loading for NewGroupPage
    - use useFetch and WaitForData to abstract loading logic
    - show a properly positioned spinner to indicate a loading in
      progress
    - show a properly positioned message on loading error, together
      with a Retry button
    
    Signed-off-by: Davide Briani <davide.briani@ispirata.com>

--- astarte-dashboard/src/react/NewGroupPage.tsx
+++ astarte-dashboard/src/react/NewGroupPage.tsx
@@ -16,35 +16,17 @@
    limitations under the License.
 */
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Form, InputGroup, Spinner } from 'react-bootstrap';
+import { Button, Container, Form, InputGroup, Spinner } from 'react-bootstrap';
 import AstarteClient, { AstarteDevice } from 'astarte-client';
 
 import { useAlerts } from './AlertManager';
+import Empty from './components/Empty';
+import WaitForData from './components/WaitForData';
+import useFetch from './hooks/useFetch';
 import SingleCardPage from './ui/SingleCardPage';
 import CheckableDeviceTable from './ui/CheckableDeviceTable';
-
-interface NewGroupPageWrapperProps {
-  phase: 'ok' | 'loading' | 'err';
-  children: React.ReactNode;
-}
-
-const NewGroupPageWrapper = ({ phase, children }: NewGroupPageWrapperProps): React.ReactElement => {
-  let innerHtml;
-  if (phase === 'ok') {
-    innerHtml = children;
-  } else if (phase === 'err') {
-    innerHtml = <p>Couldn&apos;t load the device list</p>;
-  } else {
-    innerHtml = <Spinner animation="border" role="status" />;
-  }
-  return (
-    <SingleCardPage title="Create a New Group" backLink="/groups">
-      {innerHtml}
-    </SingleCardPage>
-  );
-};
 
 interface GroupNameFormGroupProps {
   groupName: string;
@@ -107,26 +89,15 @@ interface Props {
 }
 
 export default ({ astarte }: Props): React.ReactElement => {
-  const [phase, setPhase] = useState<'ok' | 'loading' | 'err'>('loading');
   const [groupName, setGroupName] = useState('');
   const [deviceFilter, setDeviceFilter] = useState('');
-  const [devices, setDevices] = useState<AstarteDevice[]>([]);
   const [selectedDevices, setSelectedDevices] = useState<Set<AstarteDevice['id']>>(new Set());
   const [isCreatingGroup, setIsCreatingGroup] = useState(false);
+  const devicesFetcher = useFetch(() =>
+    astarte.getDevices({ details: true }).then(({ devices }) => devices),
+  );
   const formAlerts = useAlerts();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    astarte
-      .getDevices({ details: true })
-      .then((response) => {
-        setDevices(response.devices);
-        setPhase('ok');
-      })
-      .catch(() => {
-        setPhase('err');
-      });
-  }, [astarte]);
 
   const createGroup = (event: React.FormEvent<HTMLElement>) => {
     event.preventDefault();
@@ -163,37 +134,52 @@ export default ({ astarte }: Props): React.ReactElement => {
   const isValidForm = isValidGroupName && selectedDeviceCount > 0;
 
   return (
-    <NewGroupPageWrapper phase={phase}>
+    <SingleCardPage title="Create a New Group" backLink="/groups">
       <formAlerts.Alerts />
-      <Form onSubmit={createGroup}>
-        <GroupNameFormGroup groupName={groupName} onGroupNameChange={setGroupName} />
-        <div className="table-toolbar p-1">
-          <span>
-            {selectedDeviceCount > 0
-              ? `${selectedDeviceCount} ${
-                  selectedDeviceCount === 1 ? 'device' : 'devices'
-                } selected.`
-              : 'Please select at least one device.'}
-          </span>
-          <div className="float-right">
-            <FilterInputBox filter={deviceFilter} onFilterChange={setDeviceFilter} />
-          </div>
-        </div>
-        <CheckableDeviceTable
-          filter={deviceFilter}
-          devices={devices}
-          selectedDevices={selectedDevices}
-          onToggleDevice={handleDeviceToggle}
-        />
-        <Form.Row className="flex-row-reverse pr-2">
-          <Button variant="primary" type="submit" disabled={!isValidForm || isCreatingGroup}>
-            {isCreatingGroup && (
-              <Spinner as="span" size="sm" animation="border" role="status" className="mr-2" />
-            )}
-            Create group
-          </Button>
-        </Form.Row>
-      </Form>
-    </NewGroupPageWrapper>
+      <WaitForData
+        data={devicesFetcher.value}
+        status={devicesFetcher.status}
+        fallback={
+          <Container fluid className="text-center">
+            <Spinner animation="border" role="status" />
+          </Container>
+        }
+        errorFallback={
+          <Empty title="Couldn't load the device list" onRetry={devicesFetcher.refresh} />
+        }
+      >
+        {(devices) => (
+          <Form onSubmit={createGroup}>
+            <GroupNameFormGroup groupName={groupName} onGroupNameChange={setGroupName} />
+            <div className="table-toolbar p-1">
+              <span>
+                {selectedDeviceCount > 0
+                  ? `${selectedDeviceCount} ${
+                      selectedDeviceCount === 1 ? 'device' : 'devices'
+                    } selected.`
+                  : 'Please select at least one device.'}
+              </span>
+              <div className="float-right">
+                <FilterInputBox filter={deviceFilter} onFilterChange={setDeviceFilter} />
+              </div>
+            </div>
+            <CheckableDeviceTable
+              filter={deviceFilter}
+              devices={devices}
+              selectedDevices={selectedDevices}
+              onToggleDevice={handleDeviceToggle}
+            />
+            <Form.Row className="flex-row-reverse pr-2">
+              <Button variant="primary" type="submit" disabled={!isValidForm || isCreatingGroup}>
+                {isCreatingGroup && (
+                  <Spinner as="span" size="sm" animation="border" role="status" className="mr-2" />
+                )}
+                Create group
+              </Button>
+            </Form.Row>
+          </Form>
+        )}
+      </WaitForData>
+    </SingleCardPage>
   );
 };
