4aee7e650e6fc31b9e41cc4ff4d7c30e3b37ec1e
Davide Bettio <davide.bettio@ispirata.com>
2021-05-10 16:05:53 +0200

    mix.exs/mix.lock: update version number + update Astarte deps
    Bump version number to 1.0.0-rc.0, and update astarte_core, astarte_rpc
    and astarte_data_access 1.0.0-rc.0.

--- apps/astarte_pairing_api/mix.exs
+++ apps/astarte_pairing_api/mix.exs
@@ -22,7 +22,7 @@ defmodule Astarte.Pairing.API.Mixfile do
   def project do
     [
       app: :astarte_pairing_api,
-      version: "1.0.0-beta.2",
+      version: "1.0.0-rc.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -70,8 +70,8 @@ defmodule Astarte.Pairing.API.Mixfile do
 
   defp astarte_required_modules(_) do
     [
-      {:astarte_core, "~> 1.0.0-beta.2"},
-      {:astarte_rpc, github: "astarte-platform/astarte_rpc", branch: "release-1.0"}
+      {:astarte_core, "~> 1.0.0-rc.0"},
+      {:astarte_rpc, "~> 1.0.0-rc.0"}
     ]
   end
 
--- doc/mix.exs
+++ doc/mix.exs
@@ -4,7 +4,7 @@ defmodule Doc.MixProject do
   def project do
     [
       app: :doc,
-      version: "1.0.0-beta.2",
+      version: "1.0.0-rc.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
