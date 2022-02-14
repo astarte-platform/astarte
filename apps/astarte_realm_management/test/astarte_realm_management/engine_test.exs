astarte-platform/astarte

commit 8e37f9c94894e0e4933d69b3b72381f73fac19e1

realm_management: allow retention and expiry changes in minor update
Those properties are purely device side so they should not be considered
breaking when updating an interface to a different minor version

Signed-off-by: Riccardo Binetti <riccardo.binetti@secomind.com>

apps/astarte_realm_management/lib/astarte_realm_management/engine.ex
@@ -323,11 +323,20 @@ defmodule Astarte.RealmManagement.Engine do
   defp is_mapping_updated?(mapping, upd_mapping) do
     mapping.explicit_timestamp != upd_mapping.explicit_timestamp or
       mapping.doc != upd_mapping.doc or
-      mapping.description != upd_mapping.description
+      mapping.description != upd_mapping.description or
+      mapping.retention != upd_mapping.retention or
+      mapping.expiry != upd_mapping.expiry
   end
 
   defp drop_mapping_negligible_fields(%Mapping{} = mapping) do
-    %{mapping | doc: nil, description: nil, explicit_timestamp: false}
+    %{
+      mapping
+      | doc: nil,
+        description: nil,
+        explicit_timestamp: false,
+        retention: nil,
+        expiry: nil
+    }
   end
 
   def delete_interface(realm_name, name, major, opts \\ []) do

commit ce8072a1f5b77ab15086a94b9ecdbe5151574075

realm_management: test retention and expiry update
Ensure there are no regressions for the feature introduced in
8e37f9c94894e0e4933d69b3b72381f73fac19e1.

Signed-off-by: Riccardo Binetti <riccardo.binetti@secomind.com>

apps/astarte_realm_management/test/astarte_realm_management/engine_test.exs
@@ -379,9 +379,10 @@ defmodule Astarte.RealmManagement.EngineTest do
         {
             "endpoint": "/%{sensor_id}/value",
             "type": "double",
-            "explicit_timestamp": false,
             "description": "Updated description.",
-            "doc": "Updated docs."
+            "doc": "Updated docs.",
+            "retention": "stored",
+            "expiry": 7200
         }
     ]
   }
@@ -402,7 +403,9 @@ defmodule Astarte.RealmManagement.EngineTest do
             "type": "string",
             "explicit_timestamp": false,
             "description": "Updated description.",
-            "doc": "Updated docs."
+            "doc": "Updated docs.",
+            "retention": "stored",
+            "expiry": 7200
         }
     ]
   }
@@ -702,7 +705,7 @@ defmodule Astarte.RealmManagement.EngineTest do
              {:ok, [[major_version: 0, minor_version: 15]]}
   end
 
-  test "update explicit timestamp, doc and description for individual datastream interface" do
+  test "update explicit timestamp, doc, description, expiry and retention for individual datastream interface" do
     assert Engine.install_interface("autotestrealm", @test_interface_d_0) == :ok
 
     assert Engine.get_interfaces_list("autotestrealm") == {:ok, ["org.astarte-platform.Values"]}
@@ -711,6 +714,11 @@ defmodule Astarte.RealmManagement.EngineTest do
              {:ok, [[major_version: 1, minor_version: 0]]}
 
     assert Engine.update_interface("autotestrealm", @test_interface_d_1) == :ok
+
+    {:ok, updated_interface} =
+      unpack_source(Engine.interface_source("autotestrealm", "org.astarte-platform.Values", 1))
+
+    assert {:ok, ^updated_interface} = unpack_source({:ok, @test_interface_d_1})
 
     assert Engine.list_interface_versions("autotestrealm", "org.astarte-platform.Values") ==
              {:ok, [[major_version: 1, minor_version: 1]]}