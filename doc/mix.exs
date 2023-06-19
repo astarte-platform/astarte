3189b0b963ba8bac85d674e6c1d4983b528d4cb8
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2023-06-19 18:15:06 +0200

    Bump version numbers to 1.1.0
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- doc/mix.exs
+++ doc/mix.exs
@@ -10,7 +10,7 @@ defmodule Doc.MixProject do
 
     [
       app: :doc,
-      version: "1.1.0-rc.0",
+      version: "1.1.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
