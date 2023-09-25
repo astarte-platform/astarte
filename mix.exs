9b829f746922504e7ee5142c6cd12a1213c55493
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2023-09-25 15:28:37 +0200

    Prepare 1.0.5 release
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Astarte.DataAccess.Mixfile do
   def project do
     [
       app: :astarte_data_access,
-      version: "1.0.4",
+      version: "1.0.5",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
