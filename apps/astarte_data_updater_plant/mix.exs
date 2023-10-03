a4608698d2ed5800021fd2afdc0dc02b0134b928
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2023-10-03 16:48:26 +0200

    DUP: Update astarte_core
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- apps/astarte_data_updater_plant/mix.exs
+++ apps/astarte_data_updater_plant/mix.exs
@@ -63,7 +63,7 @@ defmodule Astarte.DataUpdaterPlant.Mixfile do
 
   defp astarte_required_modules(_) do
     [
-      {:astarte_core, github: "astarte-platform/astarte_core", branch: "release-1.1"},
+      {:astarte_core, "~> 1.1.1"},
       {:astarte_data_access,
        github: "astarte-platform/astarte_data_access", branch: "release-1.1"},
       {:astarte_rpc, "~> 1.1"}
