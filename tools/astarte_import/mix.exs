ee997cbee86f62deb55d2c2c6d983aa359dc9e84
Osman Hadzic <osman.hadzic@secomind.com>
2023-11-06 09:23:02 +0100

    AstarteImport: update to Elixir 1.14.x
    Signed-off-by: Osman Hadzic <osman.hadzic@secomind.com>

--- tools/astarte_import/mix.exs
+++ tools/astarte_import/mix.exs
@@ -5,7 +5,7 @@ defmodule Astarte.Import.MixProject do
     [
       app: :astarte_import,
       version: "1.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
