6f69b44facfd0bf2caf54dadce4acc90374afe3c
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2023-11-21 12:54:35 +0100

    Update Elixir to 1.15.7
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Astarte.DataAccess.Mixfile do
     [
       app: :astarte_data_access,
       version: "1.2.0-dev",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
