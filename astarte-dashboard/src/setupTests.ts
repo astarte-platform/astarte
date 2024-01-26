f43655e1eaf209840ba45e70936785f0774bb5de
Davide Briani <davide.briani@secomind.com>
2024-01-26 18:09:22 +0100

    Migrate tests to Vitest
    - Update linters to use types for Vitest instead of Jest.
    - Update the `jest:test` NPM command to a `test` command that runs Vitest.
    - Update references of `jest` to `vi` in tests.
    - Add an `afterEach` hook in `setupTests.tsx` to ensure React trees
    mounted in tests with `render` from `@testing-libray/react` are
    unmounted after each test.
    
    Signed-off-by: Davide Briani <davide.briani@secomind.com>

--- astarte-dashboard/src/setupTests.ts
+++ astarte-dashboard/src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { cleanup } from '@testing-library/react';
+
+afterEach(() => {
+  cleanup();
+});
