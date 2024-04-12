astarte-platform/astarte-dashboard

Update Bootstrap classes used in the code
Apply the changes suggested by Bootstrap's migration path:
- Renamed .no-gutters to .g-0.
- Renamed .float-left and .float-right to .float-start and .float-end.
- Renamed .border-left and .border-right to .border-start and
.border-end.
- Renamed .rounded-left and .rounded-right to .rounded-start and
.rounded-end.
- Renamed .ml-* and .mr-* to .ms-* and .me-*.
- Renamed .pl-* and .pr-* to .ps-* and .pe-*.
- Renamed .text-monospace to .font-monospace.

Signed-off-by: Davide Briani <davide.briani@secomind.com>

src/Sidebar.tsx
@@ -28,7 +28,7 @@ interface SidebarApiStatusProps {
 }
 
 const SidebarApiStatus = ({ healthy, realm }: SidebarApiStatusProps) => (
-  <NavItem className="nav-status pl-4">
+  <NavItem className="nav-status ps-4">
     <div>
       <b>Realm</b>
     </div>
@@ -37,7 +37,7 @@ const SidebarApiStatus = ({ healthy, realm }: SidebarApiStatusProps) => (
       <b>API Status</b>
     </div>
     <p className="my-1">
-      <Icon icon={healthy ? 'statusConnected' : 'statusDisconnected'} className="mr-2" />
+      <Icon icon={healthy ? 'statusConnected' : 'statusDisconnected'} className="me-2" />
       {healthy ? 'Up and running' : 'Degraded'}
     </p>
   </NavItem>
@@ -77,7 +77,7 @@ const SidebarItem = ({ icon, label, link }: SidebarItemProps) => {
 
   return (
     <NavLink as={Link} to={link} active={isSelected}>
-      <Icon icon={icon} className="mr-2" />
+      <Icon icon={icon} className="me-2" />
       {label}
     </NavLink>
   );

astarte-platform/astarte

astarte-dashboard/src/Sidebar.tsx
@@ -55,7 +55,7 @@ const SidebarApiStatus = () => {
   }
 
   return (
-    <NavItem className="nav-status pl-4">
+    <NavItem className="nav-status ps-4">
       <div>
         <b>Realm</b>
       </div>
@@ -82,7 +82,7 @@ const SidebarApiStatus = () => {
         <b>API Status</b>
       </div>
       <p className="my-1">
-        <Icon icon={isApiHealthy ? 'statusConnected' : 'statusDisconnected'} className="mr-2" />
+        <Icon icon={isApiHealthy ? 'statusConnected' : 'statusDisconnected'} className="me-2" />
         {isApiHealthy ? 'Up and running' : 'Degraded'}
       </p>
     </NavItem>
@@ -123,7 +123,7 @@ const SidebarItem = ({ icon, label, link }: SidebarItemProps) => {
 
   return (
     <NavLink as={Link} to={link} active={isSelected}>
-      <Icon icon={icon} className="mr-2" />
+      <Icon icon={icon} className="me-2" />
       {label}
     </NavLink>
   );