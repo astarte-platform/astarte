4185631dbbcbf6667eca3a6aa464952e78016612
Gabriele Ghio <gabriele.ghio@secomind.com>
2024-10-03 09:50:23 +0200

    create core feature

---
+++ tools/astarte_dev_tool/lib/utilities/node.ex
@@ -0,0 +1,36 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2024 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+defmodule AstarteDevTool.Utilities.Node do
+  @host_regex ~r/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/
+  @ip_regex ~r/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
+  @port_regex ~r/^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/
+
+  def parse_nodes(list), do: parse_nodes(list, [])
+
+  defp parse_nodes([], acc), do: {:ok, acc}
+
+  defp parse_nodes([str | t], acc) do
+    with [host, port] <- String.split(str, ":"),
+         true <- (host =~ @host_regex or host =~ @ip_regex) and port =~ @port_regex do
+      parse_nodes(t, [{host, port} | acc])
+    else
+      _ -> {:error, "Invalid format"}
+    end
+  end
+end
