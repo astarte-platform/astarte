4f11d74cf295b781d2fdd746815037bf39c3fd4e
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2024-10-10 16:30:37 +0200

    DUP on Mississippi: adapt tests to the new architecture
    Mississippi takes away the burden of handling message order from DUP,
    allowing to focus just on what to do once a message is received.
    Perform a cleanup on test files (esp. `data_updater_test.exs`)
    so that the handling is left to Mississippi.
    Also, reflect the new volatile trigger handling setup (see
    previous commits) on a new test file, `volatile_triggers_handler_test.exs`.
    
    The structure of existing tests has not been changed.
    However, the number of Mississippi testing queues has been reduced to 1
    (safe thing to do, as before there were no testing queues).
    
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- apps/astarte_data_updater_plant/config/test.exs
+++ apps/astarte_data_updater_plant/config/test.exs
@@ -11,3 +11,7 @@ config :logger, :console,
 
 config :astarte_data_updater_plant, :astarte_instance_id, "test"
 config :astarte_data_updater_plant, :rpc_client, MockRPCClient
+
+config :astarte_data_updater_plant, :amqp_data_queue_total_count, 1
+config :astarte_data_updater_plant, :amqp_data_queue_range_end, 0
+config :astarte_data_updater_plant, :amqp_data_queue_range_start, 0
