bf1fdb7809525280119a838126d65f1591e81840
Armin Ahmetović <armin.ahmetovic@secomind.com>
2025-01-13 09:27:19 +0100

    Handle API authentication errors for the AppEngine API
    - Expand the Fallback controller with some failed
      auth functions
    - Add additional error views with useful feedback
      messages
    - Add the gen_jwt_token_with_wrong_signature
      function in the JWTTestHelper
    - Add the necessary auth tests and adapt existing
      ones
    - Add new errors to the OpenAPI schema
    
    Signed-off-by: Armin Ahmetović <armin.ahmetovic@secomind.com>

--- apps/astarte_appengine_api/test/astarte_appengine_api_web/auth/auth_test.exs
+++ apps/astarte_appengine_api/test/astarte_appengine_api_web/auth/auth_test.exs
@@ -53,7 +53,7 @@ defmodule Astarte.AppEngine.APIWeb.AuthTest do
   describe "JWT" do
     test "no token returns 401", %{conn: conn} do
       conn = get(conn, @request_path)
-      assert json_response(conn, 401)["errors"]["detail"] == "Unauthorized"
+      assert json_response(conn, 401)["errors"]["detail"] == "Missing authorization token"
     end
 
     test "all access token returns the data", %{conn: conn} do
@@ -113,7 +113,8 @@ defmodule Astarte.AppEngine.APIWeb.AuthTest do
         )
         |> get("#{@request_path}/with/suffix")
 
-      assert json_response(conn, 403)["errors"]["detail"] == "Forbidden"
+      assert json_response(conn, 403)["errors"]["detail"] ==
+               "Unauthorized access to #{conn.assigns.method} #{conn.assigns.path}. Please verify your permissions"
     end
 
     test "token for another device returns 403", %{conn: conn} do
@@ -125,7 +126,8 @@ defmodule Astarte.AppEngine.APIWeb.AuthTest do
         )
         |> get(@request_path)
 
-      assert json_response(conn, 403)["errors"]["detail"] == "Forbidden"
+      assert json_response(conn, 403)["errors"]["detail"] ==
+               "Unauthorized access to #{conn.assigns.method} #{conn.assigns.path}. Please verify your permissions"
     end
 
     test "token for both devices returns the data", %{conn: conn} do
@@ -149,7 +151,8 @@ defmodule Astarte.AppEngine.APIWeb.AuthTest do
         )
         |> get(@request_path)
 
-      assert json_response(conn, 403)["errors"]["detail"] == "Forbidden"
+      assert json_response(conn, 403)["errors"]["detail"] ==
+               "Unauthorized access to #{conn.assigns.method} #{conn.assigns.path}. Please verify your permissions"
     end
 
     test "token with generic matching regexp returns the data", %{conn: conn} do
@@ -176,6 +179,28 @@ defmodule Astarte.AppEngine.APIWeb.AuthTest do
         |> get(@request_path)
 
       assert json_response(conn, 200)["data"] == @expected_data
+    end
+
+    test "invalid JWT token returns 401", %{conn: conn} do
+      conn =
+        put_req_header(
+          conn,
+          "authorization",
+          "bearer invalid_token"
+        )
+        |> get(@request_path)
+
+      assert json_response(conn, 401)["errors"]["detail"] == "Invalid JWT token"
+    end
+
+    test "token with mismatched signature returns 401", %{conn: conn} do
+      token = JWTTestHelper.gen_jwt_token_with_wrong_signature(["^GET$::#{@valid_auth_path}"])
+
+      conn =
+        put_req_header(conn, "authorization", "bearer #{token}")
+        |> get(@request_path)
+
+      assert json_response(conn, 401)["errors"]["detail"] == "Invalid JWT token"
     end
   end
 end
