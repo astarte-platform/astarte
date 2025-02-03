48418d54b6c7e025f314a5c7d8e2564e0e31466b
Francesco Noacco <francesco.noacco@secomind.com>
2025-02-03 10:22:24 +0100

    refactor(appengine): port fetch_public_key to exandra
    this function needs some special attention because by default Ecto SQL
    raises if we get an error back from exandra.
    
    as this function is also supposed to handle the situation where the
    keyspace does not exist, which results in a Xandra.Error being generated,
    we need to use a _workaround_ which lets us handle the error ourselves.
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- apps/astarte_appengine_api/lib/astarte_appengine_api/queries.ex
+++ apps/astarte_appengine_api/lib/astarte_appengine_api/queries.ex
@@ -1,7 +1,7 @@
 #
 # This file is part of Astarte.
 #
-# Copyright 2017 Ispirata Srl
+# Copyright 2017 - 2025 SECO Mind Srl
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,27 +20,20 @@ defmodule Astarte.AppEngine.API.Queries do
   alias CQEx.Result, as: DatabaseResult
   alias Astarte.AppEngine.API.Config
   alias Astarte.Core.CQLUtils
+  alias Astarte.DataAccess.Astarte.KvStore
+  alias Astarte.DataAccess.Astarte.Realm
+
+  import Ecto.Query
 
   require Logger
 
-  def fetch_public_key(client) do
-    query =
-      DatabaseQuery.new()
-      |> DatabaseQuery.statement(
-        "SELECT blobAsVarchar(value) FROM kv_store WHERE group='auth' AND key='jwt_public_key_pem'"
-      )
+  def fetch_public_key(realm_name) do
+    keyspace = Realm.keyspace_name(realm_name)
 
-    result =
-      DatabaseQuery.call!(client, query)
-      |> DatabaseResult.head()
-
-    case result do
-      ["system.blobasvarchar(value)": public_key] ->
-        {:ok, public_key}
-
-      :empty_dataset ->
-        {:error, :public_key_not_found}
-    end
+    from r in KvStore,
+      prefix: ^keyspace,
+      select: fragment("blobAsVarchar(?)", r.value),
+      where: r.group == "auth" and r.key == "jwt_public_key_pem"
   end
 
   def check_astarte_health(client, consistency) do
