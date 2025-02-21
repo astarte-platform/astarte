f50a0d1cc5fdebca783c50619d665518fffb9760
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-02-21 14:37:50 +0100

    feat(ecto): add ecto schemas
    Adds ecto schemas copied form `astarte-platform/astarte_data_access#71`.
    These should be removed once the DB access is entirely manages trough
    `astarte_data_access`.
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

---
+++ apps/astarte_realm_management/lib/astarte_realm_management/realms/interface.ex
@@ -0,0 +1,45 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2025 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# TODO: Copied from astarte_data_access PR #71, see: https://github.com/astarte-platform/astarte_data_access/pull/71
+# use `astarte_data_access` when it will be merged
+defmodule Astarte.RealmManagement.Realms.Interface do
+  use TypedEctoSchema
+
+  alias Astarte.Core.Interface.Aggregation
+  alias Astarte.Core.Interface.Ownership
+  alias Astarte.Core.Interface.Type
+  alias Astarte.Core.StorageType
+
+  @primary_key false
+  typed_schema "interfaces" do
+    field :name, :string, primary_key: true
+    field :major_version, :integer, primary_key: true
+    field :aggregation, Aggregation
+    field :automaton_accepting_states, :binary
+    field :automaton_transitions, :binary
+    field :description, :string
+    field :doc, :string
+    field :interface_id, Astarte.DataAccess.UUID
+    field :minor_version, :integer
+    field :ownership, Ownership
+    field :storage, :string
+    field :storage_type, StorageType
+    field :type, Type
+  end
+end
