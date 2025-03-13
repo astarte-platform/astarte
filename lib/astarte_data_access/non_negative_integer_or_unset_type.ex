7d213994ec9d5650b1f21b458eed7ec67e4e0d72
Eddy Babetto <eddy.babetto@secomind.com>
2025-03-13 11:32:44 +0100

    update copyright headers
    Signed-off-by: Eddy Babetto <eddy.babetto@secomind.com>

--- lib/astarte_data_access/non_negative_integer_or_unset_type.ex
+++ lib/astarte_data_access/non_negative_integer_or_unset_type.ex
@@ -1,7 +1,7 @@
 #
 # This file is part of Astarte.
 #
-# Copyright 2024 SECO Mind srl
+# Copyright 2024 - 2025 SECO Mind srl
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
