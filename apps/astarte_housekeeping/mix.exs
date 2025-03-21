a8ad0350c8823cc42ca5473a17d8e39f85200fe9
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2025-03-21 11:43:16 +0100

    HK: Update Astarte Data Access and remove outated deps
    This also includes a small bump to astarte_core.
    Got rid of old dependencies with
    ```
    mix deps.unlock --unused && mix deps.get
    ```
    
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- apps/astarte_housekeeping/mix.exs
+++ apps/astarte_housekeeping/mix.exs
@@ -71,7 +71,7 @@ defmodule Astarte.Housekeeping.Mixfile do
 
   defp astarte_required_modules(_) do
     [
-      {:astarte_core, "~> 1.2"},
+      {:astarte_core, github: "astarte-platform/astarte_core", branch: "release-1.2"},
       {:astarte_data_access,
        github: "astarte-platform/astarte_data_access", branch: "release-1.2"},
       {:astarte_rpc, "~> 1.2"}
