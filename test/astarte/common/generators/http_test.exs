f87064da91e13336e9e58ac873dfc3ff47754144
Francesco Noacco <francesco.noacco@secomind.com>
2025-04-23 18:21:22 +0200

    feat(http): init with url generator
    implement url generator as defined per RFC 3986
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

---
+++ test/astarte/common/generators/http_test.exs
@@ -0,0 +1,38 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2025 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+defmodule Astarte.Common.Generators.HTTPTest do
+  use ExUnit.Case, async: true
+  use ExUnitProperties
+
+  alias Astarte.Common.Generators.HTTP
+
+  @moduletag :common
+  @moduletag :http
+
+  describe "url generator" do
+    property "generates valid urls" do
+      check all url <- HTTP.url(), max_runs: 100 do
+        assert %URI{scheme: scheme, authority: authority} = URI.parse(url)
+
+        assert scheme != nil
+        assert authority != nil
+      end
+    end
+  end
+end
