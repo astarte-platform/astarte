a31437188e34ee8293f4aaf4072d4c7510de22c9
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-05-07 16:48:55 +0200

    test(AE): test interface publishing
    Adds property based testing for interface publishing
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

---
+++ apps/astarte_appengine_api/test/astarte_appengine_api_web/controllers/interface_controller_test.exs
@@ -0,0 +1,103 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2025 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule Astarte.AppEngine.APIWeb.InterfaceControllerTest do
+  @moduledoc false
+  alias Astarte.AppEngine.API.Device.InterfaceValues
+  alias Astarte.DataAccess.Realms.Realm
+  alias Astarte.DataAccess.KvStore
+  alias Astarte.AppEngine.API.Device
+  alias Astarte.Helpers.JWT
+
+  use Astarte.Cases.Data, async: true
+  use Astarte.Cases.Device
+  use Astarte.Cases.Conn
+  use ExUnitProperties
+  use Mimic
+
+  import Astarte.InterfaceUpdateGenerators
+
+  setup_all %{realm_name: realm} do
+    keyspace = Realm.keyspace_name(realm)
+
+    %{
+      group: "auth",
+      key: "jwt_public_key_pem",
+      value: JWT.public_key_pem()
+    }
+    |> KvStore.insert(prefix: keyspace)
+
+    :ok
+  end
+
+  setup %{conn: conn} do
+    authorized_conn =
+      conn
+      |> put_req_header("accept", "application/json")
+      |> put_req_header("authorization", "bearer #{JWT.gen_jwt_all_access_token()}")
+
+    {:ok, auth_conn: authorized_conn}
+  end
+
+  describe "Interface controller" do
+    property "publish calls device module", context do
+      %{realm_name: realm_name, interfaces: interfaces, device: device, auth_conn: conn} = context
+      valid_interfaces_for_update = interfaces |> Enum.filter(&(&1.ownership == :server))
+
+      check all interface_to_update <- member_of(valid_interfaces_for_update),
+                mapping_update <- valid_mapping_update_for(interface_to_update) do
+        path =
+          interface_values_path(
+            conn,
+            :update,
+            realm_name,
+            device.encoded_id,
+            interface_to_update.name,
+            path_tokens(mapping_update.path)
+          )
+
+        Device
+        |> allow(self(), conn.owner)
+        |> expect(:update_interface_values, fn realm,
+                                               device_id,
+                                               interface_name,
+                                               path,
+                                               value,
+                                               _parameters ->
+          assert realm == realm_name
+          assert device_id == device.encoded_id
+          assert interface_name == interface_to_update.name
+          assert value == mapping_update.value
+          assert "/" <> path == mapping_update.path
+
+          {:ok, %InterfaceValues{data: value}}
+        end)
+
+        conn = post(conn, path, %{"data" => mapping_update.value})
+
+        assert json_response(conn, 200)["data"] == mapping_update.value
+      end
+    end
+  end
+
+  defp path_tokens(path) do
+    String.split(path, "/", trim: true)
+  end
+end
