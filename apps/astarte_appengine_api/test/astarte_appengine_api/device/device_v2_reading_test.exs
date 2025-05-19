f89bd35f45924f3b4dec295e24255743b972d62a
Francesco Noacco <francesco.noacco@secomind.com>
2025-05-19 17:43:03 +0200

    test(appengine): datastream timeframe options
    add tests for `since`, `since_after`, `to` options for datastream
    interfaces
    
    the downsampling option has been set to always return `nil` as it may
    try to downsample non-downsampable keys, and should be handled by
    passing the interface to the generator (not done in this pr)
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- apps/astarte_appengine_api/test/astarte_appengine_api/device/device_v2_reading_test.exs
+++ apps/astarte_appengine_api/test/astarte_appengine_api/device/device_v2_reading_test.exs
@@ -106,6 +106,102 @@ defmodule Astarte.AppEngine.API.Device.DeviceV2ReadingTest do
     end
   end
 
+  property "allows restricting datastream data returned to an arbitrary timeframe", context do
+    %{
+      realm_name: realm_name,
+      device: device,
+      registered_paths: registered_paths,
+      registered_timings: registered_timings,
+      interfaces_with_data: interfaces
+    } = context
+
+    datastreams = interfaces |> Enum.filter(&(&1.type == :datastream))
+
+    check all interface <- member_of(datastreams),
+              "/" <> path <-
+                member_of(registered_paths[{interface.name, interface.major_version}]),
+              timings = registered_timings[{interface.name, interface.major_version}],
+              since_or_after <- member_of([:since, :since_after]),
+              lower_limit <- optional(timestamp_at_least(timings.initial_time)),
+              to <- optional(timestamp_at_most(timings.last_time)),
+              params = [{since_or_after, lower_limit}, to: to],
+              opts <- interface_values_options(params) do
+      result =
+        Device.get_interface_values!(
+          realm_name,
+          device.encoded_id,
+          interface.name,
+          path,
+          opts
+        )
+
+      # If the API returns path not found, it means we have filtered out all values, so this is
+      # a valid result
+      timestamps =
+        case result do
+          {:ok, result} -> extract_timestamps(result, opts.format)
+          {:error, :path_not_found} -> []
+        end
+
+      if opts[:since] != nil do
+        assert Enum.all?(timestamps, &(!DateTime.before?(&1, lower_limit)))
+      end
+
+      if opts[:since_after] != nil do
+        assert Enum.all?(timestamps, &DateTime.after?(&1, lower_limit))
+      end
+
+      if to != nil do
+        assert Enum.all?(timestamps, &DateTime.before?(&1, to))
+      end
+    end
+  end
+
+  defp optional(gen), do: one_of([nil, gen])
+
+  defp timestamp_at_least(datetime) do
+    min = DateTime.to_unix(datetime)
+    max = min + 500
+
+    integer(min..max)
+    |> map(&DateTime.from_unix!/1)
+  end
+
+  defp timestamp_at_most(datetime) do
+    max = DateTime.to_unix(datetime)
+    min = max - 500
+
+    integer(min..max)
+    |> map(&DateTime.from_unix!/1)
+  end
+
+  defp extract_timestamps(%InterfaceValues{data: data}, "structured"),
+    do: Enum.map(data, &to_datetime(Map.fetch!(&1, "timestamp")))
+
+  defp extract_timestamps(%InterfaceValues{metadata: metadata, data: data}, "table") do
+    timestamp_index =
+      metadata["columns"]["timestamp"] || flunk("no timestamp column in table result")
+
+    Enum.map(data, &to_datetime(Enum.at(&1, timestamp_index)))
+  end
+
+  defp extract_timestamps(%InterfaceValues{data: %{"value" => data}}, "disjoint_tables")
+       when is_list(data),
+       do: Enum.map(data, fn [_value, timestamp] -> to_datetime(timestamp) end)
+
+  defp extract_timestamps(%InterfaceValues{data: data}, "disjoint_tables")
+       when is_map(data) do
+    data
+    |> Map.values()
+    |> Enum.concat()
+    |> Enum.map(fn [_value, timestamp] -> to_datetime(timestamp) end)
+  end
+
+  defp to_datetime(timestamp) when is_integer(timestamp),
+    do: DateTime.from_unix!(timestamp, :millisecond)
+
+  defp to_datetime(datetime) when is_struct(datetime, DateTime), do: datetime
+
   defp result_size(result) when is_list(result), do: Enum.count(result)
   defp result_size(%{"value" => result}), do: result_size(result)
   defp result_size(_single_result), do: 1
