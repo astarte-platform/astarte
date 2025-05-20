36c06f49d88dbb31eae542fb33beef0102993d16
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-05-20 15:21:23 +0200

    test(DUP): add test dependencies
    Adds Mimic and Astarte Generators as test dependencies to allow for property
    based testing
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

--- apps/astarte_data_updater_plant/mix.exs
+++ apps/astarte_data_updater_plant/mix.exs
@@ -71,6 +71,7 @@ defmodule Astarte.DataUpdaterPlant.Mixfile do
       {:astarte_core, github: "astarte-platform/astarte_core", branch: "release-1.2"},
       {:astarte_data_access,
        github: "astarte-platform/astarte_data_access", branch: "release-1.2"},
+      {:astarte_generators, github: "astarte-platform/astarte_generators", only: [:dev, :test]},
       {:astarte_rpc, "~> 1.2"}
     ]
   end
@@ -82,6 +83,7 @@ defmodule Astarte.DataUpdaterPlant.Mixfile do
       {:cyanide, "~> 2.0"},
       {:excoveralls, "~> 0.15", only: :test},
       {:mox, "~> 1.0", only: :test},
+      {:mimic, "~> 1.11", only: :test},
       {:exandra, "~> 0.13"},
       # hex.pm package and esl/ex_rabbit_pool do not support amqp version 2.1.
       # This fork is supporting amqp ~> 2.0 and also ~> 3.0.
