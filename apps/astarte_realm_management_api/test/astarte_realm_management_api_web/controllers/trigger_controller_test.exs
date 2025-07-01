ebf43adfb0d4655f767b8f4d94f25916b09d842e
Francesco Noacco <francesco.noacco@secomind.com>
2025-07-01 11:21:17 +0200

    refactor(rm): manually convert action map to action struct
    https://github.com/astarte-platform/astarte/pull/1237 changed the
    get_trigger function to now return the action struct, but to do so
    it uses `keys: atoms!` in the `Jason.decode` call, which is also applied
    recursively to the static headers, and may fail if the header does not
    have a pre-existing atom.
    
    by manually assigning each field to the struct, we avoid this problem
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- apps/astarte_realm_management_api/test/astarte_realm_management_api_web/controllers/trigger_controller_test.exs
+++ apps/astarte_realm_management_api/test/astarte_realm_management_api_web/controllers/trigger_controller_test.exs
@@ -99,7 +99,7 @@ defmodule Astarte.RealmManagement.APIWeb.TriggerControllerTest do
       post_conn =
         post(conn, trigger_path(conn, :create, realm), data: valid_trigger_attrs())
 
-      response = json_response(post_conn, 201)["data"]
+      json_response(post_conn, 201)["data"]
 
       delete_conn =
         delete(conn, trigger_path(conn, :delete, realm, valid_trigger_attrs()["name"]))
