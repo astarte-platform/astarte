211944de9ce69e1cd4ec2f865573371235509917
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-07-09 17:19:20 +0200

    feat: device capabilities (#114)
    makes data_access depend on `release-1.3` of astarte_core
    
    Adds device capabilities, representing in the database `Astarte.Core.Device.Capabilities`
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Astarte.DataAccess.Mixfile do
 
   defp astarte_required_modules(_) do
     [
-      {:astarte_core, github: "astarte-platform/astarte_core", branch: "release-1.2"}
+      {:astarte_core, github: "astarte-platform/astarte_core", branch: "release-1.3"}
     ]
   end
 
