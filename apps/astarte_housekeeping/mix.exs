b193681ad69a2b28f753abc3c1193f9196aaa38f
Omar <omar.brbutovic@secomind.com>
2025-07-23 12:51:08 +0200

    chore(HK): Install credo dependency
    Add credo to the project.
    Credo is a static code analysis tool that can suggest refactoring
    opportunities for complex code, inconsistencies and common mistakes.
    The aim is to enforce a better coding style.
    
    Signed-off-by: Omar <omar.brbutovic@secomind.com>

--- apps/astarte_housekeeping/mix.exs
+++ apps/astarte_housekeeping/mix.exs
@@ -101,6 +101,7 @@ defmodule Astarte.Housekeeping.Mixfile do
       # Workaround for Elixir 1.15 / ssl_verify_fun issue
       # See also: https://github.com/deadtrickster/ssl_verify_fun.erl/pull/27
       {:ssl_verify_fun, "~> 1.1.0", manager: :rebar3, override: true},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:astarte_generators, github: "astarte-platform/astarte_generators", only: [:dev, :test]}
     ]
   end
