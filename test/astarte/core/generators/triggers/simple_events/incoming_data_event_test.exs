d078219e0e9c22bc4851f8cce579a2a5160ecd9b
Gabriele Ghio <gabriele.ghio@secomind.com>
2025-09-04 14:26:50 +0200

    incoming_data_event + test
    Signed-off-by: Gabriele Ghio <gabriele.ghio@secomind.com>

---
+++ test/astarte/core/generators/triggers/simple_events/incoming_data_event_test.exs
@@ -0,0 +1,65 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2025 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+defmodule Astarte.Core.Generators.Triggers.SimpleEvents.IncomingDataEventTest do
+  use ExUnit.Case, async: true
+  use ExUnitProperties
+
+  alias Astarte.Core.Triggers.SimpleEvents.IncomingDataEvent
+
+  alias Astarte.Core.Generators.Interface, as: InterfaceGenerator
+  alias Astarte.Core.Generators.Mapping.Value, as: ValueGenerator
+
+  alias Astarte.Core.Generators.Triggers.SimpleEvents.IncomingDataEvent,
+    as: IncomingDataEventGenerator
+
+  @moduletag :trigger
+  @moduletag :simple_event
+  @moduletag :incoming_data_event
+
+  @doc false
+  describe "triggers incoming_data_event generator" do
+    @describetag :success
+    @describetag :ut
+    property "generates valid incoming_data_event" do
+      check all incoming_data_event <- IncomingDataEventGenerator.incoming_data_event() do
+        assert %IncomingDataEvent{} = incoming_data_event
+      end
+    end
+
+    property "generates valid incoming_data_event using interface" do
+      check all interface <- InterfaceGenerator.interface(),
+                incoming_data_event <-
+                  IncomingDataEventGenerator.incoming_data_event(interface: interface) do
+        assert %IncomingDataEvent{} = incoming_data_event
+      end
+    end
+
+    property "generates valid incoming_data_event using interface and value" do
+      check all interface <- InterfaceGenerator.interface(),
+                value <- ValueGenerator.value(interface: interface),
+                incoming_data_event <-
+                  IncomingDataEventGenerator.incoming_data_event(
+                    interface: interface,
+                    value: value
+                  ) do
+        assert %IncomingDataEvent{} = incoming_data_event
+      end
+    end
+  end
+end
