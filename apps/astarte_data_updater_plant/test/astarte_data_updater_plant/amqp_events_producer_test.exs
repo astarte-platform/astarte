astarte-platform/astarte

commit b57bf46b4edb1dd410cb9ae580440c21c51006d3

DUP: stop custom handling of AMQP connections for producers
The previous implementation of AMQPEventsProducer leaked channels
when the process did not terminate correctly.
Replace the ad-hoc handling of connections and channels
using the ExRabbitPool pooling library.

Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/amqp_events_producer.ex
@@ -20,13 +20,11 @@ defmodule Astarte.DataUpdaterPlant.AMQPEventsProducer do
   require Logger
   use GenServer
 
-  alias AMQP.Basic
-  alias AMQP.Channel
-  alias AMQP.Connection
-  alias AMQP.Exchange
   alias Astarte.DataUpdaterPlant.Config
+  alias AMQP.Channel
 
   @connection_backoff 10000
+  @adapter Config.amqp_adapter!()
 
   # API
 
@@ -46,17 +44,20 @@ defmodule Astarte.DataUpdaterPlant.AMQPEventsProducer do
 
   # Server callbacks
 
-  def init(_args) do
-    rabbitmq_connect(false)
+  @impl true
+  def init(_opts) do
+    case init_producer() do
+      {:ok, chan} ->
+        {:ok, chan}
+
+      {:error, reason} ->
+        {:stop, reason}
+    end
   end
 
-  def terminate(_reason, %Channel{conn: conn} = chan) do
-    Channel.close(chan)
-    Connection.close(conn)
-  end
-
+  @impl true
   def handle_call({:publish, exchange, routing_key, payload, opts}, _from, chan) do
-    reply = Basic.publish(chan, exchange, routing_key, payload, opts)
+    reply = @adapter.publish(chan, exchange, routing_key, payload, opts)
 
     {:reply, reply, chan}
   end
@@ -64,53 +65,75 @@ defmodule Astarte.DataUpdaterPlant.AMQPEventsProducer do
   def handle_call({:declare_exchange, exchange}, _from, chan) do
     # TODO: we need to decide who is responsible of deleting the exchange once it is
     # no longer needed
-    reply = Exchange.declare(chan, exchange, :direct, durable: true)
+    reply = @adapter.declare_exchange(chan, exchange, type: :direct, durable: true)
 
     {:reply, reply, chan}
   end
 
-  def handle_info(:try_to_connect, _state) do
-    {:ok, new_state} = rabbitmq_connect()
-    {:noreply, new_state}
-  end
-
+  @impl true
   def handle_info({:DOWN, _, :process, _pid, reason}, _state) do
     Logger.warn("RabbitMQ connection lost: #{inspect(reason)}. Trying to reconnect...",
       tag: "events_producer_conn_lost"
     )
 
-    {:ok, new_state} = rabbitmq_connect()
-    {:noreply, new_state}
+    case init_producer() do
+      {:ok, channel} ->
+        {:noreply, channel}
+
+      {:error, _reason} ->
+        schedule_connect()
+        {:noreply, :not_connected}
+    end
   end
 
-  defp rabbitmq_connect(retry \\ true) do
-    with {:ok, conn} <- Connection.open(Config.amqp_producer_options!()),
-         {:ok, chan} <- Channel.open(conn),
-         :ok <- Exchange.declare(chan, Config.events_exchange_name!(), :direct, durable: true),
-         # Get notifications when the chan or connection goes down
-         Process.monitor(chan.pid) do
-      {:ok, chan}
-    else
-      {:error, reason} ->
-        Logger.warn("RabbitMQ Connection error: #{inspect(reason)}",
-          tag: "events_producer_conn_err"
+  defp init_producer() do
+    conn = ExRabbitPool.get_connection_worker(:events_producer_pool)
+
+    with {:ok, channel} <- checkout_channel(conn),
+         :ok <- declare_default_events_exchange(channel, conn) do
+      %Channel{pid: channel_pid} = channel
+      _ref = Process.monitor(channel_pid)
+
+      _ =
+        Logger.debug("AMQPEventsProducer initialized",
+          tag: "event_producer_init_ok"
         )
 
-        maybe_retry(retry)
-
-      :error ->
-        Logger.warn("Unknown RabbitMQ connection error", tag: "events_producer_conn_err")
-        maybe_retry(retry)
+      {:ok, channel}
     end
   end
 
-  defp maybe_retry(retry) do
-    if retry do
-      Logger.warn("Retrying connection in #{@connection_backoff} ms")
-      :erlang.send_after(@connection_backoff, :erlang.self(), :try_to_connect)
-      {:ok, :not_connected}
-    else
-      {:stop, :connection_failed}
+  defp checkout_channel(conn) do
+    with {:error, reason} <- ExRabbitPool.checkout_channel(conn) do
+      _ =
+        Logger.warn(
+          "Failed to check out channel for producer: #{inspect(reason)}",
+          tag: "event_producer_channel_checkout_fail"
+        )
+
+      {:error, :event_producer_channel_checkout_fail}
     end
+  end
+
+  defp declare_default_events_exchange(channel, conn) do
+    with {:error, reason} <-
+           @adapter.declare_exchange(channel, Config.events_exchange_name!(),
+             type: :direct,
+             durable: true
+           ) do
+      Logger.warn(
+        "Error declaring AMQPEventsProducer default events exchange: #{inspect(reason)}",
+        tag: "event_producer_init_fail"
+      )
+
+      # Something went wrong, let's put the channel back where it belongs
+      _ = ExRabbitPool.checkin_channel(conn, channel)
+      {:error, :event_producer_init_fail}
+    end
+  end
+
+  defp schedule_connect() do
+    _ = Logger.warn("Retrying connection in #{@connection_backoff} ms")
+    Process.send_after(@connection_backoff, self(), :init)
   end
 end

apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/config.ex
@@ -229,18 +229,16 @@ defmodule Astarte.DataUpdaterPlant.Config do
           type: :integer,
           default: 10
 
-  @envdoc "The number of connections to RabbitMQ used to produce data"
-  app_env :events_producer_connection_number,
-          :astarte_data_updater_plant,
-          :events_producer_connection_number,
-          os_env: "DATA_UPDATER_PLANT_EVENTS_PRODUCER_CONNECTION_NUMBER",
-          type: :integer,
-          default: 1
-
   # Since we have one channel per queue, this is not configurable
   def amqp_consumer_channels_per_connection_number!() do
     ceil(data_queue_total_count!() / amqp_consumer_connection_number!())
   end
+
+  # Since we have only one producer, this is not configurable
+  def events_producer_connection_number!(), do: 1
+
+  # Since we have one channel per queue, this is not configurable
+  def events_producer_channels_per_connection_number!(), do: 1
 
   @doc """
   Returns the AMQP data consumer connection options
@@ -350,7 +348,8 @@ defmodule Astarte.DataUpdaterPlant.Config do
       username: amqp_producer_username,
       password: amqp_producer_password,
       virtual_host: amqp_producer_virtual_host,
-      port: amqp_producer_port
+      port: amqp_producer_port,
+      channels: events_producer_channels_per_connection_number!()
     ]
     |> populate_producer_ssl_options()
   end
@@ -403,6 +402,15 @@ defmodule Astarte.DataUpdaterPlant.Config do
 
       Keyword.put(ssl_options, :server_name_indication, to_charlist(server_name))
     end
+  end
+
+  def events_producer_pool_config!() do
+    [
+      name: {:local, :events_producer_pool},
+      worker_module: ExRabbitPool.Worker.RabbitConnection,
+      size: events_producer_connection_number!(),
+      max_overflow: 0
+    ]
   end
 
   def data_updater_deactivation_interval_ms! do

apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/data_pipeline_supervisor.ex
@@ -22,6 +22,7 @@ defmodule Astarte.DataUpdaterPlant.DataPipelineSupervisor do
   alias Astarte.DataUpdaterPlant.ConsumersSupervisor
   alias Astarte.DataUpdaterPlant.AMQPEventsProducer
   alias Astarte.DataUpdaterPlant.RPC.Handler
+  alias Astarte.DataUpdaterPlant.Config
 
   alias Astarte.RPC.Protocol.DataUpdaterPlant, as: Protocol
 
@@ -34,6 +35,9 @@ defmodule Astarte.DataUpdaterPlant.DataPipelineSupervisor do
     children = [
       {Registry, [keys: :unique, name: Registry.MessageTracker]},
       {Registry, [keys: :unique, name: Registry.DataUpdater]},
+      {ExRabbitPool.PoolSupervisor,
+       rabbitmq_config: Config.amqp_producer_options!(),
+       connection_pools: [Config.events_producer_pool_config!()]},
       AMQPEventsProducer,
       ConsumersSupervisor,
       {Astarte.RPC.AMQP.Server, [amqp_queue: Protocol.amqp_queue(), handler: Handler]},

apps/astarte_data_updater_plant/test/astarte_data_updater_plant/amqp_options_test.exs
@@ -93,7 +93,7 @@ defmodule Astarte.DataUpdaterPlant.AMQPOptionsTest do
     test "get default values when not set" do
       amqp_producer_options = Config.amqp_producer_options!()
 
-      assert length(amqp_producer_options) == 5
+      assert length(amqp_producer_options) == 6
       assert password: Config.amqp_consumer_password!() in amqp_producer_options
       assert username: Config.amqp_consumer_username!() in amqp_producer_options
       assert port: Config.amqp_consumer_port!() in amqp_producer_options
@@ -102,10 +102,10 @@ defmodule Astarte.DataUpdaterPlant.AMQPOptionsTest do
 
       assert Keyword.get(amqp_producer_options, :ssl_options) == nil
 
-      # TODO remove asimmetry here
-      assert Config.amqp_producer_options!() ++
-               [channels: Config.amqp_consumer_channels_per_connection_number!()] ==
-               Config.amqp_consumer_options!()
+      consumer_options = Keyword.delete(Config.amqp_consumer_options!(), :channels)
+      producer_options = Keyword.delete(Config.amqp_producer_options!(), :channels)
+
+      assert consumer_options == producer_options
     end
 
     test "after setting its values" do
@@ -117,7 +117,7 @@ defmodule Astarte.DataUpdaterPlant.AMQPOptionsTest do
 
       amqp_producer_options = Config.amqp_producer_options!()
 
-      assert length(amqp_producer_options) == 5
+      assert length(amqp_producer_options) == 6
       assert password: "password" in amqp_producer_options
       assert username: "username" in amqp_producer_options
       assert port: 12345 in amqp_producer_options
@@ -138,10 +138,10 @@ defmodule Astarte.DataUpdaterPlant.AMQPOptionsTest do
       assert server_name_indication:
                Config.amqp_consumer_ssl_custom_sni!() in ssl_amqp_producer_options
 
-      # TODO remove asimmetry here
-      assert Config.amqp_producer_options!() ++
-               [channels: Config.amqp_consumer_channels_per_connection_number!()] ==
-               Config.amqp_consumer_options!()
+      consumer_options = Keyword.delete(Config.amqp_consumer_options!(), :channels)
+      producer_options = Keyword.delete(Config.amqp_producer_options!(), :channels)
+
+      assert consumer_options == producer_options
     end
 
     test "SSL is enabled for consumer and its ca_cert is set" do
@@ -158,10 +158,10 @@ defmodule Astarte.DataUpdaterPlant.AMQPOptionsTest do
       assert server_name_indication:
                Config.amqp_consumer_ssl_custom_sni!() in ssl_amqp_producer_options
 
-      # TODO remove asimmetry here
-      assert Config.amqp_producer_options!() ++
-               [channels: Config.amqp_consumer_channels_per_connection_number!()] ==
-               Config.amqp_consumer_options!()
+      consumer_options = Keyword.delete(Config.amqp_consumer_options!(), :channels)
+      producer_options = Keyword.delete(Config.amqp_producer_options!(), :channels)
+
+      assert consumer_options == producer_options
     end
 
     test "SSL is enabled and ca_cert is set for producer" do

commit f3edb85d33c4151cf6b8a1770eae8da3d7040865

fix(dup): correctly reconnect to amqp after a connection loss
the refactor in b57bf46 broke the reconnection logic by removing the
handle of the reconnection message. this also explains why the arguments
in the `Process.send_after` call have been out of order for months
without anyone noticing.

this logic is now reintroduced with the following modifications:
- we do not reconnect if we're already connected to a channel. this is
  needed to prevent subsequent `:init` messages from making the producer
  unusable: this would result in a call to
  `ExRabbitPool.checkout_channel` when all channels in the pool are
  already allocated, resulting in an `:out_of_channels` error which will
  recursively schedule a reconnection and fail.
- a regression test has been added for the feature. to avoid the test
  being 20 seconds long, the backoff has been set to 0 in tests

Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/amqp_events_producer.ex
@@ -1,7 +1,7 @@
 #
 # This file is part of Astarte.
 #
-# Copyright 2017-2020 Ispirata Srl
+# Copyright 2017-2025 SECO Mind Srl
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ defmodule Astarte.DataUpdaterPlant.AMQPEventsProducer do
   alias Astarte.DataUpdaterPlant.Config
   alias AMQP.Channel
 
-  @connection_backoff 10000
+  @connection_backoff if Mix.env() == :test, do: 0, else: 10000
   @adapter Config.amqp_adapter!()
 
   # API
@@ -85,6 +85,21 @@ defmodule Astarte.DataUpdaterPlant.AMQPEventsProducer do
         {:noreply, :not_connected}
     end
   end
+
+  @impl true
+  def handle_info(:init, :not_connected) do
+    case init_producer() do
+      {:ok, chan} ->
+        {:noreply, chan}
+
+      {:error, _reason} ->
+        schedule_connect()
+        {:noreply, :not_connected}
+    end
+  end
+
+  @impl true
+  def handle_info(:init, chan), do: {:noreply, chan}
 
   defp init_producer() do
     conn = ExRabbitPool.get_connection_worker(:events_producer_pool)

apps/astarte_data_updater_plant/test/astarte_data_updater_plant/amqp_events_producer_test.exs
@@ -1,0 +1,53 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2025 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+defmodule Astarte.DataUpdaterPlant.AMQPEventsProducerTest do
+  # Needs to be executed synchronously to avoid dirsrupting other tests
+  use ExUnit.Case
+  use Mimic
+
+  alias AMQP.Channel
+  alias Astarte.DataUpdaterPlant.AMQPEventsProducer
+
+  @tag :regression
+  test "events producer reconnects in case of error" do
+    test_pid = self()
+    pid = GenServer.whereis(AMQPEventsProducer)
+    channel = :sys.get_state(pid)
+    assert is_struct(channel, Channel)
+
+    ExRabbitPool
+    |> expect(:checkout_channel, fn _conn -> {:error, :disconnected} end)
+    |> expect(:checkout_channel, fn _conn -> {:error, :out_of_channels} end)
+    |> expect(:checkout_channel, fn conn ->
+      res = Mimic.call_original(ExRabbitPool, :checkout_channel, [conn])
+      send(test_pid, :reconnection_done)
+
+      res
+    end)
+    |> allow(self(), pid)
+
+    Channel.close(channel)
+
+    # Wait for reconnection to happen
+    assert_receive :reconnection_done
+
+    new_status = :sys.get_state(pid)
+    assert is_struct(new_status, Channel)
+  end
+end

apps/astarte_data_updater_plant/test/test_helper.exs
@@ -35,6 +35,7 @@ Mimic.copy(Astarte.DataUpdaterPlant.RPC.VMQPlugin)
 Mimic.copy(Astarte.DataUpdaterPlant.TriggersHandler)
 Mimic.copy(System)
 Mimic.copy(Xandra)
+Mimic.copy(ExRabbitPool)
 Mimic.copy(Astarte.DataAccess.Health.Health)
 
 ExUnit.start(capture_log: true)