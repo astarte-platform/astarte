678984b781209cd32b53d86711015072de40c60c
Francesco Noacco <francesco.noacco@secomind.com>
2025-09-25 18:18:30 +0200

    feat(events): per-realm device trigger fetching and querying
    implement two additional functions, for the specific use case of
    realm-wide device trigger fetching and per-device querying
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- libs/astarte_events/lib/astarte_events/triggers/queries.ex
+++ libs/astarte_events/lib/astarte_events/triggers/queries.ex
@@ -20,8 +20,15 @@ defmodule Astarte.Events.Triggers.Queries do
   require Logger
 
   alias Astarte.DataAccess.Consistency
+  alias Astarte.DataAccess.Devices.Device
   alias Astarte.DataAccess.KvStore
   alias Astarte.DataAccess.Realms.Realm
+  alias Astarte.DataAccess.Realms.SimpleTrigger
+  alias Astarte.DataAccess.Repo
+
+  import Ecto.Query
+
+  @type trigger_id() :: Astarte.DataAccess.UUID.t()
 
   def retrieve_policy_name(realm_name, trigger_id) do
     keyspace_name = Realm.keyspace_name(realm_name)
@@ -37,5 +44,57 @@ defmodule Astarte.Events.Triggers.Queries do
     ]
 
     KvStore.fetch_value("trigger_to_policy", trigger_id, :binary, opts)
+  end
+
+  @spec get_policy_name_map(String.t(), [trigger_id()]) :: %{trigger_id() => String.t()}
+  def get_policy_name_map(realm_name, trigger_ids) do
+    keyspace_name = Realm.keyspace_name(realm_name)
+
+    opts = [
+      prefix: keyspace_name,
+      consistency: Consistency.domain_model(:read)
+    ]
+
+    trigger_ids
+    |> Enum.map(&UUID.binary_to_string!/1)
+    |> Enum.chunk_every(99)
+    |> Enum.map(fn trigger_id_chunk ->
+      KvStore
+      |> where([kv], kv.group == "trigger_to_policy" and kv.key in ^trigger_id_chunk)
+      |> Repo.all(opts)
+    end)
+    |> Enum.concat()
+    |> Map.new(fn kv_entry ->
+      trigger_id = kv_entry.key |> UUID.string_to_binary!()
+      policy_name = kv_entry.value
+      {trigger_id, policy_name}
+    end)
+  end
+
+  @spec get_realm_simple_triggers(String.t()) :: [SimpleTrigger.t()]
+  def get_realm_simple_triggers(realm_name) do
+    keyspace_name = Realm.keyspace_name(realm_name)
+    opts = [prefix: keyspace_name, consistency: Consistency.domain_model(:read)]
+
+    Repo.all(SimpleTrigger, opts)
+  end
+
+  @spec get_device_groups(String.t(), Astarte.DataAccess.UUID.t()) :: [String.t()]
+  def get_device_groups(realm_name, device_id) do
+    keyspace_name = Realm.keyspace_name(realm_name)
+
+    opts = [
+      prefix: keyspace_name,
+      consistency: Consistency.domain_model(:read)
+    ]
+
+    Device
+    |> select([:groups])
+    |> Repo.get(device_id, opts)
+    |> case do
+      nil -> []
+      %{groups: nil} -> []
+      %{groups: groups} -> Map.keys(groups)
+    end
   end
 end
