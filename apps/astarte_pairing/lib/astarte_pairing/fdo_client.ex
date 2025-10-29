1f639232d365bf2820e5a911d7451a17f12f30c6
Riccardo Nalgi <riccardo.nalgi@secomind.com>
2025-10-29 17:29:09 +0100

    feat: TO0.OwnerSign implementation
    -Implemented logic to build and send the TO0.OwnerSign message after recieving the HelloAck from the Rendezvous.
    -Added documentation on testing the TO0 protocol with the current draft setup.
    -Added tests for the new TO0.OwnerSign functionality.
    Signed-off-by: Riccardo Nalgi <riccardo.nalgi@secomind.com>

--- apps/astarte_pairing/lib/astarte_pairing/fdo_client.ex
+++ apps/astarte_pairing/lib/astarte_pairing/fdo_client.ex
@@ -23,13 +23,18 @@ defmodule Astarte.Pairing.FDOClient do
   require Logger
 
   @doc """
-  TO0.Hello - Type 20 message to initiate TO0 protocol
-  Sends an empty array as per FDO specification section 5.3.1
-  Returns decoded TO0.HelloAck (message 21) with rendezvous nonce
+  TO0.Hello - Type 20 message to initiate TO0 protocol.
+  Sends an empty array as per FDO specification section 5.3.1.
+  Returns decoded TO0.HelloAck (message 21) with rendezvous nonce.
   """
   def to0_hello() do
     url = "#{fdo_rendezvous_url!()}/fdo/101/msg/20"
-    headers = [{"Content-Type", "application/cbor"}, {"Content-Length", "0"}]
+
+    headers = [
+      {"Content-Type", "application/cbor"},
+      {"Content-Length", "0"}
+    ]
+
     request_body = CBOR.encode([])
 
     Logger.debug("Sending TO0.Hello to FDO rendezvous server...", url: url)
@@ -41,8 +46,8 @@ defmodule Astarte.Pairing.FDOClient do
 
         case TO0Util.get_nonce_from_hello_ack(body) do
           {:ok, nonce} ->
-            # TODO: to0owner_sign
-            {:ok, nonce}
+            Logger.debug("Nonce retrieved from TO0.HelloAck", nonce: Base.encode16(nonce))
+            to0_owner_sign(nonce, get_auth_bearer(headers))
 
           {:error, reason} ->
             Logger.error("Failed to get nonce from TO0.HelloAck", reason: reason)
@@ -56,6 +61,58 @@ defmodule Astarte.Pairing.FDOClient do
 
       {:error, reason} ->
         Logger.error("TO0.Hello HTTP request failed", reason: inspect(reason))
+    end
+  end
+
+  defp get_auth_bearer(headers) do
+    Enum.find_value(headers, fn
+      {"authorization", value} ->
+        Logger.debug("Authorization Bearer retrieved: #{inspect(value)}")
+        value
+
+      _ ->
+        nil
+    end)
+  end
+
+  @doc """
+  TO0.OwnerSign - Type 22 message to register ownership.
+  Sends ownership voucher and waits for response from rendezvous server.
+  Returns decoded TO0.AcceptOwner (message 23) with negotiated wait time.
+  """
+  def to0_owner_sign(nonce, bearer) do
+    Logger.debug("Sending TO0.OwnerSign...")
+
+    with {:ok, owner_key} <- TO0Util.get_owner_private_key(),
+         {:ok, ownership_voucher} <- TO0Util.get_ownership_voucher(),
+         {:ok, addr_entries} <- TO0Util.get_astarte_rv_to2_addr_entries(),
+         {:ok, request_body} <-
+           TO0Util.build_owner_sign_message(ownership_voucher, owner_key, nonce, addr_entries) do
+      url = "#{fdo_rendezvous_url!()}/fdo/101/msg/22"
+
+      headers = [
+        {"Content-Type", "application/cbor"},
+        {"Content-Length", byte_size(request_body)},
+        {"Authorization", bearer}
+      ]
+
+      Logger.debug("Sending TO0.OwnerSign", payload_size: byte_size(request_body))
+
+      case http_client().post(url, request_body, headers) do
+        {:ok, %{status_code: 200, body: _body}} ->
+          Logger.debug("TO0.OwnerSign completed successfully!")
+
+        {:ok, %{status_code: status_code, body: body}} ->
+          Logger.error(
+            "TO0.OwnerSign failed with status #{status_code}, response: #{inspect(body)}"
+          )
+
+        {:error, reason} ->
+          Logger.error("TO0.OwnerSign request failed", reason: reason)
+      end
+    else
+      {:error, reason} ->
+        Logger.error("Failed to prepare TO0.OwnerSign message", reason: reason)
     end
   end
 
--- apps/astarte_pairing/test/astarte_pairing/fdo/to0util_test.exs
+++ apps/astarte_pairing/test/astarte_pairing/fdo/to0util_test.exs
@@ -1,3 +1,21 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2017-2025 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 defmodule Astarte.Pairing.TO0UtilTest do
   use ExUnit.Case, async: true
 
@@ -39,5 +57,138 @@ defmodule Astarte.Pairing.TO0UtilTest do
       assert {:error, {:unexpected_body_format, _}} =
                TO0Util.get_nonce_from_hello_ack(wrong_cbor2)
     end
+  end
+
+  describe "safe_der_decode/1" do
+    test "returns error for invalid DER data" do
+      assert {:error, {:der_decode_failed, _}} = TO0Util.safe_der_decode(<<0, 1, 2>>)
+    end
+  end
+
+  describe "safe_sign/2" do
+    test "returns error for invalid key format" do
+      data = "test"
+      invalid_key = <<1, 2, 3>>
+      assert {:error, :invalid_private_key_format} = TO0Util.safe_sign(data, invalid_key)
+
+      invalid_key2 = "not_a_binary"
+      assert {:error, :invalid_private_key_format} = TO0Util.safe_sign(data, invalid_key2)
+    end
+  end
+
+  describe "get_astarte_rv_to2_addr_entries/0" do
+    test "returns a list of entries with correct types" do
+      {:ok, entries} = TO0Util.get_astarte_rv_to2_addr_entries()
+      assert is_list(entries)
+      assert length(entries) >= 1
+
+      Enum.each(entries, fn entry ->
+        assert is_binary(entry)
+        {:ok, [decoded], _rest} = CBOR.decode(entry)
+        assert is_list(decoded)
+        assert length(decoded) == 4
+        assert is_binary(Enum.at(decoded, 0))
+        assert is_binary(Enum.at(decoded, 1))
+        assert is_integer(Enum.at(decoded, 2))
+        assert is_integer(Enum.at(decoded, 3))
+      end)
+    end
+  end
+
+  describe "build_cose_sign1/2" do
+    test "returns sign with correct cbor structure for valid payload and owner key" do
+      payload = CBOR.encode(["test", 123])
+
+      {:ok, owner_key} = get_mock_owner_key()
+
+      result = TO0Util.build_cose_sign1(payload, owner_key)
+      assert {:ok, %CBOR.Tag{tag: 18, value: cose_sign1_array}} = result
+      assert is_list(cose_sign1_array)
+      assert length(cose_sign1_array) == 4
+    end
+  end
+
+  describe "build_owner_sign_message/4" do
+    test "returns TO0.OwnerSign message when given valid inputs" do
+      with {:ok, owner_key} <- get_mock_owner_key(),
+           {:ok, ownership_voucher} <- get_mock_ownership_voucher(),
+           {:ok, addr_entries} <- get_mock_astarte_rv_to2_addr_entries(),
+           {:ok, nonce} <- get_mock_nonce(),
+           {:ok, to0_owner_sign_msg} <-
+             TO0Util.build_owner_sign_message(ownership_voucher, owner_key, nonce, addr_entries) do
+        assert is_binary(to0_owner_sign_msg)
+      end
+    end
+  end
+
+  defp get_mock_astarte_rv_to2_addr_entries() do
+    with {:ok, rv_entry1} <- TO0Util.build_rv_to2_addr_entry(CBOR.encode([]), "pippo", 8080, 3),
+         {:ok, rv_entry2} <- TO0Util.build_rv_to2_addr_entry(CBOR.encode([]), "paperino", 8080, 3) do
+      {:ok, [rv_entry1, rv_entry2]}
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp get_mock_nonce() do
+    nonce = <<32, 54, 127, 243, 66, 48, 228, 115, 59, 186, 230, 246, 198, 179, 113, 78>>
+    {:ok, nonce}
+  end
+
+  defp get_mock_ownership_voucher do
+    ownership_voucher = """
+    -----BEGIN OWNERSHIP VOUCHER-----
+    hRhlWNiGGGVQAYHfMFvr/EFPkrDfd5dxZ4GFggVQb2Zkby5leGFtcGxlLmNvbYIC
+    UVAAAAAAAAAAAAAA//9/AAABggxBAYIDQxkfaYIEQxkfaWZnb3Rlc3SDCgFYWzBZ
+    MBMGByqGSM49AgEGCCqGSM49AwEHA0IABJN09TXjwFiTHtW4/YnnmXXAf0FL2t3w
+    9d9om8aydUrtz1ejG8rIMExyhyVnYDVYgMf5hQTtAJd3/J9B+S8LijWCL1ggZ40j
+    /ULTK2LRwjMu3IwGBF8fAwvS411brOsTzhU7Y+WCBVggJNtX1qgc9dh7wz0tvTJs
+    tb4Vo/LXA5COgcj8x+hzANKCWQGDMIIBfzCCASWgAwIBAgIUG+ai0U9Ht7elK5hY
+    OqFDbXUON+gwCgYIKoZIzj0EAwIwMDELMAkGA1UEBhMCVVMxEDAOBgNVBAoMB0V4
+    YW1wbGUxDzANBgNVBAMMBkRldmljZTAgFw0yNTEwMjIxMjM5MzJaGA8yMDU1MDUx
+    ODEyMzkzMlowGDEWMBQGA1UEAxMNZGV2aWNlLmdvLWZkbzBZMBMGByqGSM49AgEG
+    CCqGSM49AwEHA0IABJsCjFMh2CusnD/CTZ0BWCL5aqb5Wt7506PqGOPzPSxffu2M
+    w6cUu2E75OgBzNSKFAryOm5S8DICsMSOJ2aUWxOjMzAxMA4GA1UdDwEB/wQEAwIH
+    gDAfBgNVHSMEGDAWgBTQie7mYwn8UTIHbj1YHKWqgpdMgTAKBggqhkjOPQQDAgNI
+    ADBFAiEAjb7AcZWtgFBvVJ5ddA9ItKWyeJdhVKpKXbcBwUz78o8CID21S/9kPx0a
+    oPqp1dAGCSYS2vlRbFDflMfFQrFVCsB3WQG5MIIBtTCCAVugAwIBAgIUPSF900zN
+    HhKedT517226SYKoL9swCgYIKoZIzj0EAwIwMDELMAkGA1UEBhMCVVMxEDAOBgNV
+    BAoMB0V4YW1wbGUxDzANBgNVBAMMBkRldmljZTAeFw0yNTEwMjIxMjM0MzlaFw0y
+    NjEwMjIxMjM0MzlaMDAxCzAJBgNVBAYTAlVTMRAwDgYDVQQKDAdFeGFtcGxlMQ8w
+    DQYDVQQDDAZEZXZpY2UwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAASkL7ijPGa2
+    pLnxy7ZC03olpdFxhIfFh+omay2heOb0+1Jl5lc/CaB60rsnQmupA4T2CTCxIbFh
+    gnC3XXfbB+nko1MwUTAdBgNVHQ4EFgQU0Inu5mMJ/FEyB249WBylqoKXTIEwHwYD
+    VR0jBBgwFoAU0Inu5mMJ/FEyB249WBylqoKXTIEwDwYDVR0TAQH/BAUwAwEB/zAK
+    BggqhkjOPQQDAgNIADBFAiBshWDCM/YmnSUhT99c1PeMSwxS8w1uiJ+4uA3nMpN3
+    RgIhAKTZE4tZmuRDZn30L71dGEG5GTMQdpfSi2XcMdsZTfbMgdKEQ6EBJqBZAgmE
+    gi9YIGCEWa+pSOW0mjRDc+rmEjbz6/jbmiFPvnl9hvo+v3Bxgi9YIEkPS6IhbWb7
+    sA3PktmvLNGzxG83mZLC3We3ZiVG7+VpQaCDCgKBWQG3MIIBszCCAVmgAwIBAgIU
+    LosSffQPeA7p74gTpP1r8nyEkmYwCgYIKoZIzj0EAwIwLzELMAkGA1UEBhMCVVMx
+    EDAOBgNVBAoMB0V4YW1wbGUxDjAMBgNVBAMMBU93bmVyMB4XDTI1MTAyMjEyMzQz
+    OVoXDTI2MTAyMjEyMzQzOVowLzELMAkGA1UEBhMCVVMxEDAOBgNVBAoMB0V4YW1w
+    bGUxDjAMBgNVBAMMBU93bmVyMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAErzCg
+    /gc+phQos1a0wwhlIialnqYc0YaJAV6h2VXMZ0bTE4UOxv8atpH9eKHl9vMDgHDe
+    MFBjPCBv1414gGySIaNTMFEwHQYDVR0OBBYEFJ6le2Ei1zjU5mCij7ZegBtBXk03
+    MB8GA1UdIwQYMBaAFJ6le2Ei1zjU5mCij7ZegBtBXk03MA8GA1UdEwEB/wQFMAMB
+    Af8wCgYIKoZIzj0EAwIDSAAwRQIhAJDLePyqpyGgUulA1HI0vDdl4+MOQZBIfHIN
+    ZY6S8UZGAiAbnGOoDZLJIGWz2FNTrsr1y8t0CWNbk6J+gOdit/rfAFhAFOU2tL7T
+    P3+w5O0SSWAYKnli+Dp23IndYOxOrS84yalTH9zt5aj3AIiMK642jsIRwHW4jK6O
+    uliN+61J1dB0nA==
+    -----END OWNERSHIP VOUCHER-----
+    """
+
+    {:ok, ownership_voucher}
+  end
+
+  defp get_mock_owner_key do
+    owner_key = """
+    -----BEGIN EC PRIVATE KEY-----
+    MHcCAQEEIFlbTEE1Ce+RSqhU8FqxsY7eNb9BaBWOTw6qFv7l0DZtoAoGCCqGSM49
+    AwEHoUQDQgAEocPEIHIrn08VRO5zkkDztwp72Sw0BSm0mZeLgOKkHLUPdVFFlc0E
+    O82b1/S2Cwzwh8MIDDx0CN2b+IBl5bRwOw==
+    -----END EC PRIVATE KEY-----
+    """
+
+    {:ok, owner_key}
   end
 end
