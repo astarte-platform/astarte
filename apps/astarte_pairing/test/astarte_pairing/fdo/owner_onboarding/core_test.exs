47790160b655fe33bfce4800e690db945beb9743
nedimtokic <64848448+nedimtokic@users.noreply.github.com>
2025-11-20 15:36:43 +0100

    chore(pairing): expose hellodevice to2 endpoint (#1601)
    Add endpoint for receiving HelloDevice
    message from device as per FDO
    specification
    
    Signed-off-by: nedimtokic <nedim.tokic@secomind.com>

---
+++ apps/astarte_pairing/test/astarte_pairing/fdo/owner_onboarding/core_test.exs
@@ -0,0 +1,79 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2025 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+defmodule Astarte.Pairing.FDO.OwnerOnboarding.CoreTest do
+  use ExUnit.Case, async: true
+
+  alias Astarte.Pairing.FDO.OwnerOnboarding.Core
+  alias Astarte.Pairing.FDO.OwnerOnboarding.HelloDevice
+
+  describe "decode_hello_device/1" do
+    setup do
+      valid_data = %{
+        max_size: 65535,
+        device_id: <<1, 2, 3, 4>>,
+        nonce: <<5, 6, 7, 8>>,
+        kex_name: "DHKEXid14",
+        cipher_name: "AES256GCM",
+        easig_info: ["ES256", <<0x01, 0x02>>]
+      }
+
+      {:ok, valid_data: valid_data}
+    end
+
+    test "successfully decodes a valid hello device structure", %{valid_data: data} do
+      cbor_payload = CBOR.encode(hello_device_list(data))
+
+      assert {:ok, %HelloDevice{}} =
+               Core.decode_hello_device(cbor_payload)
+    end
+
+    test "successfully decodes a valid hello device CBOR payload", %{valid_data: data} do
+      cbor_payload = CBOR.encode(hello_device_list(data))
+
+      assert {:ok, %HelloDevice{} = hello_device} =
+               Core.decode_hello_device(cbor_payload)
+
+      assert hello_device.max_size == data.max_size
+      assert hello_device.device_id == data.device_id
+      assert hello_device.nonce == data.nonce
+      assert hello_device.kex_name == data.kex_name
+      assert hello_device.cipher_name == data.cipher_name
+      assert hello_device.easig_info == data.easig_info
+    end
+
+    test "returns error for invalid hello device format" do
+      invalid_payload = [1, 2, 3]
+
+      cbor_payload = CBOR.encode(invalid_payload)
+
+      assert Core.decode_hello_device(cbor_payload) ==
+               {:error, :invalid_hello_device_format}
+    end
+
+    test "returns error when CBOR decode fails" do
+      invalid_binary = "invalid_cbor"
+
+      assert {:error, :invalid_hello_device_message} == Core.decode_hello_device(invalid_binary)
+    end
+  end
+
+  defp hello_device_list(data) do
+    [data.max_size, data.device_id, data.nonce, data.kex_name, data.cipher_name, data.easig_info]
+  end
+end
