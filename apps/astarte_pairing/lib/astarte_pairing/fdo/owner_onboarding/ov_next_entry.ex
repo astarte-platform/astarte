2a57038d1b3443901ef459287f9b91a6f0ef0273
AliouneGaye21 <alioune.gaye@secomind.com>
2025-11-28 16:00:32 +0100

    chore(pairing): ov next entry encoding (#1625)
    Signed-off-by: Alioune Gaye <alioune.gaye@secomind.com>

--- apps/astarte_pairing/lib/astarte_pairing/fdo/owner_onboarding/ov_next_entry.ex
+++ apps/astarte_pairing/lib/astarte_pairing/fdo/owner_onboarding/ov_next_entry.ex
@@ -18,24 +18,35 @@
 
 defmodule Astarte.Pairing.FDO.OwnerOnboarding.OVNextEntry do
   @moduledoc """
-  TO2.OVNextEntry (Msg 63): The Owner sends a specific Ownership Voucher entry.
-
+  TO2.OVNextEntry (Type 63).
   From Owner Onboarding Service to Device ROE.
+
+  This message delivers a single Ownership Voucher entry to the Device.
+  It is the response to TO2.GetOVNextEntry (Msg 62).
+
+  The OVEntry is essentially a link in the certificate chain that allows the Device
+  to verify that the current Owner is legitimate and traces back to the manufacturer.
+
+  Spec Reference: 5.5.5 TO2.OVNextEntry, Type 63
   """
   use TypedStruct
+  alias Astarte.Pairing.FDO.OwnerOnboarding.OVNextEntry
 
   typedstruct enforce: true do
     @typedoc "Structure for TO2.OVNextEntry message."
 
-    # The index of the entry being sent. Must match the request from Msg 62.
+    # OVEntryNum
+    # The index of the entry being sent. Must match the request received in Msg 62.
     field :entry_num, non_neg_integer()
 
-    # The actual Ownership Voucher entry (binary blob/COSE object).
+    # OVEntry
+    # The actual Ownership Voucher entry.
+    # This is a COSE_Sign1 structure (encoded as binary).
     field :ov_entry, binary()
   end
 
   @doc """
-  Converts the struct into a CBOR list for transmission.
+  Converts the struct into a CBOR list.
   Format: [OVEntryNum, OVEntry]
   """
   def to_cbor_list(%__MODULE__{} = t) do
@@ -43,5 +54,15 @@ defmodule Astarte.Pairing.FDO.OwnerOnboarding.OVNextEntry do
       t.entry_num,
       t.ov_entry
     ]
+  end
+
+  @doc """
+  Encodes the struct into a CBOR binary for transmission.
+  """
+  @spec encode(t()) :: binary()
+  def encode(%OVNextEntry{} = t) do
+    t
+    |> to_cbor_list()
+    |> CBOR.encode()
   end
 end
