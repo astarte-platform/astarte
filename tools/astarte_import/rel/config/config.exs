f6bf8b19423db81fd1b18ae59375765a0e931336
Gabriele Ghio <gabriele.ghio@secomind.com>
2025-12-03 11:12:08 +0100

    update astarte_import
    Signed-off-by: Gabriele Ghio <gabriele.ghio@secomind.com>

--- tools/astarte_import/rel/config/config.exs
+++ tools/astarte_import/rel/config/config.exs
@@ -1,7 +1,1 @@
 import Config
-
-cassandra_db_host = System.get_env("CASSANDRA_DB_HOST") || raise "CASSANDRA_DB_HOST not set"
-cassandra_db_port = System.get_env("CASSANDRA_DB_PORT") || raise "CASSANDRA_DB_PORT not set"
-
-config :cqerl,
-  cassandra_nodes: [{cassandra_db_host, cassandra_db_port}]
