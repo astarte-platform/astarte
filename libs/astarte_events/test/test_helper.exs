453bb734aa8bfc2fcc2df07e895f650471a19b16
nedimtokic <64848448+nedimtokic@users.noreply.github.com>
2026-01-29 11:45:25 +0100

    test(events): add full coverage tests for TriggersHandler (#1776)
    Add tests for TriggersHandler to ensure full coverage.
    Tests now verify that dispatch_event/7 correctly builds the
    SimpleEvent struct and forwards it to Core.dispatch_event/3,
    including the edge case when policy_name is nil.
    Also test that register_target/2 properly delegates to Core.
    
    Signed-off-by: nedimtokic <nedim.tokic@secomind.com>

--- libs/astarte_events/test/test_helper.exs
+++ libs/astarte_events/test/test_helper.exs
@@ -1,5 +1,6 @@
+Mimic.copy(Astarte.DataAccess.Config)
 Mimic.copy(Astarte.Events.AMQPEvents)
 Mimic.copy(Astarte.Events.AMQPTriggers)
-Mimic.copy(Astarte.DataAccess.Config)
+Mimic.copy(Astarte.Events.TriggersHandler.Core)
 
 ExUnit.start(capture_logs: true)
