bdd65cc0878061a0c8d17c93af00c4f753b7b505
Riccardo Nalgi <riccardo.nalgi@secomind.com>
2026-05-20 14:37:09 +0200

    chore(secrets): add DEK support via OpenBao transit
    Signed-off-by: Riccardo Nalgi <riccardo.nalgi@secomind.com>

--- libs/astarte_secrets/test/astarte_secrets/core_test.exs
+++ libs/astarte_secrets/test/astarte_secrets/core_test.exs
@@ -626,4 +626,43 @@ defmodule Astarte.Secrets.CoreTest do
       assert :aes256 in Core.symmetric_key_algorithms()
     end
   end
+
+  describe "generate_dek/3 integration" do
+    setup do
+      {:ok, namespace} = Secrets.create_namespace("test", :es256)
+      # TODO: replace with Secrets.create_encryption_key once the public API exists.
+      :ok = Core.create_encryption_key("test-kek", namespace)
+      %{namespace: namespace, key_name: "test-kek"}
+    end
+
+    test "generates a 256-bit DEK wrapped under an AES-256-GCM key", %{
+      namespace: namespace,
+      key_name: key_name
+    } do
+      assert {:ok, %{plaintext: pt, ciphertext: ct}} =
+               Core.generate_dek(key_name, namespace)
+
+      assert byte_size(pt) == 32
+      assert String.starts_with?(ct, "vault:v1:")
+    end
+  end
+
+  describe "unwrap_dek/4 integration" do
+    setup do
+      {:ok, namespace} = Secrets.create_namespace("test", :es256)
+      # TODO: replace with Secrets.create_encryption_key once the public API exists.
+      :ok = Core.create_encryption_key("test-kek", namespace)
+      %{namespace: namespace, key_name: "test-kek"}
+    end
+
+    test "round-trips: generate then unwrap recovers the plaintext", %{
+      namespace: namespace,
+      key_name: key_name
+    } do
+      assert {:ok, %{plaintext: original_pt, ciphertext: ct}} =
+               Core.generate_dek(key_name, namespace)
+
+      assert {:ok, ^original_pt} = Secrets.unwrap_dek(key_name, ct, namespace)
+    end
+  end
 end
