76b6ad6ffdfbbac64a01cff9ee6757e37a13b164
Riccardo Nalgi <riccardo.nalgi@secomind.com>
2026-07-21 12:14:11 +0200

    test(pairing): fix flakiness
    Signed-off-by: Riccardo Nalgi <riccardo.nalgi@secomind.com>

--- apps/astarte_pairing/test/test_helper.exs
+++ apps/astarte_pairing/test/test_helper.exs
@@ -37,4 +37,8 @@ Mimic.copy(Astarte.Secrets)
 Mimic.copy(DateTime)
 Mimic.copy(HTTPoison)
 
+# fix flakiness due to async tests
+Astarte.Secrets.Core.create_nested_namespace(["fdo_owner_keys", "default_instance"])
+Astarte.Secrets.Core.create_nested_namespace(["fdo_owner_keys", "instance"])
+
 ExUnit.start(capture_log: true)
