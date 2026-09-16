257086cb2e376658854709034eeb03abccc4a9bd
Gabriele Ghio <gabriele.ghio@secomind.com>
2026-09-16 10:47:34 +0200

    feat(generators): add trigger generator
    Signed-off-by: Gabriele Ghio <gabriele.ghio@secomind.com>

---
+++ libs/astarte_generators/test/astarte/core/generators/triggers/trigger_test.exs
@@ -0,0 +1,77 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2026 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+defmodule Astarte.Core.Generators.Triggers.TriggerTest do
+  use ExUnit.Case, async: true
+  use ExUnitProperties
+
+  import Astarte.Core.Generators.Triggers.Trigger
+
+  alias Astarte.Core.Triggers.Trigger
+
+  @trigger_uuid <<5, 94, 105, 231, 207, 92, 65, 80, 150, 231, 248, 187, 116, 95, 143, 49>>
+  @simple_trigger_uuid <<150, 67, 77, 67, 99, 180, 73, 104, 191, 225, 11, 143, 30, 171, 126, 0>>
+  @action ~s({"http_url": "https://example.com", "http_method": "post"})
+
+  describe "trigger generator" do
+    property "generates valid triggers" do
+      check all %Trigger{
+                  trigger_uuid: trigger_uuid,
+                  simple_triggers_uuids: simple_triggers_uuids,
+                  action: action,
+                  name: name,
+                  policy: policy
+                } = trigger <- trigger() do
+        action = Jason.decode!(action)
+        %URI{scheme: scheme} = action |> Map.fetch!("http_url") |> URI.parse()
+
+        assert trigger == trigger |> Trigger.encode() |> Trigger.decode()
+        assert uuid_v4?(trigger_uuid)
+        assert length(simple_triggers_uuids) == 1
+        assert Enum.all?(simple_triggers_uuids, &uuid_v4?/1)
+        assert trigger_uuid not in simple_triggers_uuids
+        assert Map.fetch!(action, "http_method") in ~w(delete get head options patch post put)
+        assert scheme in ["http", "https"]
+        assert byte_size(name) in 1..128
+        assert is_nil(policy)
+      end
+    end
+
+    test "uses bottom-up identifiers and policy names" do
+      assert %Trigger{
+               version: 0,
+               trigger_uuid: @trigger_uuid,
+               simple_triggers_uuids: [@simple_trigger_uuid],
+               action: @action,
+               name: "seeded_trigger",
+               policy: "delivery_policy"
+             } =
+               trigger(
+                 trigger_uuid: @trigger_uuid,
+                 simple_triggers_uuids: [@simple_trigger_uuid],
+                 action: @action,
+                 name: "seeded_trigger",
+                 policy: "delivery_policy"
+               )
+               |> Enum.at(0)
+    end
+  end
+
+  defp uuid_v4?(<<_prefix::48, 4::4, _middle::12, 2::2, _suffix::62>>), do: true
+  defp uuid_v4?(_uuid), do: false
+end
